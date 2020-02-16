08f476456826d39cb676658181cebfe8507037f5
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2020-02-16 19:13:10 +0100

    Implementation of Chancellor's Aide

---
+++ server/game/cards/13-CW/ChancellorsAide.js
@@ -0,0 +1,44 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { Players, TargetModes } = require('../../Constants');
+
+class ChancellorsAide extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            title: 'Player discards a card',
+            when: {
+                onCardLeavesPlay: (event, context) => event.card === context.source
+            },
+            targets: {
+                myPlayer: {
+                    mode: TargetModes.Select,
+                    targets: true,
+                    choices: {
+                        [this.controller.name]: AbilityDsl.actions.chosenDiscard(context => ({ target: context.player })),
+                        [this.controller.opponent.name]: AbilityDsl.actions.chosenDiscard(context => ({ target: context.player.opponent }))
+                    }
+                },
+                oppPlayer: {
+                    mode: TargetModes.Select,
+                    targets: true,
+                    player: Players.Opponent,
+                    choices: {
+                        [this.controller.name]: AbilityDsl.actions.joint([
+                            AbilityDsl.actions.takeHonor(context => ({ target: context.player.opponent })),
+                            AbilityDsl.actions.chosenDiscard(context => ({ target: context.player}))
+                        ]),
+                        [this.controller.opponent.name]: AbilityDsl.actions.joint([
+                            AbilityDsl.actions.takeHonor(context => ({ target: context.player.opponent })),
+                            AbilityDsl.actions.chosenDiscard(context => ({ target: context.player.opponent}))
+                        ]),
+                        ['Done']: () => true
+                    }
+                }
+            }
+        });
+    }
+}
+
+ChancellorsAide.id = 'chancellor-s-aide';
+
+module.exports = ChancellorsAide;
---
+++ test/server/cards/13-CW/ChancellorsAide.spec.js
@@ -0,0 +1,149 @@
+describe('Chancellor\'s Aide', function() {
+    integration(function() {
+        describe('Chancellor\'s Aide\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        dynastyDiscard: ['daidoji-kageyu'],
+                        inPlay: ['chancellor-s-aide'],
+                        hand: ['finger-of-jade', 'fine-katana']
+                    },
+                    player2: {
+                        hand: ['finger-of-jade', 'ornate-fan']
+                    }
+                });
+                this.chancellorsAide = this.player1.findCardByName('chancellor-s-aide');
+                this.daidojiKageyu = this.player1.placeCardInProvince('daidoji-kageyu', 'province 2');
+                this.jade = this.player1.findCardByName('finger-of-jade');
+                this.jade2 = this.player2.findCardByName('finger-of-jade');
+            });
+
+            it('should trigger upon leaving play', function() {
+                this.player1.clickCard(this.daidojiKageyu);
+                this.player1.clickCard(this.chancellorsAide);
+
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.chancellorsAide);
+            });
+
+            it('should give both players a prompt to target a player. Opponent is optional', function() {
+                this.player1.clickCard(this.daidojiKageyu);
+                this.player1.clickCard(this.chancellorsAide);
+
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.chancellorsAide);
+
+                this.player1.clickCard(this.chancellorsAide);
+                expect(this.player1).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player1).toHavePromptButton('player1');
+                expect(this.player1).toHavePromptButton('player2');
+                expect(this.player1).not.toHavePromptButton('Done');
+
+                this.player1.clickPrompt('player1');
+
+                expect(this.player2).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player2).toHavePromptButton('player1');
+                expect(this.player2).toHavePromptButton('player2');
+                expect(this.player2).toHavePromptButton('Done');
+            });
+
+            it('works when only the owner of Chancellor\'s Aide chooses a player', function() {
+                this.player1.clickCard(this.daidojiKageyu);
+                this.player1.clickCard(this.chancellorsAide);
+
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.chancellorsAide);
+
+                this.player1.clickCard(this.chancellorsAide);
+                expect(this.player1).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player1).toHavePromptButton('player1');
+                expect(this.player1).toHavePromptButton('player2');
+                expect(this.player1).not.toHavePromptButton('Done');
+
+                this.player1.clickPrompt('player2');
+
+                expect(this.player2).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player2).toHavePromptButton('player1');
+                expect(this.player2).toHavePromptButton('player2');
+                expect(this.player2).toHavePromptButton('Done');
+
+                this.player2.clickPrompt('Done');
+
+                expect(this.player2).toHavePrompt('Choose a card to discard');
+                this.player2.clickCard(this.jade2);
+
+                expect(this.jade2.location).toBe('conflict discard pile');
+            });
+
+            it('works when both players choose to target a player', function() {
+                const player1Honor = this.player1.honor;
+                const player2Honor = this.player2.honor;
+
+                this.player1.clickCard(this.daidojiKageyu);
+                this.player1.clickCard(this.chancellorsAide);
+
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.chancellorsAide);
+
+                this.player1.clickCard(this.chancellorsAide);
+                expect(this.player1).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player1).toHavePromptButton('player1');
+                expect(this.player1).toHavePromptButton('player2');
+                expect(this.player1).not.toHavePromptButton('Done');
+
+                this.player1.clickPrompt('player2');
+
+                expect(this.player2).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player2).toHavePromptButton('player1');
+                expect(this.player2).toHavePromptButton('player2');
+                expect(this.player2).toHavePromptButton('Done');
+
+                this.player2.clickPrompt('player1');
+
+                expect(this.player2).toHavePrompt('Choose a card to discard');
+                this.player2.clickCard(this.jade2);
+
+                expect(this.player1).toHavePrompt('Choose a card to discard');
+                this.player1.clickCard(this.jade);
+
+                expect(this.player1.honor).toBe(player1Honor + 1);
+                expect(this.player2.honor).toBe(player2Honor - 1);
+                expect(this.jade.location).toBe('conflict discard pile');
+                expect(this.jade2.location).toBe('conflict discard pile');
+            });
+
+            it('works when both players choose themselves', function() {
+                this.player1.clickCard(this.daidojiKageyu);
+                this.player1.clickCard(this.chancellorsAide);
+
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.chancellorsAide);
+
+                this.player1.clickCard(this.chancellorsAide);
+                expect(this.player1).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player1).toHavePromptButton('player1');
+                expect(this.player1).toHavePromptButton('player2');
+                expect(this.player1).not.toHavePromptButton('Done');
+
+                this.player1.clickPrompt('player1');
+
+                expect(this.player2).toHavePrompt('Chancellor\'s Aide');
+                expect(this.player2).toHavePromptButton('player1');
+                expect(this.player2).toHavePromptButton('player2');
+                expect(this.player2).toHavePromptButton('Done');
+
+                this.player2.clickPrompt('player2');
+
+                expect(this.player1).toHavePrompt('Choose a card to discard');
+                this.player1.clickCard(this.jade);
+
+                expect(this.player2).toHavePrompt('Choose a card to discard');
+                this.player2.clickCard(this.jade2);
+
+                expect(this.jade.location).toBe('conflict discard pile');
+                expect(this.jade2.location).toBe('conflict discard pile');
+            });
+        });
+    });
+});
