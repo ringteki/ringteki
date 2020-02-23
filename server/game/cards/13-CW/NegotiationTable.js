c2959148c56fade48471868a4cd8482675b79d5c
Marc Soiferman <t>
2020-02-22 20:11:49 -0600

    Working

--- server/game/cards/13-CW/NegotiationTable.js
+++ server/game/cards/13-CW/NegotiationTable.js
@@ -1,157 +1,125 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
-const { Players, TargetModes, CardTypes } = require('../../Constants');
+const GameActions = require('../../GameActions/GameActions');
+const { Players, CardTypes } = require('../../Constants');
 
 class NegotiationTable extends DrawCard {
     setupCardAbilities() {
         this.selectedChoices = [];
         this.action({
             title: 'Make opponent pick from several options',
-            target: {
-                mode: TargetModes.Select,
-                player: Players.Opponent,
-                choices: this.getChoices()
-            },
-            effect: '{1}',
-            effectArgs: context => this.getMesssage(context.select.toLowerCase()),
-            then: context => {
-                this.selectedChoices.push(context.select.toLowerCase());
-                if (context.select.toLowerCase() === 'done') {
-                    this.selectedChoices = [];
-                    return true;
-                }
-                return {
-                    target: {
-                        mode: TargetModes.Select,
-                        choices: this.getChoices()
-                    },
-                    then: context => {
-                        this.selectedChoices.push(context.select.toLowerCase());
-                        if (context.select.toLowerCase() === 'done') {
-                            this.selectedChoices = [];
-                            return true;
-                        }
-                        return {
-                            target: {
-                                mode: TargetModes.Select,
-                                choices: this.getChoices()
-                            },
-                            then: context => {
-                                this.selectedChoices.push(context.select.toLowerCase());
-                                if (context.select.toLowerCase() === 'done') {
-                                    this.selectedChoices = [];
-                                    return true;
-                                }
-                                return {
-                                    target: {
-                                        mode: TargetModes.Select,
-                                        choices: this.getChoices()
-                                    },
-                                    then: context => {
-                                        this.selectedChoices.push(context.select.toLowerCase());
-                                        if (context.select.toLowerCase() === 'done') {
-                                            this.selectedChoices = [];
-                                            return true;
-                                        }
-                                        return {
-                                            target: {
-                                                mode: TargetModes.Select,
-                                                choices: this.getChoices()
-                                            }
-                                        };
-                                    }
-                                };
-                            }
-                        };
-                    }
-                }
+            handler: context => {
+                let choices = [];
+                let handlers = [];
+
+                let drawChoice = 'Draw 1 card';
+                let drawHandler = () => {
+                    choices.splice(choices.indexOf(drawChoice), 1);
+                    handlers.splice(handlers.indexOf(drawHandler), 1);
+                    this.getDrawChoice(context);
+                    this.getHandlerMenu(context, choices, handlers);
+                };
+                let readyChoice = 'Choose and ready a character';
+                let readyHandler = () => {
+                    choices.splice(choices.indexOf(readyChoice), 1);
+                    handlers.splice(handlers.indexOf(readyHandler), 1);
+                    this.getReadyChoice(context);
+                    this.getHandlerMenu(context, choices, handlers);
+                };
+                let fateChoice = 'Gain 1 fate';
+                let fateHandler = () => {
+                    choices.splice(choices.indexOf(fateChoice), 1);
+                    handlers.splice(handlers.indexOf(fateHandler), 1);
+                    this.getFateChoice(context);
+                    this.getHandlerMenu(context, choices, handlers);
+                };
+                let doneChoice = 'Done';
+                let doneHandler = () => {
+                    this.getDoneChoice(context);
+                };
+
+                choices.push(drawChoice);
+                choices.push(readyChoice);
+                choices.push(fateChoice);
+                choices.push(doneChoice);
+
+                handlers.push(drawHandler);
+                handlers.push(readyHandler);
+                handlers.push(fateHandler);
+                handlers.push(doneHandler);
+
+                this.getHandlerMenu(context, choices, handlers);
             }
         });
     }
 
-    getDrawChoice() {
-        return AbilityDsl.actions.sequential([
-            AbilityDsl.actions.draw(context => ({
-                target: context.player.opponent,
-                amount: 1
-            })),
-            AbilityDsl.actions.draw(context => ({
-                target: context.player,
-                amount: 1
-            }))
-        ]);
+    getHandlerMenu(context, choices, handlers) {
+        this.game.promptWithHandlerMenu(context.player.opponent, {
+            activePromptTite: 'Choose an action',
+            source: this,
+            choices: choices,
+            handlers: handlers
+        });
     }
 
-    getReadyChoice() {
-        return AbilityDsl.actions.sequential([
-            AbilityDsl.actions.selectCard(context => ({
+    getDrawChoice(context) {
+        this.game.addMessage('{0} chooses to have each player draw a card', context.player.opponent);
+
+        GameActions.draw(context => ({
+            target: context.player.opponent,
+            amount: 1
+        })).resolve(context.player.opponent, context);
+
+        GameActions.draw(context => ({
+            target: context.player,
+            amount: 1
+        })).resolve(context.player, context);
+    }
+
+    getReadyChoice(context) {
+        this.game.addMessage('{0} chooses to have each player ready a character', context.player.opponent);
+        let bowedCharacters = context.player.cardsInPlay.filter(a => a.type === CardTypes.Character && a.bowed).length + context.player.opponent.cardsInPlay.filter(a => a.type === CardTypes.Character && a.bowed).length;
+
+        if(bowedCharacters > 0) {
+            GameActions.selectCard(context => ({
                 player: Players.Opponent,
                 cardType: CardTypes.Character,
                 targets: true,
                 message: '{0} chooses to ready {1}',
                 messageArgs: card => [context.player.opponent, card],
                 gameAction: AbilityDsl.actions.ready()
-            })),
-            AbilityDsl.actions.selectCard(context => ({
+            })).resolve(context.player.opponent, context);
+        }
+
+        //This is ugly, but it's needed to not deadlock the game
+        if(bowedCharacters > 1) {
+            GameActions.selectCard(context => ({
                 player: Players.Self,
                 cardType: CardTypes.Character,
                 targets: true,
                 message: '{0} chooses to ready {1}',
                 messageArgs: card => [context.player, card],
                 gameAction: AbilityDsl.actions.ready()
-            })),
-        ]);
-    }
-
-    getFateChoice() {
-        return AbilityDsl.actions.sequential([
-            AbilityDsl.actions.gainFate(context => ({
-                target: context.player.opponent,
-                amount: 1
-            })),
-            AbilityDsl.actions.gainFate(context => ({
-                target: context.player,
-                amount: 1
-            }))
-        ]);   
-    }
-
-    getDoneChoice() {
-        return AbilityDsl.actions.noAction();
-    }
-
-    getChoices() {
-        return {
-            'Draw 1 card': AbilityDsl.actions.conditional({
-                condition: () => !this.selectedChoices.includes('draw 1 card'),
-                trueGameAction: this.getDrawChoice(),
-                falseGameAction: AbilityDsl.actions.draw( { amount: 0 })
-            }),
-            'Choose and ready a character': AbilityDsl.actions.conditional({
-                condition: () => !this.selectedChoices.includes('choose and ready a character'),
-                trueGameAction: this.getReadyChoice(),
-                falseGameAction: AbilityDsl.actions.draw( { amount: 0 })
-            }),
-            'Gain 1 fate': AbilityDsl.actions.conditional({
-                condition: () => !this.selectedChoices.includes('gain 1 fate'),
-                trueGameAction: this.getFateChoice(),
-                falseGameAction: AbilityDsl.actions.draw( { amount: 0 })
-            }),
-            'Done': this.getDoneChoice()
-        };       
-    }
-
-    getMesssage(choice) {
-        if (choice === 'draw 1 card') {
-            return 'have each player draw 1 card';
+            })).resolve(context.player, context);
         }
-        if (choice === 'choose and ready a character') {
-            return 'have each player choose and ready a character';
-        }
-        if (choice === 'gain 1 fate') {
-            return 'have each player gain 1 fate';
-        }
-        return 'do nothing';
+    }
+
+    getFateChoice(context) {
+        this.game.addMessage('{0} chooses to have each player gain a fate', context.player.opponent);
+
+        GameActions.gainFate(context => ({
+            target: context.player.opponent,
+            amount: 1
+        })).resolve(context.player.opponent, context);
+        GameActions.gainFate(context => ({
+            target: context.player,
+            amount: 1
+        })).resolve(context.player, context);
+    }
+
+    getDoneChoice(context) {
+        this.game.addMessage('{0} chooses not to do an action', context.player.opponent);
+        return true;
     }
 }
 
--- test/server/cards/13-CW/NegotiationTable.spec.js
+++ test/server/cards/13-CW/NegotiationTable.spec.js
@@ -31,103 +31,201 @@ describe('Negotiation Table', function() {
             this.challenger.bowed = true;
         });
 
-        // it('should prompt opponent to make a choice', function() {
-        //     this.player1.clickCard(this.table1);
-        //     expect(this.player2).toHavePrompt('Negotiation Table');
-        //     expect(this.player2).toHavePromptButton('Draw 1 card');
-        //     expect(this.player2).toHavePromptButton('Choose and ready a character');
-        //     expect(this.player2).toHavePromptButton('Gain 1 fate');
-        //     expect(this.player2).toHavePromptButton('Done');
-        // });
-
-        // it('should prompt opponent to make a choice', function() {
-        //     this.player1.pass();
-        //     this.player2.clickCard(this.table2);
-        //     expect(this.player1).toHavePrompt('Negotiation Table');
-        //     expect(this.player1).toHavePromptButton('Draw 1 card');
-        //     expect(this.player1).toHavePromptButton('Choose and ready a character');
-        //     expect(this.player1).toHavePromptButton('Gain 1 fate');
-        //     expect(this.player1).toHavePromptButton('Done');
-        // });
-
-        // it('should do choice - drawing cards', function() {
-        //     let p1hand = this.player1.hand.length;
-        //     let p2hand = this.player2.hand.length;
-        //     let p1fate = this.player1.fate;
-        //     let p2fate = this.player2.fate;
-        //     this.player1.clickCard(this.table1);
-        //     this.player2.clickPrompt('Draw 1 card');
-        //     expect(this.player1.hand.length).toBe(p1hand + 1);
-        //     expect(this.player2.hand.length).toBe(p2hand + 1);
-        //     expect(this.player1.fate).toBe(p1fate);
-        //     expect(this.player2.fate).toBe(p2fate);
-        //     expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table to have each player draw 1 card');
-        // });
-
-        // it('should do choice - gaining fate', function() {
-        //     let p1hand = this.player1.hand.length;
-        //     let p2hand = this.player2.hand.length;
-        //     let p1fate = this.player1.fate;
-        //     let p2fate = this.player2.fate;
-        //     this.player1.clickCard(this.table1);
-        //     this.player2.clickPrompt('Gain 1 fate');
-        //     expect(this.player1.fate).toBe(p1fate + 1);
-        //     expect(this.player2.fate).toBe(p2fate + 1);
-        //     expect(this.player1.hand.length).toBe(p1hand);
-        //     expect(this.player2.hand.length).toBe(p2hand);
-        //     expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table to have each player gain 1 fate');
-        // });
-
-        // it('should do choice - readying a character', function() {
-        //     this.player1.clickCard(this.table1);
-        //     this.player2.clickPrompt('Choose and ready a character');
-        //     expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table to have each player choose and ready a character');
-        //     expect(this.player2).toHavePrompt('Choose a character');
-        //     expect(this.player2).toBeAbleToSelect(this.yoshi);
-        //     expect(this.player2).toBeAbleToSelect(this.fumiki);
-        //     expect(this.player2).toBeAbleToSelect(this.challenger);
-        //     expect(this.player2).not.toBeAbleToSelect(this.keepsakes);
-        //     expect(this.player2).not.toBeAbleToSelect(this.dojiWhisperer);
-
-        //     this.player2.clickCard(this.yoshi);
-        //     expect(this.getChatLogs(1)).toContain('player2 chooses to ready Kakita Yoshi');
-        //     expect(this.yoshi.bowed).toBe(false);
-
-        //     expect(this.player1).toHavePrompt('Choose a character');
-        //     expect(this.player1).not.toBeAbleToSelect(this.yoshi);
-        //     expect(this.player1).toBeAbleToSelect(this.fumiki);
-        //     expect(this.player1).toBeAbleToSelect(this.challenger);
-        //     expect(this.player1).not.toBeAbleToSelect(this.keepsakes);
-        //     expect(this.player1).not.toBeAbleToSelect(this.dojiWhisperer);
-        //     this.player1.clickCard(this.fumiki);
-        //     expect(this.getChatLogs(1)).toContain('player1 chooses to ready Doji Fumiki');
-        //     expect(this.fumiki.bowed).toBe(false);
-        // });
-
-        // it('should do choice - do nothing', function() {
-        //     let p1hand = this.player1.hand.length;
-        //     let p2hand = this.player2.hand.length;
-        //     let p1fate = this.player1.fate;
-        //     let p2fate = this.player2.fate;
-        //     this.player1.clickCard(this.table1);
-        //     this.player2.clickPrompt('Done');
-        //     expect(this.player1.fate).toBe(p1fate);
-        //     expect(this.player2.fate).toBe(p2fate);
-        //     expect(this.player1.hand.length).toBe(p1hand);
-        //     expect(this.player2.hand.length).toBe(p2hand);
-        //     expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table to do nothing');
-        // });
-
-        it('chaining cases - should chain cases', function() {
+        it('should prompt opponent to make a choice', function() {
             this.player1.clickCard(this.table1);
-            this.player2.clickPrompt('Draw 1 card');
-            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table to have each player draw 1 card');
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).toHavePromptButton('Draw 1 card');
+            expect(this.player2).toHavePromptButton('Choose and ready a character');
+            expect(this.player2).toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+        });
 
+        it('should prompt opponent to make a choice', function() {
+            this.player1.pass();
+            this.player2.clickCard(this.table2);
             expect(this.player1).toHavePrompt('Negotiation Table');
-            expect(this.player1).not.toHavePromptButton('Draw 1 card');
+            expect(this.player1).toHavePromptButton('Draw 1 card');
             expect(this.player1).toHavePromptButton('Choose and ready a character');
             expect(this.player1).toHavePromptButton('Gain 1 fate');
             expect(this.player1).toHavePromptButton('Done');
+        });
+
+        it('should do choice - drawing cards', function() {
+            let p1hand = this.player1.hand.length;
+            let p2hand = this.player2.hand.length;
+            let p1fate = this.player1.fate;
+            let p2fate = this.player2.fate;
+            this.player1.clickCard(this.table1);
+            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table');
+            this.player2.clickPrompt('Draw 1 card');
+            expect(this.player1.hand.length).toBe(p1hand + 1);
+            expect(this.player2.hand.length).toBe(p2hand + 1);
+            expect(this.player1.fate).toBe(p1fate);
+            expect(this.player2.fate).toBe(p2fate);
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player draw a card');
+        });
+
+        it('should do choice - gaining fate', function() {
+            let p1hand = this.player1.hand.length;
+            let p2hand = this.player2.hand.length;
+            let p1fate = this.player1.fate;
+            let p2fate = this.player2.fate;
+            this.player1.clickCard(this.table1);
+            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table');
+            this.player2.clickPrompt('Gain 1 fate');
+            expect(this.player1.fate).toBe(p1fate + 1);
+            expect(this.player2.fate).toBe(p2fate + 1);
+            expect(this.player1.hand.length).toBe(p1hand);
+            expect(this.player2.hand.length).toBe(p2hand);
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player gain a fate');
+        });
+
+        it('should do choice - readying a character', function() {
+            this.player1.clickCard(this.table1);
+            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table');
+            this.player2.clickPrompt('Choose and ready a character');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player ready a character');
+            expect(this.player2).toHavePrompt('Choose a character');
+            expect(this.player2).toBeAbleToSelect(this.yoshi);
+            expect(this.player2).toBeAbleToSelect(this.fumiki);
+            expect(this.player2).toBeAbleToSelect(this.challenger);
+            expect(this.player2).not.toBeAbleToSelect(this.keepsakes);
+            expect(this.player2).not.toBeAbleToSelect(this.dojiWhisperer);
+
+            this.player2.clickCard(this.yoshi);
+            expect(this.getChatLogs(1)).toContain('player2 chooses to ready Kakita Yoshi');
+            expect(this.yoshi.bowed).toBe(false);
+
+            expect(this.player1).toHavePrompt('Choose a character');
+            expect(this.player1).not.toBeAbleToSelect(this.yoshi);
+            expect(this.player1).toBeAbleToSelect(this.fumiki);
+            expect(this.player1).toBeAbleToSelect(this.challenger);
+            expect(this.player1).not.toBeAbleToSelect(this.keepsakes);
+            expect(this.player1).not.toBeAbleToSelect(this.dojiWhisperer);
+            this.player1.clickCard(this.fumiki);
+            expect(this.getChatLogs(1)).toContain('player1 chooses to ready Doji Fumiki');
+            expect(this.fumiki.bowed).toBe(false);
+        });
+
+        it('should do choice - done', function() {
+            let p1hand = this.player1.hand.length;
+            let p2hand = this.player2.hand.length;
+            let p1fate = this.player1.fate;
+            let p2fate = this.player2.fate;
+            this.player1.clickCard(this.table1);
+            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table');
+            this.player2.clickPrompt('Done');
+            expect(this.player1.fate).toBe(p1fate);
+            expect(this.player2.fate).toBe(p2fate);
+            expect(this.player1.hand.length).toBe(p1hand);
+            expect(this.player2.hand.length).toBe(p2hand);
+            expect(this.getChatLogs(1)).toContain('player2 chooses not to do an action');
+        });
+
+        it('chaining cases - should not let you pick the same case', function() {
+            this.player1.clickCard(this.table1);
+            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table');
+            this.player2.clickPrompt('Draw 1 card');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player draw a card');
+
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).not.toHavePromptButton('Draw 1 card');
+            expect(this.player2).toHavePromptButton('Choose and ready a character');
+            expect(this.player2).toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+        });
+
+        it('chaining cases - chaining all cases', function() {
+            this.player1.clickCard(this.table1);
+            expect(this.getChatLogs(1)).toContain('player1 uses Negotiation Table');
+            this.player2.clickPrompt('Draw 1 card');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player draw a card');
+
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).not.toHavePromptButton('Draw 1 card');
+            expect(this.player2).toHavePromptButton('Choose and ready a character');
+            expect(this.player2).toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+
+            this.player2.clickPrompt('Gain 1 fate');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player gain a fate');
+
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).not.toHavePromptButton('Draw 1 card');
+            expect(this.player2).toHavePromptButton('Choose and ready a character');
+            expect(this.player2).not.toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+
+            this.player2.clickPrompt('Choose and ready a character');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player ready a character');
+            expect(this.player2).toHavePrompt('Choose a character');
+            expect(this.player2).toBeAbleToSelect(this.yoshi);
+            expect(this.player2).toBeAbleToSelect(this.fumiki);
+            expect(this.player2).toBeAbleToSelect(this.challenger);
+            expect(this.player2).not.toBeAbleToSelect(this.keepsakes);
+            expect(this.player2).not.toBeAbleToSelect(this.dojiWhisperer);
+
+            this.player2.clickCard(this.yoshi);
+            expect(this.getChatLogs(1)).toContain('player2 chooses to ready Kakita Yoshi');
+            expect(this.yoshi.bowed).toBe(false);
+
+            expect(this.player1).toHavePrompt('Choose a character');
+            expect(this.player1).not.toBeAbleToSelect(this.yoshi);
+            expect(this.player1).toBeAbleToSelect(this.fumiki);
+            expect(this.player1).toBeAbleToSelect(this.challenger);
+            expect(this.player1).not.toBeAbleToSelect(this.keepsakes);
+            expect(this.player1).not.toBeAbleToSelect(this.dojiWhisperer);
+            this.player1.clickCard(this.fumiki);
+            expect(this.getChatLogs(1)).toContain('player1 chooses to ready Doji Fumiki');
+            expect(this.fumiki.bowed).toBe(false);
+
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).not.toHavePromptButton('Draw 1 card');
+            expect(this.player2).not.toHavePromptButton('Choose and ready a character');
+            expect(this.player2).not.toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+
+            this.player2.clickPrompt('Done');
+            expect(this.getChatLogs(1)).toContain('player2 chooses not to do an action');
+        });
+
+        it('should not make you choose a character to ready if there are no bowed characters', function() {
+            this.yoshi.bowed = false;
+            this.fumiki.bowed = false;
+            this.challenger.bowed = false;
+            this.game.checkGameState(true);
+            this.player1.clickCard(this.table1);
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).toHavePromptButton('Draw 1 card');
+            expect(this.player2).toHavePromptButton('Choose and ready a character');
+            expect(this.player2).toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+
+            this.player2.clickPrompt('Choose and ready a character');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player ready a character');
+            expect(this.player2).not.toHavePrompt('Choose a character');
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).not.toHavePromptButton('Choose and ready a character');
+        });
+
+        it('should be able to ready the only bowed character and then not lock up the other player', function() {
+            this.fumiki.bowed = false;
+            this.challenger.bowed = false;
+            this.game.checkGameState(true);
+            this.player1.clickCard(this.table1);
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).toHavePromptButton('Draw 1 card');
+            expect(this.player2).toHavePromptButton('Choose and ready a character');
+            expect(this.player2).toHavePromptButton('Gain 1 fate');
+            expect(this.player2).toHavePromptButton('Done');
+
+            this.player2.clickPrompt('Choose and ready a character');
+            expect(this.getChatLogs(1)).toContain('player2 chooses to have each player ready a character');
+            expect(this.player2).toHavePrompt('Choose a character');
+            expect(this.player2).toBeAbleToSelect(this.yoshi);
+            this.player2.clickCard(this.yoshi);
+            expect(this.player2).toHavePrompt('Negotiation Table');
+            expect(this.player2).not.toHavePromptButton('Choose and ready a character');
         });
     });
 });
