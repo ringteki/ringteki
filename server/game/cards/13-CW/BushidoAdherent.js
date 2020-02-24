affbb5a02c99ede523e27fad3bc7a9c6bda64ef4
Marc Soiferman <t>
2020-02-24 00:50:50 -0600

    Bushido Adherent

---
+++ server/game/cards/13-CW/BushidoAdherent.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class BushidoAdherent extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Honor a character',
+            condition: context => context.source.isParticipating(),
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: card => card.isParticipating(),
+                gameAction: AbilityDsl.actions.honor()
+            },
+            then: {
+                gameAction: AbilityDsl.actions.draw(context => ({ target: context.player.opponent }))
+            },
+            effect: 'honor {0} and have {1} draw 1 card',
+            effectArgs: context => context.player.opponent
+        });
+    }
+}
+
+BushidoAdherent.id = 'bushido-adherent';
+
+module.exports = BushidoAdherent;
