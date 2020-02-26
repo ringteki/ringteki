7437ce98abfdfab23c729c69d38db84d36c7dd78
Marc Soiferman <t>
2020-02-25 23:34:37 -0600

    Medium of the Living Soul

---
+++ server/game/cards/13-CW/MediumOfTheLivingSoul.js
@@ -0,0 +1,35 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes, Players, AbilityTypes } = require('../../Constants');
+
+class MediumOfTheLivingSoul extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Grant an ability to resolve ring effects',
+            condition: context => context.game.isDuringConflict(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Any,
+                cardCondition: card => card.isParticipating(),
+                gameAction: AbilityDsl.actions.cardLastingEffect(() => ({
+                    effect: AbilityDsl.effects.gainAbility(AbilityTypes.Reaction, {
+                        title: 'Resolve the Ring Effect',
+                        when: {
+                            onResolveRingElement: (event, context) => {
+                                let val = event.player === context.player && context.source.isParticipating();
+                                return val;
+                            }},
+                        cost: AbilityDsl.costs.removeFateFromSelf(),
+                        gameAction: AbilityDsl.actions.resolveRingEffect(context => ({ target: context.event.ring }))
+                    })
+                }))
+            },
+            effect: 'give {0} the ability to resolve a ring effect'
+        });
+    }
+}
+
+MediumOfTheLivingSoul.id = 'medium-of-the-living-soul';
+
+module.exports = MediumOfTheLivingSoul;
+
