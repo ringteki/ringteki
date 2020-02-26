85deacffb8cbda25bbacce9ba134df869aeb3053
Marc Soiferman <t>
2020-02-25 23:56:17 -0600

    The easy ones

---
+++ test/server/cards/13-CW/CunningConfidant.spec.js
@@ -0,0 +1,69 @@
+describe('A Game of Letters', function() {
+    integration(function() {
+        beforeEach(function() {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    inPlay: ['cunning-confidant', 'doji-fumiki']
+                },
+                player2: {
+                }
+            });
+
+            this.confidant = this.player1.findCardByName('cunning-confidant');
+            this.fumiki = this.player1.findCardByName('doji-fumiki');
+        });
+
+        it('should not work outside of conflicts', function() {
+            this.player2.claimRing('void');
+            expect(this.confidant.getPoliticalSkill()).toBe(this.confidant.getBasePoliticalSkill());
+        });
+
+        it('should give +2 pol when participating when opponent has more rings', function() {
+            this.player2.claimRing('void');
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.confidant],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.confidant.getPoliticalSkill()).toBe(this.confidant.getBasePoliticalSkill() + 2);
+        });
+
+        it('should not give +2 pol when participating when opponent has equal rings', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.confidant],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.confidant.getPoliticalSkill()).toBe(this.confidant.getBasePoliticalSkill());
+        });
+
+        it('should not give +2 pol when participating when you have more rings', function() {
+            this.player1.claimRing('void');
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.confidant],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.confidant.getPoliticalSkill()).toBe(this.confidant.getBasePoliticalSkill());
+        });
+
+        it('should give +2 pol when not participating', function() {
+            this.player2.claimRing('void');
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.fumiki],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.confidant.getPoliticalSkill()).toBe(this.confidant.getBasePoliticalSkill());
+        });
+    });
+});
---
+++ test/server/cards/13-CW/InsightfulGatekeeper.spec.js
@@ -0,0 +1,69 @@
+describe('A Game of Letters', function() {
+    integration(function() {
+        beforeEach(function() {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    inPlay: ['insightful-gatekeeper', 'doji-fumiki']
+                },
+                player2: {
+                }
+            });
+
+            this.gatekeeper = this.player1.findCardByName('insightful-gatekeeper');
+            this.fumiki = this.player1.findCardByName('doji-fumiki');
+        });
+
+        it('should not work outside of conflicts', function() {
+            this.player2.claimRing('void');
+            expect(this.gatekeeper.getMilitarySkill()).toBe(this.gatekeeper.getBaseMilitarySkill());
+        });
+
+        it('should give +2 mil when participating when opponent has more rings', function() {
+            this.player2.claimRing('void');
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.gatekeeper],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.gatekeeper.getMilitarySkill()).toBe(this.gatekeeper.getBaseMilitarySkill() + 2);
+        });
+
+        it('should not give +2 mil when participating when opponent has equal rings', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.gatekeeper],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.gatekeeper.getMilitarySkill()).toBe(this.gatekeeper.getBaseMilitarySkill());
+        });
+
+        it('should not give +2 mil when participating when you have more rings', function() {
+            this.player1.claimRing('void');
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.gatekeeper],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.gatekeeper.getMilitarySkill()).toBe(this.gatekeeper.getBaseMilitarySkill());
+        });
+
+        it('should give +2 mil when not participating', function() {
+            this.player2.claimRing('void');
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.fumiki],
+                defenders: [],
+                type: 'military'
+            });
+
+            expect(this.gatekeeper.getMilitarySkill()).toBe(this.gatekeeper.getBaseMilitarySkill());
+        });
+    });
+});
