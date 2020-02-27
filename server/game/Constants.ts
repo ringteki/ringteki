10c73db720829c724201438e8c694228929ab582
Marc Soiferman <t>
2020-02-26 23:20:43 -0600

    Contemplative Wisdom

--- server/game/Constants.ts
+++ server/game/Constants.ts
@@ -59,6 +59,7 @@ export enum EffectNames {
     FateCostToAttack = 'fateCostToAttack',
     FateCostToTarget = 'fateCostToTarget',
     GainAbility = 'gainAbility',
+    GainAllAbilities = 'gainAllAbilities',
     GainExtraFateWhenPlayed = 'gainExtraFateWhenPlayed',
     GainPlayAction = 'gainPlayAction',
     HideWhenFaceUp = 'hideWhenFaceUp',
