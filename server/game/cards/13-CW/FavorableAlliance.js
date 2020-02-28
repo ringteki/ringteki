25336daa7f97a58fea7432557186349c344f40bb
Marc Soiferman <t>
2020-02-28 00:42:13 -0600

    Working with RFG.  Going to commit now, then clean it up

--- server/game/cards/13-CW/FavorableAlliance.js
+++ server/game/cards/13-CW/FavorableAlliance.js
@@ -1,7 +1,10 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl.js');
+const { Players, Durations, Locations, Decks } = require('../../Constants');
 
 class FavorableAlliance extends DrawCard {
+    PILENAME = 'favorable-alliance';
+
     setupCardAbilities() {
         this.action({
             title: 'Draw cards',
@@ -10,9 +13,62 @@ class FavorableAlliance extends DrawCard {
                 maxAmount: (context) => context.player.conflictDeck.size(),
                 activePromptTitle: 'Choose a value for X'
             }),
-            effect: 'draw {1} cards',
-            effectArgs: context => context.costs.variableFateCost,
-            gameAction: AbilityDsl.actions.draw(context => ({ amount: context.costs.variableFateCost }))
+            effect: 'set aside {1} card{2}',
+            effectArgs: context => [context.costs.variableFateCost, context.costs.variableFateCost > 1 ? 's' : ''],
+            // gameAction: AbilityDsl.actions.draw(context => ({ amount: context.costs.variableFateCost }))
+            gameAction: AbilityDsl.actions.multiple([
+                AbilityDsl.actions.lookAt(context=> ({
+                    target: context.player.conflictDeck.first(context.costs.variableFateCost),
+                    message: '{0} sets aside the top {1} card{3} from their conflict deck: {2}',
+                    messageArgs: cards => [context.player, cards.length, cards, cards.length > 1 ? 's' : '']
+                })),
+                AbilityDsl.actions.handler({
+                    handler: context => {
+                        let cards = context.player.conflictDeck.first(context.costs.variableFateCost);
+                        cards.forEach(card => {
+                            card.owner.removeCardFromPile(card);
+                            card.moveTo(Locations.RemovedFromGame);
+                            context.player.removedFromGame.unshift(card);
+                            context.source.lastingEffect(() => ({
+                                until: {
+                                    onCardMoved: event => event.card === card && event.originalLocation === Locations.RemovedFromGame
+                                },
+                                match: card,
+                                effect: [
+                                    AbilityDsl.effects.canPlayFromOwn(Locations.RemovedFromGame, [card])
+                                ]
+                            }));    
+                        })
+                    }
+                })
+                // AbilityDsl.actions.playerLastingEffect(context => ({
+                //     targetController: Players.Self,
+                //     duration: Durations.Custom,
+                //     until: {
+                //         onCardMoved: event => {
+                //             let favorableCard = context.player.additionalPiles[this.PILENAME].contains(event.card) && event.originalLocation === Locations.RemovedFromGame;
+                //             if (favorableCard) {
+                //                 context.player.additionalPiles[this.PILENAME].splice(context.player.additionalPiles[this.PILENAME].indexOf(event.card), 1);
+                //             }
+                //             return favorableCard;
+                //         }
+                //     },
+                //     effect: [
+                //         AbilityDsl.effects.canPlayFromOwn(Locations.RemovedFromGame, context.player.conflictDeck.first(context.costs.variableFateCost)),
+                //     ]
+                // })),
+                // AbilityDsl.actions.moveCard(context => {
+                //     let cards = context.player.conflictDeck.first(context.costs.variableFateCost);
+                //     if(!(this.PILENAME in context.player.additionalPiles)) {
+                //         context.player.createAdditionalPile(this.PILENAME);
+                //     }
+                //     context.player.additionalPiles[this.PILENAME].cards = context.player.additionalPiles[this.PILENAME].cards.concat(cards);
+                //     return {
+                //         target: cards,
+                //         destination: Locations.RemovedFromGame
+                //     }
+                // })
+            ])
         });
     }
 }
--- test/server/cards/13-CW/FavorableAlliance.spec.js
+++ test/server/cards/13-CW/FavorableAlliance.spec.js
@@ -51,12 +51,13 @@ describe('Favorable Alliance', function() {
             expect(this.player1).toHavePromptButton('3');
             expect(this.player1).toHavePromptButton('4');
             this.player1.clickPrompt('3');
-            expect(this.voice.location).toBe('hand');
-            expect(this.katana.location).toBe('hand');
-            expect(this.fashion.location).toBe('hand');
+            expect(this.voice.location).toBe('removed from game');
+            expect(this.katana.location).toBe('removed from game');
+            expect(this.fashion.location).toBe('removed from game');
 
-            expect(this.player1.hand.length).toBe(3);
-            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to draw 3 cards');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to set aside 3 cards');
+            expect(this.getChatLogs(10)).toContain('player1 sets aside the top 3 cards from their conflict deck: Voice of Honor, Fine Katana and Height of Fashion');
             expect(this.player1.fate).toBe(fate - 3);
         });
 
@@ -91,12 +92,13 @@ describe('Favorable Alliance', function() {
             expect(this.player1).toHavePromptButton('3');
             expect(this.player1).toHavePromptButton('4');
             this.player1.clickPrompt('3');
-            expect(this.voice.location).toBe('hand');
-            expect(this.katana.location).toBe('hand');
-            expect(this.fashion.location).toBe('hand');
+            expect(this.voice.location).toBe('removed from game');
+            expect(this.katana.location).toBe('removed from game');
+            expect(this.fashion.location).toBe('removed from game');
 
-            expect(this.player1.hand.length).toBe(3);
-            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to draw 3 cards');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to set aside 3 cards');
+            expect(this.getChatLogs(10)).toContain('player1 sets aside the top 3 cards from their conflict deck: Voice of Honor, Fine Katana and Height of Fashion');
             expect(this.player1.fate).toBe(fate - 1);
         });
 
@@ -135,11 +137,12 @@ describe('Favorable Alliance', function() {
             expect(this.player1).not.toHavePromptButton('3');
             expect(this.player1).not.toHavePromptButton('4');
             this.player1.clickPrompt('2');
-            expect(this.voice.location).toBe('hand');
-            expect(this.katana.location).toBe('hand');
+            expect(this.voice.location).toBe('removed from game');
+            expect(this.katana.location).toBe('removed from game');
 
-            expect(this.player1.hand.length).toBe(2);
-            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to draw 2 cards');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to set aside 2 cards');
+            expect(this.getChatLogs(10)).toContain('player1 sets aside the top 2 cards from their conflict deck: Voice of Honor and Fine Katana');
             expect(this.player1.fate).toBe(fate);
         });
 
@@ -163,9 +166,10 @@ describe('Favorable Alliance', function() {
             expect(this.player1).not.toHavePromptButton('3');
             expect(this.player1).not.toHavePromptButton('4');
             this.player1.clickPrompt('1');
-            expect(this.voice.location).toBe('hand');
-            expect(this.player1.hand.length).toBe(1);
-            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to draw 1 cards');
+            expect(this.voice.location).toBe('removed from game');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to set aside 1 card');
+            expect(this.getChatLogs(10)).toContain('player1 sets aside the top 1 card from their conflict deck: Voice of Honor');
             expect(this.player1.fate).toBe(fate - 2);
         });
 
@@ -200,8 +204,8 @@ describe('Favorable Alliance', function() {
             expect(this.player1).not.toHavePromptButton('7');
             expect(this.player1).not.toHavePromptButton('8');
             this.player1.clickPrompt('6');
-            expect(this.player1.hand.length).toBe(6);
-            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to draw 6 cards');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to set aside 6 cards');
             expect(this.player1.fate).toBe(fate - 6);
         });
 
@@ -240,9 +244,102 @@ describe('Favorable Alliance', function() {
             expect(this.player1).not.toHavePromptButton('7');
             expect(this.player1).not.toHavePromptButton('8');
             this.player1.clickPrompt('6');
-            expect(this.player1.hand.length).toBe(6);
-            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to draw 6 cards');
+            expect(this.player1.hand.length).toBe(0);
+            expect(this.getChatLogs(10)).toContain('player1 plays Favorable Alliance to set aside 6 cards');
             expect(this.player1.fate).toBe(fate - 4);
+        });
+
+        describe('Playing cards from RFG', function() {
+            beforeEach(function() {
+                this.player1.fate = 10;
+                this.player1.player.imperialFavor = 'military';
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.yoshi],
+                    defenders: [],
+                    type: 'military',
+                    province: this.garden
+                });
+
+                this.tetsuko.bowed = true;
+                this.yoshi.fate = 1;
+                this.yoshi.honor();
+                this.player1.moveCard(this.crane, 'removed from game');
+                this.player2.pass();
+                this.player1.clickCard(this.alliance);
+                this.player1.clickPrompt('3');
+            });
+
+            it('should not be able to play cards removed from the game by other means', function() {
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.crane);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should allow playing cards from RFG that were put there by Favorable Alliance', function() {
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.katana);
+                expect(this.player1).toHavePrompt('Choose a card');
+                this.player1.clickCard(this.yoshi);
+                expect(this.yoshi.attachments.toArray()).toContain(this.katana);
+            });
+
+            it('should not allow re-playing cards that get moved back into RFG after being played', function() {
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.katana);
+                expect(this.player1).toHavePrompt('Choose a card');
+                this.player1.clickCard(this.yoshi);
+                expect(this.yoshi.attachments.toArray()).toContain(this.katana);
+                this.player1.moveCard(this.katana, 'removed from game');
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.katana);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should allow triggering reactions', function() {
+                this.player2.clickCard(this.fury);
+                this.player2.clickCard(this.yoshi);
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.voice);
+                expect(this.voice.location).toBe('removed from game');
+                this.player1.clickCard(this.voice);
+                expect(this.voice.location).toBe('conflict discard pile');
+                expect(this.yoshi.bowed).toBe(false);
+            });
+
+            it('should still let you play cards from RFG next turn', function() {
+                this.noMoreActions();
+                this.player1.clickPrompt('No');
+                this.player1.clickPrompt('Don\'t Resolve');
+
+                this.noMoreActions();
+                this.noMoreActions();
+                this.noMoreActions();
+                this.noMoreActions();
+                this.player1.clickPrompt('Military');
+                this.player2.clickPrompt('Done');
+                this.player1.clickPrompt('Done');
+                this.player2.clickPrompt('Done');
+                this.player2.clickPrompt('End Round');
+                this.player1.clickPrompt('End Round');
+
+                //New round!
+                this.player2.pass();
+                this.player1.pass();
+
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+
+                this.player2.pass();
+                expect(this.katana.location).toBe('removed from game');
+                this.player1.clickCard(this.katana);
+                this.player1.clickCard(this.yoshi);
+                expect(this.yoshi.attachments.toArray()).toContain(this.katana);
+            });
         });
     });
 });
