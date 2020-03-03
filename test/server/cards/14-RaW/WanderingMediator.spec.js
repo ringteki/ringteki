50c079c3c26ce59567beeafb7a13fccb650561a3
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2020-03-04 00:32:55 +0100

    RaW - Wandering Mediator

---
+++ test/server/cards/14-RaW/WanderingMediator.spec.js
@@ -0,0 +1,81 @@
+describe('Wandering Mediator', function() {
+    integration(function() {
+        describe('Wandering Mediator\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['wandering-mediator', 'valiant-oathkeeper']
+                    },
+                    player2: {
+                        provinces: ['dishonorable-assault', 'manicured-garden']
+                    }
+                });
+
+                this.wanderingMediator = this.player1.findCardByName('wandering-mediator');
+                this.valiantOathkeeper = this.player1.findCardByName('valiant-oathkeeper');
+
+                this.assault = this.player2.findCardByName('dishonorable-assault');
+                this.manicured = this.player2.findCardByName('manicured-garden');
+            });
+
+            it('can move it to a conflict at an air province', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.valiantOathkeeper],
+                    defenders: [],
+                    province: this.manicured
+                });
+
+                this.player2.pass();
+                this.player1.clickCard(this.wanderingMediator);
+                expect(this.wanderingMediator.isParticipating()).toBe(true);
+            });
+
+            it('can move it home from a conflict at an air province', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.wanderingMediator],
+                    defenders: [],
+                    province: this.manicured
+                });
+
+                this.player2.pass();
+                this.player1.clickCard(this.wanderingMediator);
+                expect(this.wanderingMediator.isParticipating()).toBe(false);
+            });
+
+            it('can not move it to a conflict at a non-air province', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.valiantOathkeeper],
+                    defenders: [],
+                    province: this.assault
+                });
+
+                this.player2.pass();
+                this.player1.clickCard(this.wanderingMediator);
+                expect(this.wanderingMediator.isParticipating()).toBe(false);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('can not move it home from a conflict at a non-air province', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.wanderingMediator],
+                    defenders: [],
+                    province: this.assault
+                });
+
+                this.player2.pass();
+                this.player1.clickCard(this.wanderingMediator);
+                expect(this.wanderingMediator.isParticipating()).toBe(true);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+        });
+    });
+});
