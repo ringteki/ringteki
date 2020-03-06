d026ef0a7f890ea388bf4f4a0ef1233aa34c7bd9
Marc Soiferman <t>
2020-03-06 00:54:02 -0600

    Captivating Story

---
+++ server/game/cards/14.1-RaW/CaptivatingStory.js
@@ -0,0 +1,52 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes, Players } = require('../../Constants');
+const CardAbility = require('../../CardAbility');
+
+class CaptivatingStory extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Give a character +X pol',
+            condition: context => this.game.currentConflict.getNumberOfParticipantsFor(context.source.controller) === 1,
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                cardCondition: (card, context) => card.isParticipating() && (context.player.getNumberOfFaceupProvinces() > 0 || card.fate > 0),
+                gameAction: AbilityDsl.actions.sequential([
+                    AbilityDsl.actions.cardLastingEffect(context => ({
+                        effect: AbilityDsl.effects.modifyPoliticalSkill(context.player.getNumberOfFaceupProvinces())
+                    })),
+                    AbilityDsl.actions.menuPrompt(context => ({
+                        activePromptTitle: 'Remove 1 fate from ' + context.target.name + ' to honor them?',
+                        choices: ['Yes'].concat(context.player.getNumberOfFaceupProvinces() > 0 ? ['No'] : []),
+                        choiceHandler: (choice, displayMessage) => {
+                            if(displayMessage) {
+                                context.game.addMessage('{0} chooses {1}to remove a fate from {2} to honor them', context.player, choice === 'No' ? 'not ' : '', context.target);
+                            }
+                            return { amount: choice === 'Yes' ? 1 : 0 };
+                        },
+                        gameAction: AbilityDsl.actions.joint([
+                            AbilityDsl.actions.removeFate(context => ({
+                                target: context.target
+                            })),
+                            AbilityDsl.actions.resolveAbility({
+                                target: context.source,
+                                subResolution: true,
+                                ability: new CardAbility(this.game, context.source, {
+                                    title: 'Honor this character',
+                                    gameAction: AbilityDsl.actions.honor({ target: context.target })
+                                })
+                            })
+                        ])
+                    }))
+                ])
+            },
+            effect: 'give {0} +1{1} for each faceup province they opponent controls (+{2}{1})',
+            effectArgs: context => ['political', context.player.getNumberOfFaceupProvinces()]
+        });
+    }
+}
+
+CaptivatingStory.id = 'captivating-story';
+
+module.exports = CaptivatingStory;
