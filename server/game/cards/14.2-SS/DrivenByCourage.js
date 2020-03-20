524e0a7d23807070d858302e4fc76e7f25776771
Matthew-Griffith <matthew-griffith@protonmail.com>
2020-03-20 01:43:10 -0400

    SS - Driven by Courage (#307)
    * first version of the card with test
    
    current test that are attacking driven by courage are working as expected but
    when test for another air province (toshi ranbo) is failing I am not sure why
    the province is faceup so it should be able to
    
    * now the action uses `conflictProvinceCondition`
    
    zarzuckett point out that that is field can used for province actions instead of conditon. this made the toshi ranbo test pass and a test for your opponents
    air province was added and also passes

---
+++ server/game/cards/14.2-SS/DrivenByCourage.js
@@ -0,0 +1,24 @@
+const ProvinceCard = require('../../provincecard.js');
+const { Players, CardTypes, Elements} = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class DrivenByCourage extends ProvinceCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'give target character +2/+2',
+            conflictProvinceCondition: province => province.isElement(Elements.Air),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Any,
+                cardCondition: card => card.isParticipating(),
+                gameAction: AbilityDsl.actions.cardLastingEffect(() => ({
+                    effect: AbilityDsl.effects.modifyBothSkills(2)
+                }))
+            }
+        });
+    }
+}
+
+DrivenByCourage.id = 'driven-by-courage';
+
+module.exports = DrivenByCourage;
