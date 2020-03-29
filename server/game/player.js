844ba15b7a58ecbde28a39757b300fdf13639118
Marc Soiferman <t>
2020-03-28 20:40:47 -0500

    Put a few null guards in, not sure if necessary

--- server/game/player.js
+++ server/game/player.js
@@ -1380,11 +1380,11 @@ class Player extends GameObject {
             state.stronghold = this.stronghold.getSummary(activePlayer);
         }
 
-        if(this.isTopConflictCardShown()) {
+        if(this.isTopConflictCardShown() && this.conflictDeck) {
             state.conflictDeckTopCard = this.conflictDeck.first().getSummary(activePlayer);
         }
 
-        if(this.isTopDynastyCardShown()) {
+        if(this.isTopDynastyCardShown() && this.dynastyDeck) {
             state.dynastyDeckTopCard = this.dynastyDeck.first().getSummary(activePlayer);
         }
 
