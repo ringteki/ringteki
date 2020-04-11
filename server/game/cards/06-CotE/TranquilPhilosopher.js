77db59b855a5436d1b6e9e5a02b6b868435770f6
Marc Soiferman <t>
2020-04-11 14:43:25 -0500

    Tranquil Philosopher can now target a ring without fate to gain an honour

--- server/game/cards/06-CotE/TranquilPhilosopher.js
+++ server/game/cards/06-CotE/TranquilPhilosopher.js
@@ -9,19 +9,22 @@ class TranquilPhilosopher extends DrawCard {
             target: {
                 mode: TargetModes.Ring,
                 activePromptTitle: 'Choose an unclaimed ring to move fate from',
-                ringCondition: ring => ring.isUnclaimed() && ring.fate > 0,
+                ringCondition: ring => ring.isUnclaimed(),
                 gameAction: AbilityDsl.actions.sequential([
                     AbilityDsl.actions.selectRing(context => ({
                         activePromptTitle: 'Choose an unclaimed ring to move fate to',
-                        ringCondition: ring => ring.isUnclaimed() && ring !== context.ring,
-                        message: '{0} moves a fate from {1} to {2}',
+                        ringCondition: ring => context.ring.fate > 0 && ring.isUnclaimed() && ring !== context.ring,
+                        message: '{0} moves a fate from the {1} to the {2}',
                         messageArgs: ring => [context.player, context.ring, ring],
                         gameAction: AbilityDsl.actions.placeFateOnRing({ origin: context.ring })
                     })),
                     AbilityDsl.actions.gainHonor(context => ({ target: context.player }))
                 ])
             },
-            effect: 'move 1 fate from {0} to an unclaimed ring, then gain 1 honor'
+            effect: '{1}{2}{3}',//'move 1 fate from {0} to an unclaimed ring, then gain 1 honor',
+            effectArgs: context => context.ring.fate > 0 ?
+                ['move 1 fate from the ', context.ring, ' to an unclaimed ring, then gain 1 honor'] :
+                ['gain 1 honor', '', '']
         });
     }
 }
