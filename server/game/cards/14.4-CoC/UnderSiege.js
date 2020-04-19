8b06f13ee96d9ecb9e8d40b536e5d230fd9f0b42
Marc Soiferman <t>
2020-04-18 21:15:47 -0500

    Under Siege

---
+++ server/game/cards/14.4-CoC/UnderSiege.js
@@ -0,0 +1,77 @@
+const DrawCard = require('../../drawcard.js');
+const { Locations, Durations } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class UnderSiege extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Bow a character',
+            when: {
+                onConflictDeclared: (event, context) => context.game.currentConflict && context.game.currentConflict.defendingPlayer
+            },
+            effect: 'do a bunch of stuff',
+            gameAction: AbilityDsl.actions.sequential([
+                AbilityDsl.actions.playerLastingEffect(context => ({
+                    duration: Durations.UntilEndOfRound,
+                    targetController: context.game.currentConflict.defendingPlayer,
+                    effect: [
+                        AbilityDsl.effects.delayedEffect({
+                            when: {
+                                onConflictFinished: () => true
+                            },
+                            gameAction: AbilityDsl.actions.discardCard(context => ({
+                                target: context.game.conflictRecord[context.game.conflictRecord.length - 1].attackingPlayer.opponent.hand.value()
+                            }))
+                        })
+                    ]
+                })),
+                AbilityDsl.actions.conditional(({
+                    condition: context => context.game.currentConflict.defendingPlayer.hand.size() > 0,
+                    trueGameAction: AbilityDsl.actions.sequential([
+                        AbilityDsl.actions.handler({
+                            handler: context => {
+                                let player = context.game.currentConflict.defendingPlayer;
+                                let cards = player.hand.value();
+                                this.game.addMessage('{0} sets their hand aside', player);
+                                if(cards.length > 0) {
+                                    cards.forEach(card => {
+                                        player.moveCard(card, Locations.RemovedFromGame);
+                                        card.lastingEffect(() => ({
+                                            until: {
+                                                onCardMoved: event => event.card === card && event.originalLocation === Locations.RemovedFromGame
+                                            },
+                                            match: card,
+                                            effect: [
+                                                AbilityDsl.effects.hideWhenFaceUp(),
+                                                AbilityDsl.effects.delayedEffect({
+                                                    when: {
+                                                        onConflictFinished: () => true
+                                                    },
+                                                    gameAction: AbilityDsl.actions.moveCard({
+                                                        target: card,
+                                                        destination: Locations.Hand
+                                                    })
+                                                })
+                                            ]
+                                        }));
+                                    });
+                                }
+                            }
+                        }),
+                        AbilityDsl.actions.draw(context => ({
+                            target: context.game.currentConflict.defendingPlayer,
+                            amount: 5
+                        }))
+                    ]),
+                    falseGameAction: AbilityDsl.actions.handler({
+                        handler: () => {}
+                    })
+                }))
+            ])
+        });
+    }
+}
+
+UnderSiege.id = 'under-siege';
+
+module.exports = UnderSiege;
