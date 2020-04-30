31440965db8725d5b034ce4815408a0f699f6bf7
Marc Soiferman <t>
2020-04-29 23:12:23 -0500

    Linting cleanup

--- server/game/provincecard.js
+++ server/game/provincecard.js
@@ -135,11 +135,11 @@ class ProvinceCard extends BaseCard {
     getMenu() {
         let menu = super.getMenu();
 
-        if (menu) {
+        if(menu) {
             if(this.game.isDuringConflict() && !this.isConflictProvince() && this.canBeAttacked() && this.controller === this.game.currentConflict.conflictProvince.controller) {
                 menu.push({ command: 'move_conflict', text: 'Move Conflict'});
             }
-            
+
             if(this.controller.getDynastyCardsInProvince(this.location).length <= 0) {
                 menu.push({ command: 'refill', text: 'Refill Province'});
             }
