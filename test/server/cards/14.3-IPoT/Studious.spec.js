9275c39de2843214f9066346b1b579d9b3c944e6
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2020-05-13 02:21:32 +0200

    updated with check for increased hand size

--- test/server/cards/14.3-IPoT/Studious.spec.js
+++ test/server/cards/14.3-IPoT/Studious.spec.js
@@ -38,6 +38,7 @@ describe('Studious', function() {
             this.player2.pass();
             this.player1.pass();
 
+            this.initialHandSize = this.player1.hand.length;
             this.initiateConflict({
                 type: 'political',
                 attackers: [this.historian],
@@ -52,6 +53,7 @@ describe('Studious', function() {
 
             this.player1.clickCard(this.historian);
             expect(this.getChatLogs(10)).toContain('player1 uses Venerable Historian\'s gained ability from Studious to draw 1 card');
+            expect(this.player1.hand.length).toBe(this.initialHandSize + 1);
         });
 
         it('should not react on the character losing', function () {
@@ -82,6 +84,7 @@ describe('Studious', function() {
             this.player2.pass();
             this.player1.pass();
 
+            this.initialHandSize = this.player1.hand.length;
             this.initiateConflict({
                 type: 'political',
                 attackers: [this.historian],
@@ -91,6 +94,7 @@ describe('Studious', function() {
             this.player2.clickCard(this.assassination);
             this.player2.clickCard(this.historian);
             expect(this.getChatLogs(10)).toContain('player1 draws a card due to Venerable Historian\'s Sincerity');
+            expect(this.player1.hand.length).toBe(this.initialHandSize + 1);
         });
     });
 });
