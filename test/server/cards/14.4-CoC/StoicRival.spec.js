f19f0d3b6c45507352adb0dedcb1fa0591893d1b
lancerusso <66386076+lancerusso@users.noreply.github.com>
2020-06-06 17:18:55 +0100

    Update StoicRival.spec.js
    Assign Stoic Rival to own object as searching for cardname won't work for non-controlling players?

--- test/server/cards/14.4-CoC/StoicRival.spec.js
+++ test/server/cards/14.4-CoC/StoicRival.spec.js
@@ -22,6 +22,7 @@ describe('Stoic Rival', function() {
                 this.otomoCourtier = this.player2.findCardByName('otomo-courtier');
                 this.shibaTsukune = this.player2.findCardByName('shiba-tsukune');
                 this.miyaMystic = this.player2.findCardByName('miya-mystic');
+                this.stoicRival = this.player1.findCardByName('stoic-rival');
                 this.player2.playAttachment('ornate-fan','shiba-tsukune');
                 this.player1.playAttachment('vine-tattoo','stoic-rival');
             });
@@ -29,37 +30,37 @@ describe('Stoic Rival', function() {
 
             it('should allow bowing cards with fewer attachments', function() {
                 this.player2.pass();
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.stoicRival = this.player1.clickCard(this.stoicRival);
                 expect(this.player1).toHavePrompt('Stoic Rival');
                 expect(this.player1).toBeAbleToSelect(this.otomoCourtier);
             });
 
             it('no bowing cards with same number of attachments', function() {
                 this.player2.pass();
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.stoicRival = this.player1.clickCard(this.stoicRival);
                 expect(this.player1).toHavePrompt('Stoic Rival');
                 expect(this.player1).not.toBeAbleToSelect(this.shibaTsukune);
             });
 
             it('no bowing cards with more attachments', function() {
                 this.player2.playAttachment('ornate-fan','shiba-tsukune');
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.stoicRival = this.player1.clickCard(this.stoicRival);
                 expect(this.player1).toHavePrompt('Stoic Rival');
                 expect(this.player1).not.toBeAbleToSelect(this.shibaTsukune);
             });
 
             it('should allow allow bowing cards with an attachment but still fewer', function() {
                 this.player2.pass();
-                this.player1.playAttachment('ornate-fan', 'stoic-rival');
+                this.player1.playAttachment('ornate-fan', this.stoicRival);
                 this.player2.pass();
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.player1.clickCard(this.stoicRival);
                 expect(this.player1).toHavePrompt('Stoic Rival');
                 expect(this.player1).toBeAbleToSelect(this.shibaTsukune);
             });
 
             it('should dishonor the target', function() {
                 this.player2.pass();
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.player1.clickCard(this.stoicRival);
                 this.player1.clickCard(this.otomoCourtier);
                 expect(this.otomoCourtier.isDishonored).toBe(true);
                 expect(this.otomoCourtier.inConflict).toBe(true);
@@ -67,15 +68,16 @@ describe('Stoic Rival', function() {
 
             it('should not be able to target characters not participating', function() {
                 this.player2.pass();
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.player1.clickCard(this.stoicRival);
                 expect(this.player1).toHavePrompt('Stoic Rival');
                 expect(this.player1).not.toBeAbleToSelect(this.miyaMystic);
             });
 
             it('should not be able to use while not participating', function() {
                 this.player2.clickCard('outwit');
-                this.player2.clickCard('stoic-rival');
-                this.stoicRival = this.player1.clickCard('stoic-rival');
+                this.player2.clickCard(this.stoicRival);
+                expect(this.stoicRival.inConflict).toBe(false);
+                this.player1.clickCard(this.stoicRival);
                 expect(this.player1).toHavePrompt('Action Window');
             });
         });
