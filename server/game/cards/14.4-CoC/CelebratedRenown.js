958bfc97f969e89693d7bde975aa1dbc047a36a7
Marc Soiferman <t>
2020-06-27 20:29:26 -0500

    Celebrated Renown

---
+++ server/game/cards/14.4-CoC/CelebratedRenown.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, Players } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class CelebratedRenown extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Honor a character',
+            target: {
+                controller: Players.Any,
+                cardType: CardTypes.Character,
+                cardCondition: (card, context) => {
+                    let charactersInPlay = context.game.findAnyCardsInPlay(c => c.type === CardTypes.Character);
+                    return card.fate === Math.max(...charactersInPlay.map(c => c.fate));
+                },
+                gameAction: AbilityDsl.actions.honor()
+            }
+        });
+    }
+}
+
+CelebratedRenown.id = 'celebrated-renown';
+
+module.exports = CelebratedRenown;
