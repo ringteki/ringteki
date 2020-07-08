2de194b1cbb26cde255f4cfda746cd2cefbf0584
lancerusso <66386076+lancerusso@users.noreply.github.com>
2020-07-08 21:31:02 +0100

    Update SturdyTetsubo.js
    Added Limit 2 Per Round to Sturdy Testsubo

--- server/game/cards/14.4-CoC/SturdyTetsubo.js
+++ server/game/cards/14.4-CoC/SturdyTetsubo.js
@@ -7,6 +7,7 @@ class SturdyTetsubo extends DrawCard {
         this.whileAttached({
             effect: AbilityDsl.effects.gainAbility(AbilityTypes.Reaction, {
                 title: 'Make opponent discard 1 card',
+                limit: AbilityDsl.limit.perRound(2),
                 when: {
                     afterConflict: (event, context) => context.source.isParticipating() && event.conflict.winner === context.source.controller
                         && context.player.opponent
