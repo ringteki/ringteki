6f94274c1ebcef3faedac13820db911cc9203131
Marc Soiferman <t>
2020-08-27 01:12:32 -0500

    Hida Etsuji

---
+++ server/game/cards/14.5-AHD/HidaEtsuji.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+const { Locations, Players, CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class HidaEtsuji extends DrawCard {
+    setupCardAbilities() {
+        this.persistentEffect({
+            match: (card, context) => card.type === CardTypes.Province && card.controller === context.source.controller,
+            targetLocation: Locations.Provinces,
+            targetController: Players.Self,
+            effect: AbilityDsl.effects.increaseLimitOnAbilities()
+        });
+
+        this.persistentEffect({
+            effect: AbilityDsl.effects.cardCannot({
+                cannot: 'applyCovert',
+                restricts: 'opponentsCardEffects'
+            })
+        });
+    }
+}
+
+HidaEtsuji.id = 'hida-etsuji';
+
+module.exports = HidaEtsuji;
