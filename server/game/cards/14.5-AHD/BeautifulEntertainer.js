705f7d381447d41bd556157bfdeac53d37aa4b0b
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2020-09-01 11:46:53 -0500

    AHD - Beautiful Entertainer (#595)
    Co-authored-by: Marc Soiferman <t>

---
+++ server/game/cards/14.5-AHD/BeautifulEntertainer.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class BeautifulEntertainer extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            title: 'Gain 2 Honor',
+            when: {
+                onCardLeavesPlay: (event, context) => event.card === context.source && context.player.opponent && context.player.honor < context.player.opponent.honor
+            },
+            gameAction: AbilityDsl.actions.gainHonor(context => ({
+                target: context.player,
+                amount: 2
+            }))
+        });
+    }
+}
+
+BeautifulEntertainer.id = 'beautiful-entertainer';
+
+module.exports = BeautifulEntertainer;
