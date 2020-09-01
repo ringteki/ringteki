3cf57adc00bf635eaaca4c78f3d5d9e5867a5df3
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2020-09-01 12:22:23 -0500

    AHD - Pious Guardian (#598)
    Co-authored-by: Marc Soiferman <t>

---
+++ server/game/cards/14.5-AHD/PiousGuardian.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+const { Phases } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class PiousGuardian extends DrawCard {
+    setupCardAbilities() {
+        this.interrupt({
+            title: 'Gain 1 honor',
+            when : {
+                onPhaseEnded: (event, context) => event.phase === Phases.Conflict && context.source.controller.getProvinces(a => a.isBroken).length < 2
+            },
+            gameAction: AbilityDsl.actions.gainHonor(context => ({
+                target: context.player
+            }))
+        });
+    }
+}
+
+PiousGuardian.id = 'pious-guardian';
+
+module.exports = PiousGuardian;
