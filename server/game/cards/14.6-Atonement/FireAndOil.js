7c5dbcb72add5fcea6cda9bf9059d3deaa2fcf1a
Marc Soiferman <t>
2020-09-23 23:02:03 -0500

    Atonement - Fire and Oil

---
+++ server/game/cards/14.6-Atonement/FireAndOil.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, AbilityTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class FireAndOil extends DrawCard {
+    setupCardAbilities() {
+        this.grantedAbilityLimits = {};
+        this.persistentEffect({
+            condition: context => !context.player.getProvinceCardInProvince(context.source.location).isBroken,
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.Action, {
+                title: 'Dishonor a character',
+                condition: context => context.game.isDuringConflict() && context.game.currentConflict
+                    && context.game.currentConflict.conflictProvince && context.game.currentConflict.conflictProvince.controller === context.player,
+                cost: AbilityDsl.costs.payHonor(1),
+                target: {
+                    cardType: CardTypes.Character,
+                    cardCondition: card => card.isAttacking(),
+                    gameAction: AbilityDsl.actions.dishonor()
+                }
+            })
+        });
+    }
+
+    leavesPlay() {
+        this.grantedAbilityLimits = {};
+        super.leavesPlay();
+    }
+}
+
+FireAndOil.id = 'fire-and-oil';
+
+module.exports = FireAndOil;
