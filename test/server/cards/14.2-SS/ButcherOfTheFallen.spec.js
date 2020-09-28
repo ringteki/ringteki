125f9c8f562f211cc3b52d60c1f434ac7338c218
Tom Nys <tom.nys@hotmail.com>
2020-09-29 01:35:00 +0200

    Correctly u^date butcher according to imperial law

--- test/server/cards/14.2-SS/ButcherOfTheFallen.spec.js
+++ test/server/cards/14.2-SS/ButcherOfTheFallen.spec.js
@@ -30,34 +30,7 @@ describe('Butcher of the Fallen', function() {
             this.sd5 = this.player1.findCardByName('shameful-display', 'stronghold province');
         });
 
-        it('unbroken = 5, should not let anyone with printed mil 5 or less defend', function() {
-            this.noMoreActions();
-
-            this.initiateConflict({
-                attackers: [this.butcher],
-                type: 'military'
-            });
-
-            expect(this.player2).toHavePrompt('Choose Defenders');
-
-            this.player2.clickCard(this.mil0);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil0);
-            this.player2.clickCard(this.mil1);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil1);
-            this.player2.clickCard(this.mil2);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil2);
-            this.player2.clickCard(this.mil3);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil3);
-            this.player2.clickCard(this.mil4);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil4);
-            this.player2.clickCard(this.mil5);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil5);
-            this.player2.clickCard(this.mil6);
-            expect(this.game.currentConflict.defenders).toContain(this.mil6);
-        });
-
-        it('unbroken = 4, should not let anyone with printed mil 4 or less defend', function() {
-            this.sd4.isBroken = true;
+        it('unbroken = 5, should not let anyone with less than mil 5 defend', function() {
             this.noMoreActions();
 
             this.initiateConflict({
@@ -83,7 +56,34 @@ describe('Butcher of the Fallen', function() {
             expect(this.game.currentConflict.defenders).toContain(this.mil6);
         });
 
-        it('unbroken = 2, should not let anyone with printed mil 2 or less defend', function() {
+        it('unbroken = 4, should not let anyone with less than mil 4 defend', function() {
+            this.sd4.isBroken = true;
+            this.noMoreActions();
+
+            this.initiateConflict({
+                attackers: [this.butcher],
+                type: 'military'
+            });
+
+            expect(this.player2).toHavePrompt('Choose Defenders');
+
+            this.player2.clickCard(this.mil0);
+            expect(this.game.currentConflict.defenders).not.toContain(this.mil0);
+            this.player2.clickCard(this.mil1);
+            expect(this.game.currentConflict.defenders).not.toContain(this.mil1);
+            this.player2.clickCard(this.mil2);
+            expect(this.game.currentConflict.defenders).not.toContain(this.mil2);
+            this.player2.clickCard(this.mil3);
+            expect(this.game.currentConflict.defenders).not.toContain(this.mil3);
+            this.player2.clickCard(this.mil4);
+            expect(this.game.currentConflict.defenders).toContain(this.mil4);
+            this.player2.clickCard(this.mil5);
+            expect(this.game.currentConflict.defenders).toContain(this.mil5);
+            this.player2.clickCard(this.mil6);
+            expect(this.game.currentConflict.defenders).toContain(this.mil6);
+        });
+
+        it('unbroken = 2, should not let anyone with less than mil 2 defend', function() {
             this.sd2.isBroken = true;
             this.sd3.isBroken = true;
             this.sd4.isBroken = true;
@@ -101,7 +101,7 @@ describe('Butcher of the Fallen', function() {
             this.player2.clickCard(this.mil1);
             expect(this.game.currentConflict.defenders).not.toContain(this.mil1);
             this.player2.clickCard(this.mil2);
-            expect(this.game.currentConflict.defenders).not.toContain(this.mil2);
+            expect(this.game.currentConflict.defenders).toContain(this.mil2);
             this.player2.clickCard(this.mil3);
             expect(this.game.currentConflict.defenders).toContain(this.mil3);
             this.player2.clickCard(this.mil4);
