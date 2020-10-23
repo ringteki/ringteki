1a5db3d2f2ddb799791566b2192d8945ca315e7f
AAfghahi <arash.afghahi@gmail.com>
2020-10-23 00:41:11 -0400

    added Procedural Interference as well, sorry wrong branch

---
+++ server/game/cards/15.1-TL/ProceduralInterference.js
@@ -0,0 +1,43 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { Players, Locations, CardTypes, TargetModes } = require('../../Constants');
+
+class ProceduralInterference extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Discard all cards in a province or gain 2 honor',
+            targets: {
+                province:{
+                    location: Locations.Provinces,
+                    controller: Players.Opponent,
+                    cardType: CardTypes.Province
+                },
+                select: {
+                    mode: TargetModes.Select,
+                    dependsOn: 'province',
+                    player: Players.Opponent,
+                    choices: {
+                        'discard each card in the province': AbilityDsl.actions.moveCard(context => ({
+                            destination: Locations.DynastyDiscardPile,
+                            target: context.targets.province.controller.getDynastyCardsInProvince(context.targets.province.location)
+                        })),
+                        'let opponent gain 2 honor': AbilityDsl.actions.gainHonor({
+                            amount: 2
+                        })
+                    }
+                }
+            },   
+            effect: '{1}{2}',
+            effectArgs: context => {
+                if(context.selects.select.choice === 'let opponent gain 2 honor') {
+                    return ['take 2 honor from ', context.player.opponent];
+                }
+                return ['discard each card in the province ', context.targets.province];
+            }
+        });
+    }
+}
+
+ProceduralInterference.id = 'procedural-interference';
+
+module.exports = ProceduralInterference;
---
+++ test/server/cards/15.1-TL/ProceduralInterferece.spec.js
@@ -0,0 +1,85 @@
+describe('Procedural Interference', function() {
+    integration(function() {
+        beforeEach(function() {
+            this.setupTest({
+                phase: 'dynasty',
+                player1: {
+                    dynastyDiscard: ['aranat', 'daidoji-uji', 'procedural-interference', 'procedural-interference'],
+                    honor: 10 
+                },
+                player2: {
+                    dynastyDiscard: ['aranat', 'daidoji-uji', 'imperial-storehouse', 'heavy-ballista'],
+                    honor: 10,
+                    provinces:['shameful-display', 'silent-ones-monastery']
+                }
+            });
+            this.interference = this.player1.findCardByName('procedural-interference');
+            this.interference2 = this.player1.findCardByName('procedural-interference');
+            this.aranat = this.player1.findCardByName('aranat');
+            this.uji = this.player1.findCardByName('daidoji-uji');
+        
+
+            this.aranat2 = this.player2.findCardByName('aranat');
+            this.uji2 = this.player2.findCardByName('daidoji-uji');
+            this.storehouse2 = this.player2.findCardByName('imperial-storehouse');
+            this.ballista2 = this.player2.findCardByName('heavy-ballista');
+
+            this.shamefulDisplay = this.player2.findCardByName('shameful-display', 'province 1');
+            this.shameful2 = this.player1.findCardByName('shameful-display', 'province 1');
+            this.silent = this.player2.findCardByName('silent-ones-monastery', 'province 2')
+          
+            this.player2.reduceDeckToNumber('dynasty deck', 0);
+            this.player2.moveCard(this.aranat2, 'province 1');
+            this.player2.moveCard(this.ballista2, 'dynasty deck');
+
+            this.player1.reduceDeckToNumber('dynasty deck', 0);
+            this.player1.moveCard(this.interference, 'province 1');
+            this.player1.moveCard(this.interference2, 'province 1')
+            this.player1.moveCard(this.uji, 'dynasty deck');
+        });
+
+        it('should prompt a province to be chosen', function () {
+            this.player1.clickCard(this.interference);
+            expect(this.player1).toBeAbleToSelect(this.shamefulDisplay);
+        });
+
+        it('should give the opponent an option to select between effects', function() {
+            this.player1.clickCard(this.interference);
+            this.player1.clickCard(this.shamefulDisplay);
+
+            expect(this.player2).toHavePromptButton('discard each card in the province');
+            expect(this.player2).toHavePromptButton('let opponent gain 2 honor');
+        });
+
+        it('should discard all cards in the province when selected', function() {
+            this.player1.clickCard(this.interference);
+            this.player1.clickCard(this.shamefulDisplay);
+            this.player2.clickPrompt('Discard each card in the province');
+
+            expect(this.aranat2.location).toBe('dynasty discard pile');
+            expect(this.ballista2.location).toBe('province 1');
+            expect(this.ballista2.facedown).toBe(true);
+            expect(this.getChatLogs(5)).toContain('player1 plays Procedural Interference to discard each card in the province Shameful Display');
+        });
+
+        it('should let the player gain 2 honor', function() {
+            this.player1.clickCard(this.interference);
+            this.player1.clickCard(this.shamefulDisplay);
+            this.player2.clickPrompt('let opponent gain 2 honor');
+
+            expect(this.player1.player.honor).toBe(12);
+            expect(this.player2.player.honor).toBe(10);
+        });
+
+        it('should not let you gain 4 honor when Silent Ones is present', function(){
+            this.player1.clickCard(this.interference);
+            this.player1.clickCard(this.shamefulDisplay);
+            this.player2.clickPrompt('let opponent gain 2 honor');
+            this.player1.clickCard(this.interference2);
+            expect(this.player2).not.toHavePromptButton('let opponent gain 2 honor');
+        })
+
+    });
+});
+
+
