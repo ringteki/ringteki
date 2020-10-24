1a572872310e8b85bbbd6bdb06cecb481a1806d8
Marc Soiferman <t>
2020-10-23 23:42:15 -0500

    Twisted Loyalties - Watchtower of Sun's Shadow

--- server/game/cards/15.1-TL/WatchtowerOfSunsShadow.js
+++ server/game/cards/15.1-TL/WatchtowerOfSunsShadow.js
@@ -6,15 +6,26 @@ class WatchtowerOfSunsShadow extends DrawCard {
     setupCardAbilities() {
         this.persistentEffect({
             condition: context => {
-                if(context.player.isDefendingPlayer()) {
-                    let cards = context.player.getDynastyCardsInProvince(this.game.currentConflict.conflictProvince.location);
+                if(context.game.currentConflict && context.game.currentConflict.conflictProvince && context.player.isDefendingPlayer()) {
+                    let cards = context.player.getDynastyCardsInProvince(context.game.currentConflict.conflictProvince.location);
                     return cards.some(card => card.isFaceup() && card.type === CardTypes.Holding && card.hasTrait('kaiu-wall'));
                 }
                 return false;
             },
             targetController: Players.Opponent,
-            match: (card, context) => card.controller === context.player.opponent,
+            match: (card) => card.isAttacking(),
             effect: AbilityDsl.effects.modifyBothSkills(card => -card.fate)
+        });
+
+        this.forcedInterrupt({
+            title: 'Lose 2 honor',
+            when: {
+                onBreakProvince: (event, context) => event.card.controller === context.player && event.card.location === context.source.location
+            },
+            gameAction: AbilityDsl.actions.loseHonor(context => ({
+                amount: 2,
+                target: context.player
+            }))
         });
     }
 }
--- test/server/cards/15.1-TL/WatchtowerOfSunsShadow.spec.js
+++ test/server/cards/15.1-TL/WatchtowerOfSunsShadow.spec.js
@@ -4,25 +4,27 @@ describe('Watchtower of Suns Shadow', function() {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
-                    inPlay: ['doji-kuwanan'],
-                    hand: ['assassination', 'duty'],
+                    inPlay: ['doji-kuwanan', 'doji-challenger', 'kakita-yoshi'],
+                    hand: ['way-of-the-crane', 'called-to-war'],
                     dynastyDiscard: ['northern-curtain-wall']
                 },
                 player2: {
                     inPlay: ['hida-kisada'],
-                    dynastyDiscard: ['river-of-the-last-stand', 'northern-curtain-wall', 'imperial-storehouse']
+                    dynastyDiscard: ['watchtower-of-sun-s-shadow', 'northern-curtain-wall', 'imperial-storehouse']
                 }
             });
 
+            this.crane = this.player1.findCardByName('way-of-the-crane');
+            this.yoshi = this.player1.findCardByName('kakita-yoshi');
             this.kuwanan = this.player1.findCardByName('doji-kuwanan');
+            this.challenger = this.player1.findCardByName('doji-challenger');
             this.kisada = this.player2.findCardByName('hida-kisada');
-            this.river = this.player2.placeCardInProvince('river-of-the-last-stand', 'province 1');
+            this.watchtower = this.player2.placeCardInProvince('watchtower-of-sun-s-shadow', 'province 1');
             this.wall = this.player2.placeCardInProvince('northern-curtain-wall', 'province 2');
             this.storehouse = this.player2.placeCardInProvince('imperial-storehouse', 'province 3');
 
             this.player1Wall = this.player1.placeCardInProvince('northern-curtain-wall', 'province 1');
-            this.assassination = this.player1.findCardByName('assassination');
-            this.duty = this.player1.findCardByName('duty');
+            this.calledToWar = this.player1.findCardByName('called-to-war');
 
             this.p1 = this.player2.findCardByName('shameful-display', 'province 1');
             this.p2 = this.player2.findCardByName('shameful-display', 'province 2');
@@ -32,85 +34,191 @@ describe('Watchtower of Suns Shadow', function() {
             this.player1p1 = this.player1.findCardByName('shameful-display', 'province 1');
         });
 
-        it('should make your opponent discard two cards & draw a card during a conflict at itself', function () {
+        it('should give opponent\'s participating characters -1/-1 for each fate they have during a conflict at itself', function () {
+            this.kuwanan.fate = 2;
+            this.challenger.fate = 1;
+            this.kisada.fate = 3;
+            this.yoshi.fate = 4;
+
             this.noMoreActions();
             this.initiateConflict({
                 type: 'military',
-                attackers: [this.kuwanan],
+                attackers: [this.kuwanan, this.challenger],
                 defenders: [this.kisada],
                 province: this.p1
             });
 
-            let hand = this.player1.hand.length;
-            expect(hand).toBe(2);
-            this.player2.clickCard(this.river);
-            expect(this.player1.hand.length).toBe(1);
-            expect(this.assassination.location).toBe('conflict discard pile');
-            expect(this.duty.location).toBe('conflict discard pile');
+            expect(this.kuwanan.getMilitarySkill()).toBe(5 - 2);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4 - 2);
+
+            expect(this.challenger.getMilitarySkill()).toBe(3 - 1);
+            expect(this.challenger.getPoliticalSkill()).toBe(3 - 1);
+
+            expect(this.kisada.getMilitarySkill()).toBe(7);
+            expect(this.kisada.getPoliticalSkill()).toBe(2);
+
+            expect(this.yoshi.getMilitarySkill()).toBe(2);
+            expect(this.yoshi.getPoliticalSkill()).toBe(6);
         });
 
         it('should work during a conflict at another kaiu wall', function () {
+            this.kuwanan.fate = 2;
+            this.challenger.fate = 1;
+            this.kisada.fate = 3;
+            this.yoshi.fate = 4;
+
             this.noMoreActions();
             this.initiateConflict({
                 type: 'military',
-                attackers: [this.kuwanan],
+                attackers: [this.kuwanan, this.challenger],
                 defenders: [this.kisada],
                 province: this.p2
             });
 
-            let hand = this.player1.hand.length;
-            expect(hand).toBe(2);
-            this.player2.clickCard(this.river);
-            expect(this.player1.hand.length).toBe(1);
-            expect(this.assassination.location).toBe('conflict discard pile');
-            expect(this.duty.location).toBe('conflict discard pile');
+            expect(this.kuwanan.getMilitarySkill()).toBe(5 - 2);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4 - 2);
+
+            expect(this.challenger.getMilitarySkill()).toBe(3 - 1);
+            expect(this.challenger.getPoliticalSkill()).toBe(3 - 1);
+
+            expect(this.kisada.getMilitarySkill()).toBe(7);
+            expect(this.kisada.getPoliticalSkill()).toBe(2);
+
+            expect(this.yoshi.getMilitarySkill()).toBe(2);
+            expect(this.yoshi.getPoliticalSkill()).toBe(6);
         });
 
         it('should not work during a conflict not at a kaiu wall', function () {
+            this.kuwanan.fate = 2;
+            this.challenger.fate = 1;
+            this.kisada.fate = 3;
+            this.yoshi.fate = 4;
+
             this.noMoreActions();
             this.initiateConflict({
                 type: 'military',
-                attackers: [this.kuwanan],
+                attackers: [this.kuwanan, this.challenger],
                 defenders: [this.kisada],
                 province: this.p3
             });
 
-            let hand = this.player1.hand.length;
-            expect(hand).toBe(2);
-            this.player2.clickCard(this.river);
-            expect(this.player1.hand.length).toBe(2);
+            expect(this.kuwanan.getMilitarySkill()).toBe(5);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4);
+
+            expect(this.challenger.getMilitarySkill()).toBe(3);
+            expect(this.challenger.getPoliticalSkill()).toBe(3);
+
+            expect(this.kisada.getMilitarySkill()).toBe(7);
+            expect(this.kisada.getPoliticalSkill()).toBe(2);
+
+            expect(this.yoshi.getMilitarySkill()).toBe(2);
+            expect(this.yoshi.getPoliticalSkill()).toBe(6);
         });
 
         it('should not work during a conflict not at a holding', function () {
+            this.kuwanan.fate = 2;
+            this.challenger.fate = 1;
+            this.kisada.fate = 3;
+            this.yoshi.fate = 4;
+
             this.noMoreActions();
             this.initiateConflict({
                 type: 'military',
-                attackers: [this.kuwanan],
+                attackers: [this.kuwanan, this.challenger],
                 defenders: [this.kisada],
                 province: this.p4
             });
 
-            let hand = this.player1.hand.length;
-            expect(hand).toBe(2);
-            this.player2.clickCard(this.river);
-            expect(this.player1.hand.length).toBe(2);
+            expect(this.kuwanan.getMilitarySkill()).toBe(5);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4);
+
+            expect(this.challenger.getMilitarySkill()).toBe(3);
+            expect(this.challenger.getPoliticalSkill()).toBe(3);
+
+            expect(this.kisada.getMilitarySkill()).toBe(7);
+            expect(this.kisada.getPoliticalSkill()).toBe(2);
+
+            expect(this.yoshi.getMilitarySkill()).toBe(2);
+            expect(this.yoshi.getPoliticalSkill()).toBe(6);
         });
 
         it('should not work on attack', function () {
+            this.kuwanan.fate = 2;
+            this.challenger.fate = 1;
+            this.kisada.fate = 3;
+            this.yoshi.fate = 4;
+
             this.noMoreActions();
             this.player1.passConflict();
+
             this.noMoreActions();
             this.initiateConflict({
                 type: 'military',
                 attackers: [this.kisada],
-                defenders: [this.kuwanan]
+                defenders: [this.kuwanan, this.challenger],
+                province: this.player1p1
             });
 
-            this.player1.pass();
-            let hand = this.player1.hand.length;
-            expect(hand).toBe(2);
-            this.player2.clickCard(this.river);
-            expect(this.player1.hand.length).toBe(2);
+            expect(this.kuwanan.getMilitarySkill()).toBe(5);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4);
+
+            expect(this.challenger.getMilitarySkill()).toBe(3);
+            expect(this.challenger.getPoliticalSkill()).toBe(3);
+
+            expect(this.kisada.getMilitarySkill()).toBe(7);
+            expect(this.kisada.getPoliticalSkill()).toBe(2);
+
+            expect(this.yoshi.getMilitarySkill()).toBe(2);
+            expect(this.yoshi.getPoliticalSkill()).toBe(6);
+        });
+
+        it('should update if you put fate on a character', function () {
+            this.kuwanan.fate = 2;
+            this.challenger.fate = 1;
+            this.kisada.fate = 3;
+            this.yoshi.fate = 4;
+
+            this.noMoreActions();
+            this.initiateConflict({
+                type: 'military',
+                attackers: [this.kuwanan, this.challenger],
+                defenders: [this.kisada],
+                province: this.p1
+            });
+
+            expect(this.kuwanan.getMilitarySkill()).toBe(5 - 2);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4 - 2);
+
+            this.player2.pass();
+            this.player1.clickCard(this.crane);
+            this.player1.clickCard(this.yoshi);
+            this.player2.pass();
+            this.player1.clickCard(this.calledToWar);
+            this.player1.clickCard(this.kuwanan);
+            this.player2.clickPrompt('No');
+
+            expect(this.kuwanan.fate).toBe(3);
+
+            expect(this.kuwanan.getMilitarySkill()).toBe(5 - 3);
+            expect(this.kuwanan.getPoliticalSkill()).toBe(4 - 3);
+        });
+
+        it('forced interrupt', function () {
+            this.noMoreActions();
+            this.initiateConflict({
+                type: 'military',
+                attackers: [this.kuwanan, this.challenger],
+                defenders: [this.kisada],
+                province: this.p1
+            });
+
+            let honor = this.player2.honor;
+
+            this.kisada.bowed = true;
+            this.noMoreActions();
+
+            expect(this.player2.honor).toBe(honor - 2);
+            expect(this.getChatLogs(5)).toContain('player2 uses Watchtower of Sun\'s Shadow to make player2 lose 2 honor');
         });
     });
 });
