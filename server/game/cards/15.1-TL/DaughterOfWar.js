43dafb750d7ec4c206f07fdc768c8d1657b2edc7
Arash <arash.afghahi@gmail.com>
2020-11-03 14:12:09 -0500

    changed to decksearch

--- server/game/cards/15.1-TL/DaughterOfWar.js
+++ server/game/cards/15.1-TL/DaughterOfWar.js
@@ -1,6 +1,6 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
-const { CardTypes, Locations} = require('../../Constants');
+const { CardTypes, Locations, Decks} = require('../../Constants');
 
 class DaughterOfWar extends DrawCard {
     setupCardAbilities() {
@@ -12,27 +12,13 @@ class DaughterOfWar extends DrawCard {
             when: {
                 onCardLeavesPlay: (event, context) => event.card === context.source.parent
             },
-            gameAction: AbilityDsl.actions.cardMenu(context => ({
+            gameAction: AbilityDsl.actions.deckSearch(context => ({
                 activePromptTitle: 'Choose a character to put into play ',
-                cards: context.player.dynastyDeck.first(context.player.dynastyDeck.size()),
+                deck: Decks.DynastyDeck,
                 cardCondition: card => card.type === CardTypes.Character && card.costLessThan(context.source.parent.getCost()),
-                choices: ['Don\'t choose a character'],
-                handlers: [
-                    function() {
-                        context.game.addMessage('{0} chooses not to put any character into play', context.player);
-                        context.player.shuffleDynastyDeck();
-                    }
-                ],
-                subActionProperties: card => ({ target: card }),
-                message: '{0} chooses to put {1} into play',
-                messageArgs: (card, player) => [player, card],
-                gameAction: AbilityDsl.actions.sequential([
-                    AbilityDsl.actions.putIntoPlay(),
-                    AbilityDsl.actions.shuffleDeck(context => ({
-                        deck: Locations.DynastyDeck,
-                        target: context.player
-                    }))
-                ])
+                reveal: true,
+                faceup: true,
+                destination: Locations.PlayArea
             }))
         });
     }
