290d2d8b6dd788e4dbe3a06efba1c88eb786e69f
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2020-11-03 12:12:27 -0600

    This is working (#737)
    Co-authored-by: Marc Soiferman <t>

---
+++ server/game/cards/15.1-TL/Kunshu.js
@@ -0,0 +1,37 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { AbilityTypes, CardTypes, Locations, Players, PlayTypes } = require('../../Constants');
+
+class Kunshu extends DrawCard {
+    setupCardAbilities() {
+        this.attachmentConditions({
+            myControl: true,
+            unique: true
+        });
+
+        this.grantedAbilityLimits = {};
+        this.whileAttached({
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.Action, {
+                title: 'Play a card',
+                cost: AbilityDsl.costs.discardImperialFavor(),
+                condition: context => context.source.isParticipating(),
+                printedAbility: false,
+                target: {
+                    cardType: [CardTypes.Event, CardTypes.Attachment],
+                    location: [Locations.ConflictDiscardPile],
+                    player: Players.Self,
+                    controller: Players.Opponent,
+                    gameAction: AbilityDsl.actions.playCard(() => ({
+                        playType: PlayTypes.Other,
+                        ignoreFateCost: true
+                    }))
+                },
+                effect: 'play {0}'
+            })
+        });
+    }
+}
+
+Kunshu.id = 'kunshu';
+
+module.exports = Kunshu;
