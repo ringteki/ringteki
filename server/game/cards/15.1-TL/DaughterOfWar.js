af27e5f6dba84f098ac95c5cebaee2424d06b5f5
AAfghahi <48933336+AAfghahi@users.noreply.github.com>
2020-11-03 14:42:02 -0500

    Twisted Loyalties - Daughter of War (#747)
    * sinister
    
    * changes
    
    * oguri2
    
    * linted
    
    * daughter of war
    
    * changed to decksearch
    
    * changed effect
    
    Co-authored-by: Tom Nys <tom.nys@hotmail.com>

---
+++ server/game/cards/15.1-TL/DaughterOfWar.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes, Locations, Decks} = require('../../Constants');
+
+class DaughterOfWar extends DrawCard {
+    setupCardAbilities() {
+        this.attachmentConditions({
+            myControl: true
+        });
+        this.interrupt({
+            title: 'Put a character into play ',
+            when: {
+                onCardLeavesPlay: (event, context) => event.card === context.source.parent
+            },
+            gameAction: AbilityDsl.actions.deckSearch(context => ({
+                activePromptTitle: 'Choose a character to put into play ',
+                deck: Decks.DynastyDeck,
+                cardCondition: card => card.type === CardTypes.Character && card.costLessThan(context.source.parent.getCost()),
+                reveal: true,
+                faceup: true,
+                destination: Locations.PlayArea
+            })),
+            effect: 'to search their deck for a character with cost less than {1} to put into play',
+            effectArgs: context => [context.source.parent.getCost()]
+        });
+    }
+}
+DaughterOfWar.id = 'daughter-of-war';
+
+module.exports = DaughterOfWar;
