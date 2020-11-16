c9bacefd0a3fa1eb3bd09bec553c39fe56dcdf77
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2020-11-16 16:56:38 -0600

    HiF - Nimble Noyan (#773)
    Co-authored-by: Marc Soiferman <t>

--- server/game/Constants.ts
+++ server/game/Constants.ts
@@ -137,6 +137,7 @@ export enum EffectNames {
     ShowTopConflictCard = 'showTopConflictCard',
     ShowTopDynastyCard = 'showTopDynastyCard',
     ContributeToConflict = 'contribute',
+    CanContributeWhileBowed = 'canContributeWhileBowed',
     ChangeConflictSkillFunction = 'skillFunction',
     ModifyConflictElementsToResolve = 'modifyConflictElementsToResolve',
     RestrictNumberOfDefenders = 'restrictNumberOfDefenders',
