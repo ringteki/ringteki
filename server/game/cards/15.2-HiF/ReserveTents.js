ff1ebb8fd53ecf52b535022b79aea823daeb3cec
Tom Nys <tom.nys@hotmail.com>
2020-11-18 03:20:16 +0100

    HiF - Reserve Tents

---
+++ server/game/cards/15.2-HiF/ReserveTents.js
@@ -0,0 +1,23 @@
+const AbilityDsl = require('../../abilitydsl.js');
+const { CardTypes, Players } = require('../../Constants.js');
+const DrawCard = require('../../drawcard.js');
+
+class ReserveTents extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Move a character to the conflict',
+            limit: AbilityDsl.limit.perRound(2),
+            condition: context => context.game.isDuringConflict(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                player: Players.Self,
+                gameAction: AbilityDsl.actions.moveToConflict()
+            }
+        })
+    }
+}
+
+ReserveTents.id = 'reserve-tents';
+
+module.exports = ReserveTents;
