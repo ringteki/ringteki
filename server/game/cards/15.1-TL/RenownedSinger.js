fba12be7a4219ee7a5de70c92978fca30f141556
Marc Soiferman <t>
2020-12-19 12:56:38 -0600

    Update for Singer UX

--- server/game/cards/15.1-TL/RenownedSinger.js
+++ server/game/cards/15.1-TL/RenownedSinger.js
@@ -14,30 +14,36 @@ class RenownedSinger extends DrawCard {
                 location: Locations.ConflictDiscardPile,
                 cardType: [CardTypes.Character, CardTypes.Attachment, CardTypes.Event],
                 controller: Players.Self,
-                gameAction: AbilityDsl.actions.cardMenu(context => ({
-                    activePromptTitle: 'Choose a card to add to your opponent\'s hand',
-                    player: context.player.opponent,
-                    cards: context.target,
-                    targets: true,
-                    message: '{0} chooses {1} to be put into {2}\'s hand. {3} is put on bottom of {2}\'s conflict deck',
-                    messageArgs: card => [context.player.opponent, card, context.player, context.target.filter(a => a !== card)],
-                    subActionProperties: card => ({ target: card }),
-                    gameAction: AbilityDsl.actions.sequential([
-                        AbilityDsl.actions.moveCard(context => ({
-                            target: context.targets.selectedCard,
-                            destination: Locations.Hand
-                        })),
-                        AbilityDsl.actions.returnToDeck(context => ({
-                            target: context.target.filter(a => a.location !== Locations.Hand),
-                            location: Locations.ConflictDiscardPile,
-                            bottom: true,
-                            shuffle: false
-                        }))
-                    ])
-                }))
+                gameAction: AbilityDsl.actions.handler({
+                    handler: context => this.game.promptWithHandlerMenu(context.player.opponent, {
+                        activePromptTitle: 'Choose a card to add to your opponent\'s hand',
+                        context: context,
+                        cards: context.target,
+                        cardHandler: handCard => {
+                            let bottomCard = context.target.filter(a => a !== handCard);
+                            context.game.addMessage('{0} chooses {1} to be put into {2}\'s hand. {3} is put on the bottom of {2}\'s conflict deck',
+                                context.player.opponent, handCard, context.player, bottomCard);
+
+                            let gameAction = AbilityDsl.actions.multiple([
+                                AbilityDsl.actions.moveCard({
+                                    target: handCard,
+                                    destination: Locations.Hand
+                                }),
+                                AbilityDsl.actions.returnToDeck({
+                                    target: bottomCard,
+                                    location: Locations.ConflictDiscardPile,
+                                    bottom: true,
+                                    shuffle: false
+                                })
+                            ]);
+
+                            gameAction.resolve(undefined, context);
+                        }
+                    })
+                })
             },
-            effect: 'have {1} pick between two cards to add to their hand',
-            effectArgs: context => [context.player.opponent]
+            effect: 'have {1} return one of {2} to {3}\'s hand',
+            effectArgs: context => [context.player.opponent, context.target, context.player]
         });
     }
 }
