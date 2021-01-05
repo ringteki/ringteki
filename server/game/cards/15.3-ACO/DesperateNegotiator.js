96ae1bb2b2c14157f81b997d91fb6d0f41d714e2
Arash <arash.afghahi@gmail.com>
2021-01-04 23:47:25 -0500

    desperate negotiator

---
+++ server/game/cards/15.3-ACO/DesperateNegotiator.js
@@ -0,0 +1,14 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class DesperateNegotiator extends DrawCard {
+    setupCardAbilities() {
+        this.dire({
+            effect: AbilityDsl.effects.modifyBothSkills(2)
+        });
+    }
+}
+
+DesperateNegotiator.id = 'desperate-negotiator';
+
+module.exports = DesperateNegotiator;
---
+++ test/server/cards/15.3-ACO/DesperateNegotiator.spec.js
@@ -0,0 +1,34 @@
+describe('Desperate Negotiator', function() {
+    integration(function() {
+        beforeEach(function() {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    inPlay: ['desperate-negotiator']
+                },
+                player2:{
+                    inPlay: ['desperate-negotiator']
+                }
+            });
+
+            this.negotiator = this.player1.findCardByName('desperate-negotiator');
+            this.negotiator2 = this.player2.findCardByName('desperate-negotiator');
+            this.negotiator2.fate = 1;
+
+        });
+
+
+        it('should get +2 skill when dire', function() {
+            expect(this.negotiator.getMilitarySkill()).toBe(this.negotiator.printedMilitarySkill + 2);
+            expect(this.negotiator.getPoliticalSkill()).toBe(this.negotiator.printedPoliticalSkill + 2);
+        });
+
+        it('should not get +3 military skill when dire', function() {
+            expect(this.negotiator2.fate).toBe(1);
+            this.game.checkGameState(true);
+            expect(this.negotiator2.getMilitarySkill()).toBe(this.negotiator2.printedMilitarySkill);
+            expect(this.negotiator2.getPoliticalSkill()).toBe(this.negotiator.printedPoliticalSkill);
+        });
+
+    });
+});
