64e37001a7d3538b65a0cd57a09ca8ecc009abdc
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2021-01-05 00:51:15 -0600

    Update DesperateNegotiator.spec.js

--- test/server/cards/15.3-ACO/DesperateNegotiator.spec.js
+++ test/server/cards/15.3-ACO/DesperateNegotiator.spec.js
@@ -18,12 +18,12 @@ describe('Desperate Negotiator', function() {
         });
 
 
-        it('should get +2 skill when dire', function() {
+        it('should get +2/+2 when dire', function() {
             expect(this.negotiator.getMilitarySkill()).toBe(this.negotiator.printedMilitarySkill + 2);
             expect(this.negotiator.getPoliticalSkill()).toBe(this.negotiator.printedPoliticalSkill + 2);
         });
 
-        it('should not get +3 military skill when dire', function() {
+        it('should not get +2/+2 when dire', function() {
             expect(this.negotiator2.fate).toBe(1);
             this.game.checkGameState(true);
             expect(this.negotiator2.getMilitarySkill()).toBe(this.negotiator2.printedMilitarySkill);
