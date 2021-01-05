aa81f8e4b92d3128fba286c25c8ec80ecd9c65c8
AAfghahi <48933336+AAfghahi@users.noreply.github.com>
2021-01-04 23:57:40 -0700

    ACO - Feeding An Army #861 (#878)
    * this is Feeding, but maybe not entirely
    
    * Trigger Build
    
    * Update FeedingAnArmy.spec.js
    
    Co-authored-by: marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>

---
+++ server/game/cards/15.3-ACO/FeedingAnArmy.js
@@ -0,0 +1,22 @@
+import { Phases } from '../../Constants.js';
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+
+class FeedingAnArmy extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Put fate on characters',
+            when: {
+                onPhaseStarted: event => event.phase === Phases.Conflict
+            },
+            cost: [AbilityDsl.costs.breakProvince({ cardCondition: card => card.isFaceup() })],
+            gameAction: AbilityDsl.actions.placeFate(context => ({
+                target: context.player.cardsInPlay.filter(card => card.costLessThan(4))
+            }))
+        });
+    }
+}
+
+FeedingAnArmy.id = 'feeding-an-army';
+
+module.exports = FeedingAnArmy;
