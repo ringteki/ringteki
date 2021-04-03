14578f5c6f858db7ea646e325fb293191dac4225
Marc Soiferman <t>
2021-04-03 12:29:58 -0500

    CoP - Higashi Kaze Company

---
+++ server/game/cards/15.5-CoP/HigashiKazeCompany.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+const { Players, CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class HigashiKazeCompany extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Prevent a character from bowing',
+            when: {
+                afterConflict: (event, context) => event.conflict.winner === context.player && context.source.isParticipating()
+            },
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Any,
+                cardCondition: (card, context) => card.getFate() === 0 && card.isParticipating() && card !== context.source,
+                gameAction: AbilityDsl.actions.cardLastingEffect({
+                    effect: AbilityDsl.effects.doesNotBow()
+                })
+            },
+            effect: 'prevent {0} from bowing at the end of the conflict'
+        });
+    }
+}
+
+HigashiKazeCompany.id = 'higashi-kaze-company';
+
+module.exports = HigashiKazeCompany;
