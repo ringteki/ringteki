09b3bbd1250d719f52d66dbd848a758ed3d41da4
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2021-04-08 00:11:48 -0500

    CoP - Shosuro Deceiver (#1020)
    * Infinite loops
    
    * The rest
    
    * Linting
    
    Co-authored-by: Marc Soiferman <t>

--- server/game/effects.js
+++ server/game/effects.js
@@ -3,6 +3,7 @@ const _ = require('underscore');
 const AbilityLimit = require('./abilitylimit.js');
 const CopyCharacter = require('./Effects/CopyCharacter');
 const GainAllAbiliites = require('./Effects/GainAllAbilities.js');
+const GainAllAbiliitesDynamic = require('./Effects/GainAllAbilitiesDynamic.js');
 const Restriction = require('./Effects/restriction.js');
 const SuppressEffect = require('./Effects/SuppressEffect');
 const GainAbility = require('./Effects/GainAbility');
@@ -72,6 +73,7 @@ const Effects = {
     fateCostToTarget: (properties) => EffectBuilder.card.flexible(EffectNames.FateCostToTarget, properties),
     gainAbility: (abilityType, properties) => EffectBuilder.card.static(EffectNames.GainAbility, new GainAbility(abilityType, properties)),
     gainAllAbilities: (character) => EffectBuilder.card.static(EffectNames.GainAllAbilities, new GainAllAbiliites(character)),
+    gainAllAbilitiesDynamic: (match) => EffectBuilder.card.static(EffectNames.GainAllAbilitiesDynamic, new GainAllAbiliitesDynamic(match)),
     gainExtraFateWhenPlayed: (amount = 1) => EffectBuilder.card.flexible(EffectNames.GainExtraFateWhenPlayed, amount),
     gainPlayAction: (playActionClass) => EffectBuilder.card.detached(EffectNames.GainPlayAction, {
         apply: card => {
