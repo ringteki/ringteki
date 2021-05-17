6be4f45fbb129169150c08d11ba9380fa9cee83f
Marc Soiferman <t>
2021-05-16 22:41:19 -0500

    Way of the Warrior

---
+++ server/game/cards/16-UFLS/WayOfTheWarrior.js
@@ -0,0 +1,40 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, Players } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class WayOfTheWarrior extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Give a character a lot of things',
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Any,
+                cardCondition: card => card.isParticipating() && card.hasTrait('bushi'),
+                gameAction: AbilityDsl.actions.sequential([
+                    AbilityDsl.actions.cardLastingEffect(() => ({
+                        effect: [
+                            AbilityDsl.effects.cardCannot({
+                                cannot: 'sendHome',
+                                restricts: 'opponentsCardEffects'
+                            }),
+                            AbilityDsl.effects.cardCannot({
+                                cannot: 'bow',
+                                restricts: 'opponentsCardEffects'
+                            }),
+                            AbilityDsl.effects.cardCannot({
+                                cannot: 'dishonor',
+                                restricts: 'opponentsCardEffects'
+                            })
+                        ]
+                    })),
+                    AbilityDsl.actions.ready()
+                ])
+            },
+            effect: 'ready and prevent opponent\'s card effects from bowing, sending home, or dishonoring {0}'
+        });
+    }
+}
+
+WayOfTheWarrior.id = 'way-of-the-warrior';
+
+module.exports = WayOfTheWarrior;
