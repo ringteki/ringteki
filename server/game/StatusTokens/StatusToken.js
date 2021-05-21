d562fda235d6fe36c08b2ce956efa78fd8fb48e5
Marc Soiferman <t>
2021-05-21 01:27:20 -0500

    Disciple of Deception

--- server/game/StatusTokens/StatusToken.js
+++ server/game/StatusTokens/StatusToken.js
@@ -1,4 +1,6 @@
 const EffectSource = require('../EffectSource');
+const AbilityDsl = require('../abilitydsl');
+const { CardTypes, CharacterStatus, EffectNames } = require('../Constants');
 
 class StatusToken extends EffectSource {
     constructor(game, card, status, title) {
@@ -6,7 +8,8 @@ class StatusToken extends EffectSource {
         this.card = card;
         this.printedType = 'token';
         this.persistentEffects = [];
-        this.grantedStatus = status;
+        this._grantedStatus = status;
+        this.overrideStatus = undefined;
 
         this.applyEffects();
     }
@@ -15,7 +18,21 @@ class StatusToken extends EffectSource {
         return this.card.controller;
     }
 
+    get grantedStatus() {
+        if(this.overrideStatus) {
+            return this.overrideStatus;
+        }
+        return this._grantedStatus;
+    }
+
     applyEffects() {
+        if(this.grantedStatus === CharacterStatus.Honored) {
+            this.applyHonoredEffect();
+        } else if(this.grantedStatus === CharacterStatus.Dishonored) {
+            this.applyDishonoredEffect();
+        } else if(this.grantedStatus === CharacterStatus.Tainted) {
+            this.applyTaintedEffect();
+        }
     }
 
     removeEffects() {
@@ -30,6 +47,74 @@ class StatusToken extends EffectSource {
         this.removeEffects();
         this.card = card;
         this.applyEffects();
+    }
+
+    applyDishonoredEffect() {
+        if(!this.card || this.card.type !== CardTypes.Character) {
+            return;
+        }
+        let effect = {
+            match: this.card,
+            effect: AbilityDsl.effects.modifyBothSkills(card => -card.getGlory())
+        };
+        this.persistentEffects.push(effect);
+        effect.ref = this.addEffectToEngine(effect);
+    }
+
+    applyHonoredEffect() {
+        if(!this.card || this.card.type !== CardTypes.Character) {
+            return;
+        }
+        let effect = {
+            match: this.card,
+            effect: AbilityDsl.effects.modifyBothSkills(card => card.getGlory())
+        };
+        this.persistentEffects.push(effect);
+        effect.ref = this.addEffectToEngine(effect);
+    }
+
+    applyTaintedEffect() {
+        if(!this.card) {
+            return;
+        }
+        let effects = [];
+        if(this.card.type === CardTypes.Character) {
+            effects.push({
+                match: this.card,
+                effect: AbilityDsl.effects.modifyBothSkills(2),
+                ref: undefined
+            });
+            effects.push({
+                match: this.card,
+                condition: () => !this.card.anyEffect(EffectNames.TaintedStatusDoesNotCostHonor),
+                effect: AbilityDsl.effects.honorCostToDeclare(1),
+                ref: undefined
+            });
+        } else if(this.card.type === CardTypes.Province) {
+            effects.push({
+                match: this.card,
+                effect: AbilityDsl.effects.modifyProvinceStrength(2),
+                ref: undefined
+            });
+            effects.push({
+                match: this.card.controller,
+                effect: AbilityDsl.effects.costToDeclareAnyParticipants({
+                    type: 'defenders',
+                    message: 'loses 1 honor',
+                    cost: (player) => AbilityDsl.actions.loseHonor({
+                        target: player,
+                        amount: 1
+                    })
+                }),
+                ref: undefined
+            });
+        } else {
+            return;
+        }
+        effects.forEach(effect => {
+            this.persistentEffects.push(effect);
+            effect.ref = this.addEffectToEngine(effect);
+        });
     }
 }
 
