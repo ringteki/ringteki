3e98f4ba9d9871bacdafd792a08b35bca270d2e2
Marc Soiferman <t>
2021-05-21 22:15:51 -0500

    Endless Ranks

---
+++ server/game/cards/16-UFLS-Oni/EndlessRanks.js
@@ -0,0 +1,30 @@
+const { Players, CardTypes, Locations } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
+const BaseOni = require('./BaseOni.js');
+
+class EndlessRanks extends BaseOni {
+    setupCardAbilities() {
+        super.setupCardAbilities();
+        this.reaction({
+            title: 'Bow a character',
+            when: {
+                afterConflict: (event, context) => {
+                    return event.conflict.winner === context.source.controller && context.source.isParticipating();
+                }
+            },
+            target: {
+                controller: Players.Self,
+                cardType: CardTypes.Character,
+                location: Locations.DynastyDiscardPile,
+                gameAction: AbilityDsl.actions.moveCard(context => ({
+                    target: context.target,
+                    destination: Locations.DynastyDeck
+                }))
+            }
+        });
+    }
+}
+
+EndlessRanks.id = 'endless-ranks';
+
+module.exports = EndlessRanks;
