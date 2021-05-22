0276a32a5dd8c4c9da7a46896cb6386dab4bdbdc
Marc Soiferman <t>
2021-05-22 13:07:37 -0500

    Accursed Summoning

--- server/game/cards/16-UFLS/AccursedSummoning.js
+++ server/game/cards/16-UFLS/AccursedSummoning.js
@@ -1,26 +1,113 @@
 const DrawCard = require('../../drawcard.js');
-const { Locations, CardTypes } = require('../../Constants');
 const AbilityDsl = require('../../abilitydsl.js');
-const OniOfObsidianAndBlood = require('../16-UFLS-Oni/OniOfObsidianAndBlood.js');
+
+const accursedSummoningCost = function () {
+    return {
+        action: { name: 'accursedSummoningCost' },
+        getActionName(context) { // eslint-disable-line no-unused-vars
+            return 'accursedSummoningCost';
+        },
+        getCostMessage: function (context) { // eslint-disable-line no-unused-vars
+            return ['losing {0} honor'];
+        },
+        canPay: function (context) {
+            return context.game.actions.loseHonor().canAffect(context.player, context);
+        },
+        resolve: function (context, result) {
+            let creatures = context.player.outsideTheGameCards;
+            creatures = creatures.filter(card => context.game.actions.putIntoConflict().canAffect(card, context));
+
+            const creaturesByCost = [[], [], [], [], []];
+            creatures.forEach(creature => {
+                creaturesByCost[creature.printedCost].push(creature);
+            });
+            context.costs.accursedSummoningCostCreature = undefined;
+            result.cancelled = false;
+            const promptForCost = () => context.game.promptWithHandlerMenu(context.player, {
+                activePromptTitle: 'Select a fate cost',
+                source: context.source,
+                choices: ['1', '2', '3', '4', 'All', 'Cancel'],
+                handlers: [
+                    () => {
+                        promptForCards(creaturesByCost[1]);
+                    },
+                    () => {
+                        promptForCards(creaturesByCost[2]);
+                    },
+                    () => {
+                        promptForCards(creaturesByCost[3]);
+                    },
+                    () => {
+                        promptForCards(creaturesByCost[4]);
+                    },
+                    () => {
+                        promptForCards(creatures);
+                    },
+                    () => {
+                        context.costs.accursedSummoningCostCreature = undefined;
+                        result.cancelled = true;
+                        return true;
+                    }
+                ]
+            });
+
+            const promptForCards = (creatures) => context.game.promptWithHandlerMenu(context.player, {
+                activePromptTitle: 'Select a creature to summon',
+                source: context.source,
+                cards: creatures,
+                choices: ['Back', 'Cancel'],
+                cardHandler: card => {
+                    context.costs.accursedSummoningCostCreature = card;
+                    context.costs.accursedSummoningCost = card.printedCost;
+                },
+                handlers: [
+                    () => {
+                        promptForCost();
+                        return true;
+                    },
+                    () => {
+                        context.costs.accursedSummoningCostCreature = undefined;
+                        result.cancelled = true;
+                        return true;
+                    }
+                ]
+            });
+
+            promptForCost();
+        },
+        payEvent: function (context) {
+            if(context.costs.accursedSummoningCostCreature) {
+                const oni = context.costs.accursedSummoningCostCreature;
+                const copy = new oni.constructor(context.player, oni.cardData);
+                context.game.allCards.push(copy);
+                context.costs.accursedSummoningCostCreature = copy;
+
+                let events = [];
+                const honorAmount = context.costs.accursedSummoningCost;
+                let honorAction = context.game.actions.loseHonor({ target: context.player, amount: honorAmount });
+                events.push(honorAction.getEvent(context.player, context));
+                return events;
+            }
+            return [];
+        },
+        promptsPlayer: true
+    };
+};
 
 class AccursedSummoning extends DrawCard {
     setupCardAbilities() {
         this.action({
-            title: 'Create an Oni',
-            gameAction: AbilityDsl.actions.sequentialContext(context => {
-                const outOfPlay = context.player.outsideTheGameCards;
-                const oni = outOfPlay[9];
-                const copy = new oni.constructor(context.player, oni.cardData);
-                context.game.allCards.push(copy);
-                return ({
-                    gameActions: [
-                        AbilityDsl.actions.putIntoPlay(() => ({
-                            target: copy
-                        }))
-                    ]
-                });                
-            })
+            title: 'Summon a Shadowlands Creature',
+            cost: [accursedSummoningCost()],
+            condition: context => context.game.isDuringConflict(),
+            gameAction: AbilityDsl.actions.putIntoConflict(context => ({
+                target: context.costs.accursedSummoningCostCreature || context.player.outsideTheGameCards[1]
+            }))
         });
+    }
+
+    isTemptationsMaho() {
+        return true;
     }
 }
 
