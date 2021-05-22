382fee16eb7ac5d974945fdbff2e3ea9edd4743b
Marc Soiferman <t>
2021-05-21 19:54:05 -0500

    Bog Hag

---
+++ server/game/cards/16-UFLS-Oni/BogHag.js
@@ -0,0 +1,23 @@
+const AbilityDsl = require('../../abilitydsl.js');
+const BaseOni = require('./BaseOni.js');
+
+class BogHag extends BaseOni {
+    setupCardAbilities() {
+        super.setupCardAbilities();
+        this.reaction({
+            title: 'Resolve the contested ring',
+            when: {
+                afterConflict: (event, context) => {
+                    return event.conflict.winner === context.source.controller && context.source.isParticipating() && context.player.opponent;
+                }
+            },
+            gameAction: AbilityDsl.actions.discardCard(context => ({ target: context.player.opponent.conflictDeck.first(8) })),
+            effect: 'discard the top 8 cards of {1}\'s conflict deck',
+            effectArgs: context => [context.player.opponent]
+        });
+    }
+}
+
+BogHag.id = 'bog-hag';
+
+module.exports = BogHag;
