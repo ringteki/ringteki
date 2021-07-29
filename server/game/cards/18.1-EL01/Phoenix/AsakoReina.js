1b6422c8d6997dd08596c5935e47f43bc7eef051
Iestyn Jones <66386076+lancerusso@users.noreply.github.com>
2021-07-29 21:52:19 +0100

    Update AsakoReina.js

--- server/game/cards/18.1-EL01/Phoenix/AsakoReina.js
+++ server/game/cards/18.1-EL01/Phoenix/AsakoReina.js
@@ -14,53 +14,53 @@ class AsakoReina extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Gain boons based based on your currently claimed rings',
-                gameAction: AbilityDsl.actions.multiple([
-                    AbilityDsl.actions.gainHonor(
-                        context=>
-                        ({ 
-                            condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.air)].isConsideredClaimed(context.player),
-                            amount: 1
-                        })
-                        ),
-                    AbilityDsl.actions.draw(
-                                context=>({
-                                    condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.earth)].isConsideredClaimed(context.player),
-                                    amount: 1
-                                })
-                        ),                                  
-                    AbilityDsl.actions.gainFate(
-                                context=>({
-                                    condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.void)].isConsideredClaimed(context.player),
-                                    amount: 1
-                                })
-                                ),
-                                AbilityDsl.actions.selectCard(
-                            context=>({
-                                activePromptTitle: 'Choose a 2 cost or lower character to ready',
-                                condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.water)].isConsideredClaimed(context.player),
-                                cardCondition: card => card.costLessThan(3),
-                                cardType: CardTypes.Character,
-                                    optional: false,
-                                    gameAction: AbilityDsl.actions.ready(),
-                                    targets: false,
-                                    message: '{0} chooses to ready {1} with {2}\'s effect',
-                                    messageArgs: (card, player) => [player, card, context.source]                               
-                                })
-                            ),
-                            AbilityDsl.actions.selectCard(
-                                context=>({
-                                    activePromptTitle: 'Choose a character to honor',
-                                    condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.fire)].isConsideredClaimed(context.player),
-                                        cardType: CardTypes.Character,
-                                        optional: false,
-                                        gameAction: AbilityDsl.actions.honor(),
-                                        targets: false,
-                                        message: '{0} chooses to honor {1} with {2}\'s effect',
-                                        messageArgs: (card, player) => [player, card, context.source]                                   
-                                    })
-                                ),
-                            
-                            ])
+            gameAction: AbilityDsl.actions.multiple([
+                AbilityDsl.actions.gainHonor(
+                    context =>
+                    ({
+                        condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.air)].isConsideredClaimed(context.player),
+                        amount: 1
+                    })
+                ),
+                AbilityDsl.actions.draw(
+                    context => ({
+                        condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.earth)].isConsideredClaimed(context.player),
+                        amount: 1
+                    })
+                ),
+                AbilityDsl.actions.gainFate(
+                    context => ({
+                        condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.void)].isConsideredClaimed(context.player),
+                        amount: 1
+                    })
+                ),
+                AbilityDsl.actions.selectCard(
+                    context => ({
+                        activePromptTitle: 'Choose a 2 cost or lower character to ready',
+                        condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.water)].isConsideredClaimed(context.player),
+                        cardCondition: card => card.costLessThan(3),
+                        cardType: CardTypes.Character,
+                        optional: false,
+                        gameAction: AbilityDsl.actions.ready(),
+                        targets: false,
+                        message: '{0} chooses to ready {1} with {2}\'s effect',
+                        messageArgs: (card, player) => [player, card, context.source]
+                    })
+                ),
+                AbilityDsl.actions.selectCard(
+                    context => ({
+                        activePromptTitle: 'Choose a character to honor',
+                        condition: context.game.rings[this.getCurrentElementSymbol(elementKeys.fire)].isConsideredClaimed(context.player),
+                        cardType: CardTypes.Character,
+                        optional: false,
+                        gameAction: AbilityDsl.actions.honor(),
+                        targets: false,
+                        message: '{0} chooses to honor {1} with {2}\'s effect',
+                        messageArgs: (card, player) => [player, card, context.source]
+                    })
+                ),
+
+            ])
         });
     }
     getPrintedElementSymbols() {
@@ -80,13 +80,13 @@ class AsakoReina extends DrawCard {
             prettyName: 'honor a character',
             element: Elements.Fire
         });
-        
+
         symbols.push({
             key: elementKeys.water,
             prettyName: 'ready a 2 cost or less character',
             element: Elements.Water
         });
-        
+
         symbols.push({
             key: elementKeys.void,
             prettyName: '+1 fate',
@@ -98,4 +98,3 @@ class AsakoReina extends DrawCard {
 
 AsakoReina.id = 'asako-reina';
 module.exports = AsakoReina;
-
