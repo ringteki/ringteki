7f0c3c03ab4ca834896339ae7c2bb6588b6f3b1f
Marc Soiferman <t>
2021-08-10 20:57:50 -0500

    Conduit

--- server/game/cards/18.1-EL01/Phoenix/ConduitOfTheElements.js
+++ server/game/cards/18.1-EL01/Phoenix/ConduitOfTheElements.js
@@ -1,20 +1,64 @@
 const DrawCard = require('../../../drawcard.js');
 const AbilityDsl = require('../../../abilitydsl.js');
-const { TargetModes } = require('../../../Constants.js');
+const { Durations, Elements } = require('../../../Constants');
 
 class ConduitOfTheElements extends DrawCard {
     setupCardAbilities() {
-        this.action({
-            title: 'Switch the contested ring',
-            condition: context => context.source.isParticipating(),
-            target: {
-                mode: TargetModes.Ring,
-                activePromptTitle: 'Choose a claimed ring',
-                ringCondition: (ring, context) => ring.claimedBy === context.player.name,
-                gameAction: AbilityDsl.actions.switchConflictElement()
+        this.reaction({
+            title: 'Give an element to the contested ring',
+            when: {
+                onCardPlayed: (event, context) => event.player === context.player &&
+                    context.source.isParticipating() &&
+                    event.card.hasTrait('kiho') &&
+                    (event.card.hasTrait('air') || event.card.hasTrait('earth') || event.card.hasTrait('fire') ||
+                    event.card.hasTrait('void') || event.card.hasTrait('water')) &&
+                    this.game.isDuringConflict()
             },
-            effect: 'switch the contested ring with the {0}'
+            gameAction: AbilityDsl.actions.ringLastingEffect(context => ({
+                duration: Durations.UntilEndOfConflict,
+                target: context.game.currentConflict.ring,
+                effect: AbilityDsl.effects.addElement(this.getElementsOfPlayedCard(context))
+            })),
+            effect: 'add {1} to the conflict ring',
+            effectArgs: context => [this.getElements(context)],
         });
+    }
+
+    getElementsOfPlayedCard(context) {
+        let elements = [];
+        const card = context.event.card;
+        const eArray = [Elements.Air, Elements.Earth, Elements.Fire, Elements.Water, Elements.Void];
+        eArray.forEach(e => {
+            if (card.hasTrait(e)) {
+                elements.push(e);
+            }
+        })
+        return elements;
+    }
+
+    getElements(context) {
+        const capitalize = {
+            air: 'Air',
+            water: 'Water',
+            earth: 'Earth',
+            fire: 'Fire',
+            void: 'Void'
+        };
+
+        let string = '';
+        let elements = this.getElementsOfPlayedCard(context);
+        for(let i = 0; i < elements.length; i++) {
+            if(i !== 0) {
+                if(i === elements.length - 1) {
+                    string += ' and ';
+                } else {
+                    string += ', ';
+                }
+            }
+            string += capitalize[elements[i]];
+        }
+
+        return string;
     }
 }
 
--- test/server/cards/18.1-EL01/Phoenix/ConduitOfTheElements.spec.js
+++ test/server/cards/18.1-EL01/Phoenix/ConduitOfTheElements.spec.js
@@ -4,7 +4,8 @@ describe('Conduit of the Elements', function() {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
-                    inPlay: ['conduit-of-the-elements']
+                    inPlay: ['conduit-of-the-elements'],
+                    hand: ['hurricane-punch', 'iron-foundations-stance', 'kyofuki-s-hammer']
                 },
                 player2: {
                     inPlay: []
@@ -12,39 +13,70 @@ describe('Conduit of the Elements', function() {
             });
 
             this.conduit = this.player1.findCardByName('conduit-of-the-elements');
-            this.player1.claimRing('fire');
-            this.player2.claimRing('water');
+            this.punch = this.player1.findCardByName('hurricane-punch');
+            this.stance = this.player1.findCardByName('iron-foundations-stance');
+            this.hammer = this.player1.findCardByName('kyofuki-s-hammer');
         });
 
-        it('should prompt you to select a ring you have claimed', function() {
+        it('should add elements to rings if you play a kiho', function() {
             this.noMoreActions();
             this.initiateConflict({
                 attackers: [this.conduit],
                 defenders: [],
-                ring: 'air'
+                ring: 'water'
             });
             this.player2.pass();
+            expect(this.game.rings.water.hasElement('water')).toBe(true);
+            expect(this.game.rings.water.hasElement('air')).toBe(false);
+
+            this.player1.clickCard(this.punch);
             this.player1.clickCard(this.conduit);
-            expect(this.player1).not.toBeAbleToSelectRing('air');
-            expect(this.player1).not.toBeAbleToSelectRing('earth');
-            expect(this.player1).toBeAbleToSelectRing('fire');
-            expect(this.player1).not.toBeAbleToSelectRing('water');
-            expect(this.player1).not.toBeAbleToSelectRing('void');
+            this.player1.clickCard(this.conduit);
+
+            expect(this.game.rings.water.hasElement('water')).toBe(true);
+            expect(this.game.rings.water.hasElement('air')).toBe(true);
+
+            expect(this.getChatLogs(5)).toContain('player1 uses Conduit of the Elements to add Air to the conflict ring');
         });
 
-        it('should swap the ring', function() {
+        it('should add elements to rings if you play a kiho', function() {
             this.noMoreActions();
             this.initiateConflict({
                 attackers: [this.conduit],
                 defenders: [],
-                ring: 'air'
+                ring: 'water'
             });
             this.player2.pass();
-            expect(this.game.currentConflict.ring.element).toBe('air');
+            expect(this.game.rings.water.hasElement('water')).toBe(true);
+            expect(this.game.rings.water.hasElement('earth')).toBe(false);
+
+            this.player1.clickCard(this.stance);
             this.player1.clickCard(this.conduit);
-            this.player1.clickRing('fire');
-            expect(this.game.currentConflict.ring.element).toBe('fire');
-            expect(this.getChatLogs(5)).toContain('player1 uses Conduit of the Elements to switch the contested ring with the Fire Ring');
+            this.player1.clickCard(this.conduit);
+
+            expect(this.game.rings.water.hasElement('water')).toBe(true);
+            expect(this.game.rings.water.hasElement('earth')).toBe(true);
+
+            expect(this.getChatLogs(5)).toContain('player1 uses Conduit of the Elements to add Earth to the conflict ring');
+        });
+
+        it('should not add elements to rings if you play a non-kiho', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.conduit],
+                defenders: [],
+                ring: 'water'
+            });
+            this.player2.pass();
+            expect(this.game.rings.water.hasElement('water')).toBe(true);
+            expect(this.game.rings.water.hasElement('earth')).toBe(false);
+
+            this.player1.clickCard(this.hammer);
+            this.player1.clickCard(this.conduit);
+            this.player1.clickCard(this.conduit);
+
+            expect(this.game.rings.water.hasElement('water')).toBe(true);
+            expect(this.game.rings.water.hasElement('earth')).toBe(false);
         });
     });
 });
