c8a30dbfbcf905e47c5132a837e1e28c080317c9
Tom Nys <tom-nys@hotmail.com>
2022-09-07 22:25:56 +0200

    Mantis Raider

---
+++ server/game/cards/19-ELCore/Neutral/MantisRaider.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../../drawcard.js');
+const { TargetModes } = require('../../../Constants');
+const AbilityDsl = require('../../../abilitydsl');
+
+class MantisRaider extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Gain all fate from a ring',
+            condition: context => context.game.isDuringConflict(),
+            target: {
+                mode: TargetModes.Ring,
+                ringCondition: ring => ring.fate != 0,
+                activePromptTitle: 'Choose a ring to gain all fate from',
+                gameAction: AbilityDsl.actions.takeFateFromRing()
+            }
+        });
+    }
+}
+
+MantisRaider.id = 'mantis-raider';
+
+module.exports = MantisRaider;
