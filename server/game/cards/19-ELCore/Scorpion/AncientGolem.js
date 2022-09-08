d6afc229544d4432b243f77430d2785e645c77b1
Tom Nys <tom-nys@hotmail.com>
2022-09-08 20:22:03 +0200

    Ancient Golem

---
+++ server/game/cards/19-ELCore/Scorpion/AncientGolem.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../../drawcard.js');
+const { TargetModes, CardTypes, Durations } = require('../../../Constants');
+const AbilityDsl = require('../../../abilitydsl');
+
+class AncientGolem extends DrawCard {
+    setupCardAbilities() {
+        this.persistentEffect({
+            effect: [
+                AbilityDsl.effects.cardCannot('declareAsAttacker')
+            ]
+        });
+
+        this.interrupt({
+            title: 'Dishonor a character and draw a card',
+            anyPlayer: true,
+            when: {
+                onCardLeavesPlay: (event, context) => event.card === context.source 
+            },
+            cost: AbilityDsl.costs.dishonor(),
+            gameAction: AbilityDsl.actions.draw()
+        });
+    }
+}
+
+AncientGolem.id = 'ancient-golem';
+
+module.exports = AncientGolem;
