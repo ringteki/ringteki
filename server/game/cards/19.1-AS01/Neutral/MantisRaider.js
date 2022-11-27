ccf2836eda566de327c7a080dd7e7c64f3185128
Luiz Sócrate <luiz@socrate.com.br>
2022-11-28 00:41:36 +0100

    New Mantis Raider

--- server/game/cards/19.1-AS01/Neutral/MantisRaider.js
+++ server/game/cards/19.1-AS01/Neutral/MantisRaider.js
@@ -1,20 +1,32 @@
 const DrawCard = require('../../../drawcard.js');
 const AbilityDsl = require('../../../abilitydsl');
-const { TargetModes } = require('../../../Constants.js');
 
 class MantisRaider extends DrawCard {
     setupCardAbilities() {
+        this.reaction({
+            title: 'Steal a fate',
+            when: {
+                onDefendersDeclared: (event, context) =>
+                    context.source.isAttacking() &&
+                    event.conflict.defenders.length === 0
+            },
+            effect: 'take a fate from {1} and place it on {0}.',
+            effectArgs: (context) => context.player.opponent,
+            gameAction: AbilityDsl.actions.placeFate((context) => ({
+                origin: context.player.opponent
+            }))
+        });
+
         this.action({
-            title: 'Ready this character',
-            condition: context => context.source.isParticipating(),
-            target: {
-                mode: TargetModes.Ring,
-                ringCondition: ring => ring.fate > 0,
-                gameAction: AbilityDsl.actions.takeFateFromRing(context => ({
-                    target: context.ring,
-                    amount: context.ring.fate
-                }))
-            }
+            title: 'Give this character +2/+2',
+            condition: () => this.game.isDuringConflict(),
+            cost: AbilityDsl.costs.removeFateFromSelf(),
+            effect: 'give himself +2{1}/+2{2}',
+            effectArgs: () => ['military', 'political'],
+            gameAction: AbilityDsl.actions.cardLastingEffect({
+                effect: AbilityDsl.effects.modifyBothSkills(2)
+            }),
+            limit: AbilityDsl.limit.perConflict(2)
         });
     }
 }
