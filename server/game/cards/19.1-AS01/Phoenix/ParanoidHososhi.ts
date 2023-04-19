05c6a5dea4e7d8319aadd82327e120eeb90c8f52
Luiz Socrate <luiz@socrate.com.br>
2023-04-19 20:38:45 +0200

    Update: Paranoid Hososhi ability changed to action

---
+++ server/game/cards/19.1-AS01/Phoenix/ParanoidHososhi.ts
@@ -0,0 +1,38 @@
+import AbilityContext = require('../../../AbilityContext');
+import { CardTypes, Phases, Players } from '../../../Constants';
+import AbilityDsl = require('../../../abilitydsl');
+import BaseCard = require('../../../basecard');
+import DrawCard = require('../../../drawcard');
+
+export default class ParanoidHososhi extends DrawCard {
+    static id = 'paranoid-hososhi';
+
+    public setupCardAbilities() {
+        this.legendary(2);
+
+        this.action({
+            title: 'Steal fate from a character',
+            phase: Phases.Conflict,
+            cost: AbilityDsl.costs.bowSelf(),
+            target: {
+                controller: Players.Any,
+                cardType: CardTypes.Character,
+                cardCondition: (card, context) => card.getCost() === this.getHighestCostOfCharactersInPlay(context),
+                gameAction: AbilityDsl.actions.removeFate((context) => ({
+                    amount: 1,
+                    recipient: context.player
+                }))
+            },
+            effect: 'take 1 fate from {0} — evil begone!'
+        });
+    }
+
+    private getHighestCostOfCharactersInPlay(context: AbilityContext) {
+        return context.game
+            .findAnyCardsInPlay((card: BaseCard) => card.type === CardTypes.Character)
+            .reduce((prevHighestCost: number, card: DrawCard) => {
+                const cost = card.getCost();
+                return typeof cost === 'number' && cost > prevHighestCost ? cost : prevHighestCost;
+            }, 0);
+    }
+}
