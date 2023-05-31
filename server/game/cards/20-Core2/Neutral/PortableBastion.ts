ca2c93071b76193ccc59414abc4a4885955619c5
Luiz Socrate <luiz@socrate.com.br>
2023-05-31 16:40:09 +0200

    NEW: Portable Bastion

---
+++ server/game/cards/20-Core2/Neutral/PortableBastion.ts
@@ -0,0 +1,36 @@
+import { AbilityTypes, CardTypes, Locations, Players } from '../../../Constants';
+import type TriggeredAbilityContext = require('../../../TriggeredAbilityContext');
+import AbilityDsl = require('../../../abilitydsl');
+import type BaseCard = require('../../../basecard');
+import DrawCard = require('../../../drawcard');
+
+export default class PortableBastion extends DrawCard {
+    static id = 'portable-bastion';
+
+    public setupCardAbilities() {
+        this.attachmentConditions({ trait: 'shugenja' });
+
+        this.persistentEffect({
+            location: Locations.Any,
+            targetController: Players.Any,
+            effect: AbilityDsl.effects.reduceCost({
+                amount: 1,
+                targetCondition: (target: BaseCard) => target.type === CardTypes.Character && target.hasTrait('earth'),
+                match: (card: BaseCard, source: any) => card === source
+            })
+        });
+
+        this.whileAttached({
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.WouldInterrupt, {
+                title: 'Move a character to the conflict',
+                condition: (context: TriggeredAbilityContext) => context.source.isParticipating(),
+                when: {
+                    onMoveToConflict: (event: any) => event.card.type === CardTypes.Character
+                },
+                effect: "deny {1}'s movement",
+                effectArgs: (context: TriggeredAbilityContext) => [context.event.card],
+                gameAction: AbilityDsl.actions.cancel()
+            })
+        });
+    }
+}
