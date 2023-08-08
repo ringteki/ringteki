8ae74aef45db7f7631515d48e97ca231d61aa3bc
Luiz Socrate <luiz@socrate.com.br>
2023-08-08 10:00:02 +0200

    NEW: Jade Infused Arrows

---
+++ server/game/cards/20-Core2/Crab/JadeInfusedArrows.ts
@@ -0,0 +1,34 @@
+import type AbilityContext from '../../../AbilityContext';
+import { CardTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class EarthsStagnation extends DrawCard {
+    static id = 'earth-s-stagnation';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Give attached character a skill bonus',
+            condition: (context) => context.game.isDuringConflict() && context.source.parent?.isParticipating(),
+            cost: AbilityDsl.costs.payFate(1),
+            gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
+                target: context.source.parent,
+                effect: AbilityDsl.effects.modifyMilitarySkill(this.#bonusAmount(context))
+            })),
+            effect: 'give +{1}{2} to {3}',
+            effectArgs: (context) => [this.#bonusAmount(context), 'military', context.source.parent],
+            limit: AbilityDsl.limit.unlimitedPerConflict()
+        });
+    }
+
+    #bonusAmount(context: AbilityContext): number {
+        return context.player.opponent?.cardsInPlay.any(
+            (card: DrawCard) =>
+                card.getType() === CardTypes.Character &&
+                card.isParticipating() &&
+                (card.isTainted || card.hasTrait('shadowlands') || card.hasTrait('haunted'))
+        )
+            ? 4
+            : 2;
+    }
+}
