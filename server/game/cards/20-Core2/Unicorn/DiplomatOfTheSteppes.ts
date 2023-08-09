d9f839673358d4fcd484586307ab954cf75718e4
Luiz Socrate <luiz@socrate.com.br>
2023-08-09 12:25:37 +0200

    NEW: Diplomat of the Steppes

---
+++ server/game/cards/20-Core2/Unicorn/DiplomatOfTheSteppes.ts
@@ -0,0 +1,22 @@
+import { ConflictTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class CaptiveAudience extends DrawCard {
+    static id = 'captive-audience';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Change the conflict to military',
+            cost: AbilityDsl.costs.payHonor(1),
+            condition: (context) =>
+                (context.source as DrawCard).isParticipating() &&
+                (context.player.isAttackingPlayer()
+                    ? this.game.currentConflict.attackerSkill > this.game.currentConflict.defenderSkill
+                    : this.game.currentConflict.defenderSkill > this.game.currentConflict.attackerSkill),
+            effect: 'switch the conflict type to {1}',
+            effectArgs: () => 'military',
+            gameAction: AbilityDsl.actions.switchConflictType({ targetConflictType: ConflictTypes.Military })
+        });
+    }
+}
