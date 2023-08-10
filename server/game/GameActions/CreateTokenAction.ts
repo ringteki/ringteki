237629c4422a52268d4b314a4365d452baa3c51c
Luiz Socrate <luiz@socrate.com.br>
2023-08-10 22:23:59 +0200

    NEW: Akodo War College

--- server/game/GameActions/CreateTokenAction.ts
+++ server/game/GameActions/CreateTokenAction.ts
@@ -6,23 +6,26 @@ import BaseCard = require('../basecard');
 import Effects = require('../effects');
 
 export interface CreateTokenProperties extends CardActionProperties {
+    atHome?: boolean
 }
 
 export class CreateTokenAction extends CardGameAction {
     name = 'createToken';
     effect = 'create a token';
     targetType = [CardTypes.Character, CardTypes.Holding, CardTypes.Event];
+    defaultProperties: CreateTokenProperties = { atHome: false };
 
     canAffect(card: BaseCard, context: AbilityContext): boolean {
-        if(!card.isFacedown() || !card.isInProvince() || card.location === Locations.StrongholdProvince) {
+        if (!card.isFacedown() || !card.isInProvince() || card.location === Locations.StrongholdProvince) {
             return false;
-        } else if(!context.game.isDuringConflict('military')) {
+        } else if (!context.game.isDuringConflict('military')) {
             return false;
         }
         return super.canAffect(card, context);
     }
 
     eventHandler(event, additionalProperties = {}): void {
+        let { atHome } = this.getProperties(event.context, additionalProperties) as CreateTokenProperties;
         let context = event.context;
         let card = event.card;
         let token = context.game.createToken(card);
@@ -30,10 +33,12 @@ export class CreateTokenAction extends CardGameAction {
         this.checkForRefillProvince(card, event, additionalProperties);
         card.moveTo(Locations.RemovedFromGame);
         card.owner.moveCard(token, Locations.PlayArea);
-        if(context.player.isAttackingPlayer()) {
-            context.game.currentConflict.addAttacker(token);
-        } else {
-            context.game.currentConflict.addDefender(token);
+        if (!atHome) {
+            if (context.player.isAttackingPlayer()) {
+                context.game.currentConflict.addAttacker(token);
+            } else {
+                context.game.currentConflict.addDefender(token);
+            }
         }
         context.game.actions.cardLastingEffect({
             duration: Durations.UntilEndOfPhase,
@@ -43,7 +48,7 @@ export class CreateTokenAction extends CardGameAction {
                 },
                 message: '{0} returns to the deep',
                 messageArgs: [token],
-                gameAction: context.game.actions.discardFromPlay()    
+                gameAction: context.game.actions.discardFromPlay()
             })
         }).resolve(token, context);
     }
