3802650f076fc6aa53ea6822f7d3a7a4f2b357b6
Luiz Socrate <luiz@socrate.com.br>
2023-08-10 20:48:10 +0200

    NEW: Akodo Yoshitsune

---
+++ server/game/cards/20-Core2/Lion/AkodoYoshitsune.ts
@@ -0,0 +1,31 @@
+import { Locations, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import type { Conflict } from '../../../conflict';
+import DrawCard from '../../../drawcard';
+
+export default class AkodoYoshitsune extends DrawCard {
+    static id = 'akodo-yoshitsune';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            targetLocation: Locations.ConflictDeck,
+            match: (card, context) => context && card === context.player.conflictDeck.first(),
+            effect: AbilityDsl.effects.hideWhenFaceUp()
+        });
+
+        this.persistentEffect({
+            condition: (context) => this.conflictAtKaiuWall(context),
+            targetController: Players.Self,
+            effect: AbilityDsl.effects.showTopConflictCard()
+        });
+
+        this.reaction({
+            title: 'Gain an honor',
+            when: {
+                afterConflict: (event, context) => (event.conflict as Conflict | undefined)?.winner === context.player
+            },
+            gameAction: AbilityDsl.actions.gainHonor((context) => ({ target: context.player })),
+            limit: AbilityDsl.limit.unlimitedPerConflict()
+        });
+    }
+}
