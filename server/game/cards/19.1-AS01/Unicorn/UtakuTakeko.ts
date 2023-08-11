b87eff9950cf428616fb018fc32d3d70bfb4a779
Luiz Socrate <luiz@socrate.com.br>
2023-08-12 00:51:23 +0200

    Update: Utaku Takeko

--- server/game/cards/19.1-AS01/Unicorn/UtakuTakeko.ts
+++ server/game/cards/19.1-AS01/Unicorn/UtakuTakeko.ts
@@ -1,5 +1,4 @@
-import { CardTypes, EventNames, Locations, Players, TargetModes } from '../../../Constants';
-import { EventRegistrar } from '../../../EventRegistrar';
+import { CardTypes, Decks } from '../../../Constants';
 import { PlayCharacterAsIfFromHandAtHome } from '../../../PlayCharacterAsIfFromHand';
 import { PlayDisguisedCharacterAsIfFromHandAtHome } from '../../../PlayDisguisedCharacterAsIfFromHand';
 import AbilityDsl = require('../../../abilitydsl');
@@ -9,25 +8,20 @@ import DrawCard = require('../../../drawcard');
 export default class UtakuTakeko extends DrawCard {
     static id = 'utaku-takeko';
 
-    private charactersLeftPlayThisPhase = new Set<string>();
-    private eventRegistrar?: EventRegistrar;
-
     public setupCardAbilities() {
-        this.eventRegistrar = new EventRegistrar(this.game, this);
-        this.eventRegistrar.register([EventNames.OnPhaseStarted, EventNames.OnCardLeavesPlay]);
-
         this.action({
             title: 'Play character from your discard pile',
-            target: {
-                location: Locations.DynastyDiscardPile,
-                mode: TargetModes.Single,
-                cardType: CardTypes.Character,
-                controller: Players.Self,
-                cardCondition: (card) =>
+
+            gameAction: AbilityDsl.actions.deckSearch(() => ({
+                activePromptTitle: 'Select a character to play',
+                amount: 8,
+                deck: Decks.DynastyDeck,
+                cardCondition: (card, context) =>
+                    card.type === CardTypes.Character &&
                     card.glory >= 1 &&
                     card.isFaction('unicorn') &&
                     !card.isUnique() &&
-                    !this.charactersLeftPlayThisPhase.has(card.name),
+                    AbilityDsl.actions.putIntoPlay().canAffect(card, context),
                 gameAction: AbilityDsl.actions.sequential([
                     AbilityDsl.actions.cardLastingEffect((context) => ({
                         target: context.target,
@@ -37,28 +31,23 @@ export default class UtakuTakeko extends DrawCard {
                         ]
                     })),
                     AbilityDsl.actions.playCard((context) => ({ target: context.target }))
-                ])
-            },
+                ]),
+                shuffle: true
+            })),
             effect: 'recall a {1} relative who is {2} {3}',
-            effectArgs: (context) => [this.msgDistance(context.target), this.msgArticle(context.target), context.target]
+            effectArgs: (context) => [
+                this.#msgDistance(context.target),
+                this.#msgArticle(context.target),
+                context.target
+            ]
         });
     }
 
-    public onPhaseStarted() {
-        this.charactersLeftPlayThisPhase.clear();
-    }
-
-    public onCardLeavesPlay(event: any) {
-        if (event.card.type === CardTypes.Character && event.card.controller === this.controller) {
-            this.charactersLeftPlayThisPhase.add(event.card.name);
-        }
-    }
-
-    private msgDistance(card: BaseCard): string {
+    #msgDistance(card: BaseCard): string {
         return card.hasTrait('gaijin') ? 'very distant' : 'distant';
     }
 
-    private msgArticle(card: BaseCard): string {
+    #msgArticle(card: BaseCard): string {
         if (card.hasTrait('army')) {
             return 'in the';
         }
