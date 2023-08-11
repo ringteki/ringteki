843f552d7a45db06468539f1934f79857c13f76d
Luiz Socrate <luiz@socrate.com.br>
2023-08-12 00:40:57 +0200

    Updated: Deployed Garrison

---
+++ server/game/cards/18.1-EL01/Crab/DeployedGarrison.ts
@@ -0,0 +1,65 @@
+import type AbilityContext from '../../../AbilityContext';
+import { CardTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import type BaseCard from '../../../basecard';
+import type { Conflict } from '../../../conflict';
+import DrawCard from '../../../drawcard';
+import type ProvinceCard from '../../../provincecard';
+
+export default class DeployedGarrison extends DrawCard {
+    static id = 'deployed-garrison';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            effect: AbilityDsl.effects.cardCannot({
+                cannot: 'applyCovert',
+                restricts: 'opponentsCardEffects'
+            })
+        });
+
+        this.reaction({
+            title: 'Does not bow at the end of the conflict',
+            when: {
+                afterConflict: (event, context) =>
+                    context.player.isDefendingPlayer() &&
+                    event.conflict.winner === context.source.controller &&
+                    context.source.isParticipating() &&
+                    this.#conflictNearHolding(context)
+            },
+            gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
+                target: context.source,
+                effect: AbilityDsl.effects.doesNotBow()
+            })),
+            effect: 'not bow during the conflict resolution'
+        });
+    }
+
+    #conflictNearHolding(context: AbilityContext) {
+        if (!context.player.isDefendingPlayer()) {
+            return false;
+        }
+
+        const attackedProvinces = (context.game.currentConflict as Conflict).getConflictProvinces();
+        const nearbyProvinces: ProvinceCard[] = context.player.getProvinces((province: ProvinceCard) => {
+            for (const attackedProvince of attackedProvinces) {
+                if (
+                    attackedProvince === province ||
+                    context.player.areLocationsAdjacent(attackedProvince.location, province.location)
+                ) {
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        for (const province of nearbyProvinces) {
+            for (const card of context.player.getDynastyCardsInProvince(province.location) as BaseCard[]) {
+                if (card.isFaceup() && card.type === CardTypes.Holding) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
