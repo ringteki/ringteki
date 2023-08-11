3b44740fbb6af9e93c1c9cfea738cbd4d9ccad71
Luiz Socrate <luiz@socrate.com.br>
2023-08-12 00:24:22 +0200

    Updated: Bake Kujira

---
+++ server/game/cards/18.1-EL01/Neutral/BakeKujira.ts
@@ -0,0 +1,34 @@
+import AbilityContext from '../../../AbilityContext';
+import { CardTypes, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class BakeKujira extends DrawCard {
+    static id = 'bake-kujira';
+
+    setupCardAbilities() {
+        this.legendary(1);
+
+        this.reaction({
+            title: 'Eat a character',
+            when: {
+                afterConflict: (event, context) =>
+                    event.conflict.winner === context.source.controller && context.source.isParticipating()
+            },
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Any,
+                cardCondition: (card, context) => card.isParticipating() && card !== context.source,
+                gameAction: AbilityDsl.actions.conditional({
+                    condition: (context) => this.#shouldDiscardTarget(context),
+                    trueGameAction: AbilityDsl.actions.discardFromPlay(),
+                    falseGameAction: AbilityDsl.actions.removeFate()
+                })
+            }
+        });
+    }
+
+    #shouldDiscardTarget(context: AbilityContext): boolean {
+        return context.target.getFate() === 0;
+    }
+}
