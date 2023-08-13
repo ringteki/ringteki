022940009405e5e55e8f662ba967a80fa71d49ad
Luiz Socrate <luiz@socrate.com.br>
2023-08-13 18:11:48 +0200

    NEW: Cantor of Gales

---
+++ server/game/cards/20-Core2/Phoenix/CantorOfGales.ts
@@ -0,0 +1,14 @@
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class CantorOfGales extends DrawCard {
+    static id = 'cantor-of-gales';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            condition: (context) =>
+                context.player.cardsInPlay.any((card: DrawCard) => card.isParticipating() && card.isHonored),
+            effect: AbilityDsl.effects.changePlayerSkillModifier(2)
+        });
+    }
+}
