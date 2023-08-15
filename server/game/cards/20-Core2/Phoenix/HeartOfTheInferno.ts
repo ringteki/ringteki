2608f270a5ff34df22405b55ac6c91b7a1ac3d62
Luiz Socrate <luiz@socrate.com.br>
2023-08-15 22:48:51 +0200

    NEW: Heart of the Inferno

---
+++ server/game/cards/20-Core2/Phoenix/HeartOfTheInferno.ts
@@ -0,0 +1,35 @@
+import { CardTypes, TargetModes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import type BaseCard from '../../../basecard';
+import DrawCard from '../../../drawcard';
+
+export default class HeartOfTheInferno extends DrawCard {
+    static id = 'heart-of-the-inferno';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Bow a card',
+            condition: (context) =>
+                context.player.anyCardsInPlay((card: DrawCard) => card.isParticipating() && card.hasTrait('shugenja')),
+            target: {
+                mode: TargetModes.Single,
+                cardCondition: (card: BaseCard) => card.attachments.size() === 0 && card.isParticipating(),
+                gameAction: [
+                    AbilityDsl.actions.bow(),
+                    AbilityDsl.actions.conditional({
+                        condition: (context) =>
+                            context.player.anyCardsInPlay(
+                                (card: DrawCard) =>
+                                    card.isParticipating() &&
+                                    card.hasTrait('shugenja') &&
+                                    card.hasTrait('fire') &&
+                                    (context.target as DrawCard).type === CardTypes.Attachment
+                            ),
+                        trueGameAction: AbilityDsl.actions.discardFromPlay(),
+                        falseGameAction: AbilityDsl.actions.noAction()
+                    })
+                ]
+            }
+        });
+    }
+}
