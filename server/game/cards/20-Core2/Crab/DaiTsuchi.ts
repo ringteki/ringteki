8b02ee56bb05370323f8b5c6d93d1dcbba80b3cb
Luiz Socrate <luiz@socrate.com.br>
2023-08-17 23:35:15 +0200

    NEW: Dai Tsuchi

---
+++ server/game/cards/20-Core2/Crab/DaiTsuchi.ts
@@ -0,0 +1,39 @@
+import AbilityContext from '../../../AbilityContext';
+import { AbilityTypes, CardTypes, Durations } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class DaiTsuchi extends DrawCard {
+    static id = 'dai-tsuchi';
+
+    public setupCardAbilities() {
+        this.attachmentConditions({
+            cardCondition: (card) => card instanceof DrawCard && card.printedMilitarySkill >= 3
+        });
+
+        this.whileAttached({
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.Action, {
+                title: 'Return attachment to owners hand',
+                condition: (context: AbilityContext) =>
+                    this.game.isDuringConflict('military') && context.source.isParticipating(),
+                target: {
+                    cardType: CardTypes.Attachment,
+                    cardCondition: (card: DrawCard) =>
+                        card.parent && card.parent.type === CardTypes.Character && card.parent.isParticipating(),
+                    gameAction: AbilityDsl.actions.returnToHand()
+                },
+                gameAction: AbilityDsl.actions.playerLastingEffect((context) => ({
+                    duration: Durations.UntilEndOfConflict,
+                    targetController: context.target.owner,
+                    effect: AbilityDsl.effects.playerCannot({
+                        cannot: 'play',
+                        restricts: 'copiesOfX',
+                        params: context.target.name
+                    })
+                })),
+                effect: "return {0} to {1}'s hand and prevent them from playing copies this conflict",
+                effectArgs: (context: AbilityContext) => [context.target.owner]
+            })
+        });
+    }
+}
