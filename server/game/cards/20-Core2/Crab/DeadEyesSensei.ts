9a54c1ca23702249700bbce3cf7884f1768180a8
Luiz Socrate <luiz@socrate.com.br>
2023-08-17 23:47:50 +0200

    NEW: Dead Eyes Sensei

---
+++ server/game/cards/20-Core2/Crab/DeadEyesSensei.ts
@@ -0,0 +1,29 @@
+import { CardTypes, Durations } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class DeadEyesSensei extends DrawCard {
+    static id = 'dead-eyes-sensei';
+
+    public setupCardAbilities() {
+        this.action({
+            title: 'Ready a character and give them Berserker',
+            cost: AbilityDsl.costs.removeFate({
+                cardType: CardTypes.Character,
+                cardCondition: (card) => card.bowed || !card.hasTrait('berserker')
+            }),
+            effect: 'ready {1} and give them Berserker',
+            effectArgs: (context) => context.costs.removeFate,
+            gameAction: AbilityDsl.actions.multiple([
+                AbilityDsl.actions.ready((context) => ({
+                    target: context.costs.removeFate
+                })),
+                AbilityDsl.actions.cardLastingEffect((context) => ({
+                    target: context.costs.removeFate,
+                    duration: Durations.UntilEndOfPhase,
+                    effect: AbilityDsl.effects.addTrait('creature')
+                }))
+            ])
+        });
+    }
+}
