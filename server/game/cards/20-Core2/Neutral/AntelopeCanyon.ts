559b344a5e46733eca7b24837f4b6fe8a68fb286
Luiz Socrate <luiz@socrate.com.br>
2023-08-20 23:36:19 +0200

    UPDATE: Antelope Canyon

--- server/game/cards/20-Core2/Neutral/AntelopeCanyon.ts
+++ server/game/cards/20-Core2/Neutral/AntelopeCanyon.ts
@@ -1,7 +1,8 @@
-import { CardTypes } from '../../../Constants';
-import AbilityDsl = require('../../../abilitydsl');
-import type DrawCard = require('../../../drawcard');
-import ProvinceCard = require('../../../provincecard');
+import AbilityContext from '../../../AbilityContext';
+import { CardTypes, Durations, Locations } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+import ProvinceCard from '../../../provincecard';
 
 export default class AntelopeCanyon extends ProvinceCard {
     static id = 'antelope-canyon';
@@ -14,14 +15,50 @@ export default class AntelopeCanyon extends ProvinceCard {
                 cardType: CardTypes.Character,
                 cardCondition: (card, context) => card.isDefending() && context.player.opponent !== undefined
             },
-            gameAction: AbilityDsl.actions.lookAt((context) => ({
-                target: context.player.opponent.hand
-                    .shuffle()
-                    .slice(0, (context.target as DrawCard).getCost())
-                    .sort((a: DrawCard, b: DrawCard) => a.name.localeCompare(b.name)),
-                message: '{0} uses {1} with {2}. It reveals {3}',
-                messageArgs: (cards) => [context.player, context.source, context.target, cards]
-            }))
+            gameAction: AbilityDsl.actions.conditional({
+                condition: (context) => context.player.opponent?.hand.size() > 0,
+                falseGameAction: AbilityDsl.actions.noAction(),
+                trueGameAction: AbilityDsl.actions.sequentialContext((context) => {
+                    const opponent = context.player.opponent;
+                    const setAsideCards: DrawCard[] = opponent?.hand.shuffle().slice(0, this.#cardCount(context));
+                    if (setAsideCards.length === 0) {
+                        return { gameActions: [AbilityDsl.actions.noAction()] };
+                    }
+
+                    return {
+                        gameActions: [
+                            AbilityDsl.actions.handler({
+                                handler: () => {
+                                    this.game.addMessage('{0} sets aside {1}', opponent, setAsideCards);
+                                    for (const card of setAsideCards) {
+                                        opponent.moveCard(card, Locations.RemovedFromGame);
+                                    }
+                                }
+                            }),
+
+                            AbilityDsl.actions.playerLastingEffect({
+                                duration: Durations.UntilEndOfRound,
+                                targetController: opponent,
+                                effect: AbilityDsl.effects.playerDelayedEffect({
+                                    when: { onConflictFinished: () => true },
+                                    gameAction: AbilityDsl.actions.handler({
+                                        handler: (context) => {
+                                            context.game.addMessage('{0} picks back their cards', opponent);
+                                            for (const card of setAsideCards) {
+                                                opponent.moveCard(card, Locations.Hand);
+                                            }
+                                        }
+                                    })
+                                })
+                            })
+                        ]
+                    };
+                })
+            })
         });
+    }
+
+    #cardCount(context: AbilityContext): number {
+        return (context.target as DrawCard).getCost();
     }
 }
