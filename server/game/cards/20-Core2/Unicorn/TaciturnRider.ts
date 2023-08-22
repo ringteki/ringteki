8282d2e2d5f71418ec62eac7de7089ee91acad8c
Luiz Socrate <luiz@socrate.com.br>
2023-08-23 00:12:00 +0200

    FIX: Taciturn Rider

--- server/game/cards/20-Core2/Unicorn/TaciturnRider.ts
+++ server/game/cards/20-Core2/Unicorn/TaciturnRider.ts
@@ -7,9 +7,10 @@ export default class TaciturnRider extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Ready me',
-            condition: context => context.source.isAttacking() && context.game.currentConflict.getParticipants(
-                (card: DrawCard) => card.controller === context.player
-            ).length > 0,
+            condition: (context) =>
+                context.source.isAttacking() &&
+                context.game.currentConflict.getParticipants((card: DrawCard) => card.controller === context.player)
+                    .length === 1,
             gameAction: AbilityDsl.actions.ready()
         });
     }
