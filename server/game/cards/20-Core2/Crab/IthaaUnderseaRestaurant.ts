cfd0de0a4fbc4673d0f06275d1878597b6ab9196
Luiz Socrate <luiz@socrate.com.br>
2023-08-25 00:14:51 +0200

    UPDATE: Ithaa Undersea Restaurant

--- server/game/cards/20-Core2/Crab/IthaaUnderseaRestaurant.ts
+++ server/game/cards/20-Core2/Crab/IthaaUnderseaRestaurant.ts
@@ -1,12 +1,23 @@
-import AbilityDsl = require('../../../abilitydsl');
-import ProvinceCard = require('../../../provincecard');
+import { CardTypes, Players } from "../../../Constants";
+import AbilityDsl from "../../../abilitydsl";
+import DrawCard from "../../../drawcard";
+import ProvinceCard from "../../../provincecard";
 
 export default class IthaaUnderseaRestaurant extends ProvinceCard {
     static id = 'ithaa-undersea-restaurant';
 
     public setupCardAbilities() {
-        this.persistentEffect({
-            effect: AbilityDsl.effects.changePlayerGloryModifier(2)
-        });
+        this.action({
+            title: "Increase province strength",
+            target: {
+                cardType: CardTypes.Character,
+                player: Players.Self,
+                cardCondition: (card: DrawCard) => card.isParticipating(),
+                gameAction: AbilityDsl.actions.cardLastingEffect(context => ({
+                    target: context.source,
+                    effect: AbilityDsl.effects.modifyProvinceStrength((context.target as DrawCard).militarySkill)
+                }))
+            }
+        })
     }
 }
