dbb501f8b7e43f5e7b8d22ffa752db9430a78283
Luiz Socrate <luiz@socrate.com.br>
2023-08-26 12:08:46 +0200

    NEW: Steel on Steel

---
+++ server/game/cards/20-Core2/Neutral/SteelOnSteel.ts
@@ -0,0 +1,23 @@
+import { ConflictTypes, DuelTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class SteelOnSteel extends DrawCard {
+    static id = 'steel-on-steel';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Initiate a military duel, discarding the loser',
+            condition: (context) => context.game.isDuringConflict(ConflictTypes.Military),
+            initiateDuel: {
+                type: DuelTypes.Military,
+                gameAction: duel => AbilityDsl.actions.conditional({
+                    target: duel.loser?.[0],
+                    condition: duel.loser?.[0]?.getFate() > 0,
+                    trueGameAction: AbilityDsl.actions.removeFate(),
+                    falseGameAction: AbilityDsl.actions.discardFromPlay()
+                })
+            }
+        });
+    }
+}
