50ddcdb78577822ca1178e1d1897530adbe1b991
Luiz Socrate <luiz@socrate.com.br>
2023-09-03 21:49:34 +0200

    NEW: Lancer of the 9th Legion

---
+++ server/game/cards/20-Core2/Lion/LancerOfThe9thLegion.ts
@@ -0,0 +1,21 @@
+import { CardTypes, ConflictTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class LancerOfThe9thLegion extends DrawCard {
+    static id = 'lancer-of-the-9th-legion';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Bow a character',
+            condition: (context) =>
+                context.game.isDuringConflict(ConflictTypes.Military) && context.source.isAttacking(),
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: (card, context) =>
+                    card.isParticipating() && card.getMilitarySkill() <= context.source.getMilitarySkill(),
+                gameAction: AbilityDsl.actions.bow()
+            }
+        });
+    }
+}
