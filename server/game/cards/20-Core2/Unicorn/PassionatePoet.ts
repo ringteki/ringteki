806dc6e188d6f52b95d74809eca10da24079ac35
Luiz Socrate <luiz@socrate.com.br>
2023-09-04 23:25:09 +0200

    FIX: Passionate Poet

--- server/game/cards/20-Core2/Unicorn/PassionatePoet.ts
+++ server/game/cards/20-Core2/Unicorn/PassionatePoet.ts
@@ -8,8 +8,8 @@ export default class PassionatePoet extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Give all participating enemies -1/-1',
-            condition: context => context.source.isAttacking(),
-            gameAction: AbilityDsl.actions.cardLastingEffect(context => ({
+            condition: (context) => (context.source as DrawCard).isParticipating(),
+            gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
                 target: context.game.currentConflict.getCharacters(context.player.opponent),
                 effect: AbilityDsl.effects.modifyBothSkills(-1),
                 duration: Durations.UntilEndOfConflict
