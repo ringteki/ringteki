0225e43bd0635b2e807f57ad1b6f170da50eb3b1
Marc Soiferman <t>
2023-09-04 23:27:46 -0500

    Collector of Favors

---
+++ server/game/cards/20-Core2/Crane/CollectorOfFavors.ts
@@ -0,0 +1,20 @@
+import { AbilityTypes } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class CollectorOfFavors extends DrawCard {
+    static id = 'collector-of-favors';
+
+    setupCardAbilities() {
+        this.whileAttached({
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.Reaction, {
+                title: 'Gain a fate',
+                when: {
+                    afterConflict: (event, context) =>
+                        event.conflict.winner === context.source.controller && context.source.isParticipating() && context.source.hasTrait('courtier')
+                },
+                gameAction: AbilityDsl.actions.gainFate()
+            })
+        });
+    }
+}
