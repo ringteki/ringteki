9231120e3cf8ff6e9342531b3ef3721d92b23198
Marc Soiferman <t>
2023-09-04 19:41:06 -0500

    Influential Actor

---
+++ server/game/cards/20-Core2/Crane/InfluentialActor.ts
@@ -0,0 +1,27 @@
+import { CardTypes, Durations, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class InfluentialActor extends DrawCard {
+    static id = 'influential-actor';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Move a status token',
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                gameAction: AbilityDsl.actions.selectToken((context) => ({
+                    card: context.source,
+                    activePromptTitle: 'Which token do you wish to move?',
+                    message: '{0} moves {1} to {2}',
+                    messageArgs: (token, player) => [player, token, context.target],
+                    gameAction: AbilityDsl.actions.moveStatusToken(() => ({
+                        recipient: context.target
+                    }))
+                })),
+            },
+            effect: 'move a status token on it to {0}'
+        });
+    }
+}
