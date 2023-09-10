af8e9cc310eab80cb07c795aeed7dbce828b1ec9
Marc Soiferman <t>
2023-09-10 15:10:18 -0500

    Fix wisdom of the wind

--- server/game/cards/20-Core2/Phoenix/WisdomOfTheWind.ts
+++ server/game/cards/20-Core2/Phoenix/WisdomOfTheWind.ts
@@ -11,40 +11,25 @@ export default class WisdomOfTheWind extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Give attached character a skill bonus',
+            effect: 'honor or dishonor {0}',
             condition: (context) =>
                 context.game.isDuringConflict() &&
                 context.player.anyCardsInPlay((card: DrawCard) => card.hasTrait('shugenja')),
-            targets: {
-                [CHARACTER]: {
-                    cardType: CardTypes.Character,
-                    mode: TargetModes.Single,
-                    cardCondition: (card: DrawCard) => card.isParticipating(),
-                    gameAction: [
-                        AbilityDsl.actions.bow(),
-                        AbilityDsl.actions.conditional({
-                            condition: (context) =>
-                                context.player.anyCardsInPlay(
-                                    (card: DrawCard) =>
-                                        card.isParticipating() &&
-                                        card.hasTrait('shugenja') &&
-                                        card.hasTrait('fire') &&
-                                        (context.target as DrawCard).type === CardTypes.Attachment
-                                ),
-                            trueGameAction: AbilityDsl.actions.discardFromPlay(),
-                            falseGameAction: AbilityDsl.actions.noAction()
-                        })
-                    ]
-                },
-                [EFFECT]: {
-                    mode: TargetModes.Select,
-                    dependsOn: CHARACTER,
-                    choices: {
-                        'Honor it': AbilityDsl.actions.honor((context) => ({ target: context.targets[CHARACTER] })),
-                        'Dishonor it': AbilityDsl.actions.dishonor((context) => ({
-                            target: context.targets[CHARACTER]
-                        }))
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: (card: DrawCard) => card.isParticipating(),
+                gameAction: AbilityDsl.actions.chooseAction({
+                    options: {
+                        'Honor this character': {
+                            action: AbilityDsl.actions.honor(),
+                            message: '{0} chooses to honor {1}'
+                        },
+                        'Dishonor this character': {
+                            action: AbilityDsl.actions.dishonor(),
+                            message: '{0} chooses to dishonor {1}'
+                        }
                     }
-                }
+                })
             },
             then: {
                 gameAction: AbilityDsl.actions.conditional({
@@ -52,15 +37,15 @@ export default class WisdomOfTheWind extends DrawCard {
                         context.player.anyCardsInPlay(
                             (card: DrawCard) => card.hasTrait('shugenja') && card.hasTrait('air')
                         ),
+                    // TODO: This needs to be a lasting effect on the conflict, not only the current participating characters
                     trueGameAction: AbilityDsl.actions.chooseAction({
                         activePromptTitle: 'Make other status tokens be ignored?',
                         player: Players.Self,
                         options: {
                             Yes: {
                                 action: AbilityDsl.actions.cardLastingEffect((context) => ({
-                                    target: context.player.cardsInPlay.filter(
-                                        (card: DrawCard) =>
-                                            card.isParticipating() && card !== context.targets[CHARACTER]
+                                    target: context.game.currentConflict.getAttackers().concat(context.game.currentConflict.getDefenders()).filter(
+                                        (card: DrawCard) => card !== context.target
                                     ),
                                     effect: [
                                         AbilityDsl.effects.honorStatusDoesNotModifySkill(),
