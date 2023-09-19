60c64eacf2a119f6607df18122e4d3a0c5c2cdc6
Luiz Socrate <luiz@socrate.com.br>
2023-09-19 21:54:26 +0200

    NEW: Borderlands Defender 2

---
+++ server/game/cards/20-Core2/Crab/BorderlandsDefender2.ts
@@ -0,0 +1,22 @@
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class BorderlandsDefender extends DrawCard {
+    static id = 'borderlands-defender-2';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            condition: (context) => context.source.isDefending(),
+            effect: [
+                AbilityDsl.effects.cardCannot({
+                    cannot: 'sendHome',
+                    restricts: 'opponentsCardEffects'
+                }),
+                AbilityDsl.effects.cardCannot({
+                    cannot: 'bow',
+                    restricts: 'opponentsCardEffects'
+                })
+            ]
+        });
+    }
+}
