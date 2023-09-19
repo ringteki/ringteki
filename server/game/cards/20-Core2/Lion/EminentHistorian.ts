96df1534793f57584b057f5f71a9d4fcb364bbe5
Luiz Socrate <luiz@socrate.com.br>
2023-09-19 23:50:58 +0200

    NEW: Eminent Historian

---
+++ server/game/cards/20-Core2/Lion/EminentHistorian.ts
@@ -0,0 +1,21 @@
+import { CardTypes, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class EminentHistorian extends DrawCard {
+    static id = 'eminent-historian';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Honor a character',
+            condition: (context) =>
+                context.source.isParticipating() && context.player.opponent && context.player.isMoreHonorable(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                cardCondition: (card) => card.isParticipating(),
+                gameAction: AbilityDsl.actions.honor()
+            }
+        });
+    }
+}
