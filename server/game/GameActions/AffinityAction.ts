3bcf08a761e020851891a8363d01ce7ad8abab36
Luiz Socrate <luiz@socrate.com.br>
2023-09-27 23:51:44 +0200

    Add noAffinityAction

--- server/game/GameActions/AffinityAction.ts
+++ server/game/GameActions/AffinityAction.ts
@@ -3,8 +3,9 @@ import type { GameObject } from '../GameObject';
 import { GameAction, type GameActionProperties } from './GameAction';
 
 export interface AffinityActionProperties extends GameActionProperties {
-    trait: string;
     gameAction: GameAction;
+    trait: string;
+    noAffinityGameAction?: GameAction;
     promptTitleForConfirmingAffinity?: string;
 }
 
@@ -14,32 +15,41 @@ export class AffinityAction extends GameAction {
     getProperties(context: AbilityContext, additionalProperties = {}): AffinityActionProperties {
         let properties = super.getProperties(context, additionalProperties) as AffinityActionProperties;
         properties.gameAction.setDefaultTarget(() => properties.target);
+        properties.noAffinityGameAction?.setDefaultTarget(() => properties.target);
         return properties;
     }
 
-    getEffectMessage(context: AbilityContext): [string, any[]] {
-        return this.getProperties(context).gameAction.getEffectMessage(context);
+    getEffectMessage(context: AbilityContext, additionalProperties = {}): [string, any[]] {
+        let properties = this.getProperties(context, additionalProperties);
+        if (context.player.hasAffinity(properties.trait)) {
+            return properties.gameAction.getEffectMessage(context);
+        }
+
+        return properties.noAffinityGameAction?.getEffectMessage(context) ?? ['', []];
     }
 
     hasLegalTarget(context: AbilityContext, additionalProperties = {}) {
-        return this.getProperties(context, additionalProperties).gameAction.hasLegalTarget(
-            context,
-            additionalProperties
-        );
+        let properties = this.getProperties(context, additionalProperties);
+        if (context.player.hasAffinity(properties.trait)) {
+            return properties.gameAction.hasLegalTarget(context, additionalProperties);
+        }
+
+        return properties.noAffinityGameAction?.hasLegalTarget(context, additionalProperties) ?? false;
     }
 
     canAffect(target: GameObject, context: AbilityContext, additionalProperties = {}) {
-        return this.getProperties(context, additionalProperties).gameAction.canAffect(
-            target,
-            context,
-            additionalProperties
-        );
+        let properties = this.getProperties(context, additionalProperties);
+        if (context.player.hasAffinity(properties.trait)) {
+            return properties.gameAction.canAffect(target, context, additionalProperties);
+        }
+
+        return properties.noAffinityGameAction?.canAffect(target, context, additionalProperties) ?? false;
     }
 
-    addEventsToArray(events: any[], context: AbilityContext, additionalProperties = {}) {
+    addEventsToArray(events: any[], context: AbilityContext, additionalProperties = {}): void {
         let properties = this.getProperties(context, additionalProperties);
         if (!context.player.hasAffinity(properties.trait)) {
-            return;
+            return properties.noAffinityGameAction?.addEventsToArray(events, context, additionalProperties);
         }
 
         if (!properties.promptTitleForConfirmingAffinity) {
@@ -60,9 +70,12 @@ export class AffinityAction extends GameAction {
     }
 
     hasTargetsChosenByInitiatingPlayer(context: AbilityContext, additionalProperties = {}) {
-        return this.getProperties(context, additionalProperties).gameAction.hasTargetsChosenByInitiatingPlayer(
-            context,
-            additionalProperties
+        let properties = this.getProperties(context, additionalProperties);
+        if (context.player.hasAffinity(properties.trait)) {
+            return properties.gameAction.hasTargetsChosenByInitiatingPlayer(context, additionalProperties);
+        }
+        return (
+            properties.noAffinityGameAction?.hasTargetsChosenByInitiatingPlayer(context, additionalProperties) ?? false
         );
     }
 }
