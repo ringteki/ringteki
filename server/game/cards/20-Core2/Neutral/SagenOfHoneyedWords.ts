e846fb69ea532c66f38f1a6decf48c26bc4461fc
Luiz Socrate <luiz@socrate.com.br>
2023-10-13 23:12:49 +0200

    NEW: Sagen of Honeyed Words

---
+++ server/game/cards/20-Core2/Neutral/SagenOfHoneyedWords.ts
@@ -0,0 +1,24 @@
+import { CardTypes, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class SagenOfHoneyedWords extends DrawCard {
+    static id = 'sagen-of-honeyed-words';
+
+    public setupCardAbilities() {
+        this.action({
+            title: 'Gain a skill bonus based on your company',
+            condition: (context) => context.source.isParticipating(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                cardCondition: (card) => card.isParticipating(),
+            },
+            gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
+                target: context.source,
+                effect: AbilityDsl.effects.modifyMilitarySkill({ amount: context.target.printedGlory })
+            })),
+        });
+    }
+}
+
