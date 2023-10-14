6cf7c24d05939fab35ccba5df60f2f6b3344bebb
Luiz Socrate <luiz@socrate.com.br>
2023-10-14 13:43:59 +0200

    Unearthed Claims

---
+++ server/game/cards/20-Core2/Lion/UnearthedClaims.ts
@@ -0,0 +1,35 @@
+import { CardTypes, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import type DrawCard from '../../../drawcard';
+import { BattlefieldAttachment } from '../../BattlefieldAttachment';
+
+export default class UnearthedClaims extends BattlefieldAttachment {
+    static id = 'unearthed-claims';
+
+    public setupCardAbilities() {
+        super.setupCardAbilities();
+
+        this.action({
+            title: 'Gives Pride to an attacker and a defender',
+            condition: (context) => context.source.parent?.isConflictProvince(),
+            targets: {
+                ownSide: {
+                    cardType: CardTypes.Character,
+                    controller: Players.Self,
+                    cardCondition: (card: DrawCard) => card.isParticipating(),
+                    gameAction: AbilityDsl.actions.cardLastingEffect({
+                        effect: AbilityDsl.effects.addKeyword('pride')
+                    })
+                },
+                otherSide: {
+                    cardType: CardTypes.Character,
+                    controller: Players.Opponent,
+                    cardCondition: (card: DrawCard) => card.isParticipating(),
+                    gameAction: AbilityDsl.actions.cardLastingEffect({
+                        effect: AbilityDsl.effects.addKeyword('pride')
+                    })
+                }
+            }
+        });
+    }
+}
