6d6e947653919490449cea3d7a7a11c31686c8a3
Luiz Socrate <luiz@socrate.com.br>
2023-11-03 17:59:11 +0100

    Test Shosuro Hiroyuki

--- server/game/cards/20-Core2/Scorpion/ShosuroHiroyuki.ts
+++ server/game/cards/20-Core2/Scorpion/ShosuroHiroyuki.ts
@@ -8,20 +8,19 @@ export default class ShosuroHiroyuki extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Force opponent to discard card or dishonor a character',
-            condition: (context) =>
-                context.game.isDuringConflict('political') && (context.source as DrawCard).isParticipating(),
+            condition: (context) => (context.source as DrawCard).isParticipating('political'),
             target: {
                 cardType: CardTypes.Character,
                 controller: Players.Any,
                 cardCondition: (card: DrawCard, context) =>
                     card.isParticipating() && card.politicalSkill < (context.source as DrawCard).politicalSkill,
-                gameAction: AbilityDsl.actions.conditional((context) => ({
-                    condition: (context) => (context.target as DrawCard).isDishonored,
+                gameAction: AbilityDsl.actions.conditional(({ target }: { target: DrawCard }) => ({
+                    condition: () => (target as DrawCard).isDishonored,
                     trueGameAction: AbilityDsl.actions.discardAtRandom({
                         amount: 1,
-                        target: (context.target as DrawCard).controller
+                        target: target.controller
                     }),
-                    falseGameAction: AbilityDsl.actions.dishonor({ target: context.target })
+                    falseGameAction: AbilityDsl.actions.dishonor({ target })
                 }))
             }
         });
