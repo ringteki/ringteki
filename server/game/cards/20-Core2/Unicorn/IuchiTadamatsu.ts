c6238a226a3cc5139892d2520a75faa67ea879b2
Luiz Socrate <luiz@socrate.com.br>
2023-11-05 15:36:53 +0100

    Iuchi Tadamatsu

---
+++ server/game/cards/20-Core2/Unicorn/IuchiTadamatsu.ts
@@ -0,0 +1,25 @@
+import AbilityDsl from '../../../abilitydsl';
+import { CardTypes } from '../../../Constants';
+import DrawCard from '../../../drawcard';
+
+export default class IuchiTadamatsu extends DrawCard {
+    static id = 'iuchi-tadamatsu';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            effect: AbilityDsl.effects.reduceCost({
+                match: (card: DrawCard) => card.hasTrait('meishodo'),
+                targetCondition: (target: DrawCard, source: DrawCard) => target === source
+            })
+        });
+
+        this.action({
+            title: 'Ready this character',
+            cost: AbilityDsl.costs.sacrifice({
+                cardType: CardTypes.Attachment,
+                cardCondition: (card, context) => card.parent === context.source
+            }),
+            gameAction: AbilityDsl.actions.ready()
+        });
+    }
+}
