769ba24969899b7afabcad34b88031272b000f83
Luiz Socrate <luiz@socrate.com.br>
2023-11-05 19:03:05 +0100

    The Rushing Wave

---
+++ server/game/cards/20-Core2/Unicorn/TheRushingWave.ts
@@ -0,0 +1,50 @@
+import AbilityDsl from '../../../abilitydsl';
+import { CardTypes, Durations, Locations } from '../../../Constants';
+import DrawCard from '../../../drawcard';
+import { ProvinceCard } from '../../../ProvinceCard';
+
+function adjacentProvinces(centralProvince: ProvinceCard): Array<ProvinceCard> {
+    return centralProvince.controller.getProvinces((province: ProvinceCard) =>
+        centralProvince.controller.areLocationsAdjacent(centralProvince.location, province.location)
+    );
+}
+
+export default class TheRushingWave extends DrawCard {
+    static id = 'the-rushing-wave';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Set a province to zero strength',
+            condition: (context) =>
+                context.player.cardsInPlay.any(
+                    (card: DrawCard) => card.getType() === CardTypes.Character && card.hasTrait('shugenja')
+                ),
+            target: {
+                location: Locations.Provinces,
+                cardType: CardTypes.Province,
+                gameAction: AbilityDsl.actions.onAffinity({
+                    trait: 'water',
+                    gameAction: AbilityDsl.actions.cardLastingEffect(({ target }: { target: ProvinceCard }) => ({
+                        target: target.controller.getProvinces(
+                            (province: ProvinceCard) =>
+                                target.location === province.location ||
+                                target.controller.areLocationsAdjacent(target.location, province.location)
+                        ),
+                        targetLocation: Locations.Provinces,
+                        duration: Durations.UntilEndOfPhase,
+                        effect: AbilityDsl.effects.setProvinceStrength(0)
+                    })),
+                    noAffinityGameAction: AbilityDsl.actions.cardLastingEffect({
+                        targetLocation: Locations.Provinces,
+                        duration: Durations.UntilEndOfPhase,
+                        effect: AbilityDsl.effects.setProvinceStrength(0)
+                    }),
+                    effect: 'also set the strength of {0} to 0',
+                    effectArgs: (context) => [adjacentProvinces(context.target)]
+                })
+            },
+            effect: "set {1}'s strength to 0 until the end of the phase",
+            effectArgs: (context) => [context.target]
+        });
+    }
+}
---
+++ test/server/cards/20-Core2/Unicorn/TheRushingWave.spec.js
@@ -0,0 +1,35 @@
+describe('The Rushing Wave', function () {
+    integration(function () {
+        beforeEach(function () {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    inPlay: ['adept-of-the-waves', 'miya-mystic'],
+                    hand: ['the-rushing-wave']
+                },
+                player2: {
+                    provinces: ['manicured-garden', 'pilgrimage', 'fertile-fields', 'entrenched-position']
+                }
+            });
+
+            this.adept = this.player1.findCardByName('adept-of-the-waves');
+            this.mystic = this.player1.findCardByName('miya-mystic');
+            this.theRushingWave = this.player1.findCardByName('the-rushing-wave');
+
+            this.pilgrimage = this.player2.findCardByName('pilgrimage');
+        });
+
+        it('should set the province strength to 0', function () {
+            this.player1.clickCard(this.theRushingWave);
+            expect(this.player1).toHavePrompt('Choose a province');
+            this.player1.clickCard(this.pilgrimage);
+            expect(this.pilgrimage.getStrength()).toBe(0);
+            expect(this.getChatLogs(5)).toContain(
+                "player1 plays The Rushing Wave to set Pilgrimage's strength to 0 until the end of the phase"
+            );
+            expect(this.getChatLogs(5)).toContain(
+                'player1 channels their water affinity to also set the strength of Manicured Garden and Fertile Fields to 0'
+            );
+        });
+    });
+});
