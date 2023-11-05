cb75d74fa48707343ab35fd1e4669a0c82ec134e
Luiz Socrate <luiz@socrate.com.br>
2023-11-05 19:38:38 +0100

    Move Utaku Infantry

---
+++ server/game/cards/20-Core2/Unicorn/UtakuInfantry.ts
@@ -0,0 +1,18 @@
+import AbilityDsl from "../../../abilitydsl";
+import DrawCard from "../../../drawcard";
+import type Player from "../../../player";
+
+function getNoOfUnicornCharacters(player: Player) {
+    return player.cardsInPlay.filter((card: DrawCard) => card.isParticipating() && card.isFaction('unicorn')).length;
+}
+
+export default class UtakuInfantry extends DrawCard {
+    static id = 'utaku-infantry';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            condition: context => context.source.isParticipating(),
+            effect: AbilityDsl.effects.modifyBothSkills((card: DrawCard) => getNoOfUnicornCharacters(card.controller))
+        });
+    }
+}
