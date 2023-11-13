d8b2ace4c1bcf2743580813369f95aceb4edf56b
Luiz Socrate <luiz@socrate.com.br>
2023-11-13 11:02:49 +0100

    UPDATE: Sanctified Earth

--- server/game/cards/20-Core2/Phoenix/SanctifiedEarth.ts
+++ server/game/cards/20-Core2/Phoenix/SanctifiedEarth.ts
@@ -1,34 +1,43 @@
 import AbilityDsl from '../../../abilitydsl';
+import { AbilityTypes } from '../../../Constants';
 import DrawCard from '../../../drawcard';
+import type { TriggeredAbilityProps } from '../../../Interfaces';
 
 export default class SanctifiedEarth extends DrawCard {
     static id = 'sanctified-earth';
 
     public setupCardAbilities() {
-        this.action({
-            title: 'Give attached character a skill bonus',
-            condition: (context) => context.source.parent?.isParticipating() ?? false,
-            gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
-                target: context.source.parent,
-                effect: AbilityDsl.effects.modifyBothSkills(2)
-            })),
-            then: {
-                gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
-                    target: context.source.parent,
-                    effect: AbilityDsl.effects.cardCannot({
-                        cannot: 'sendHome',
-                        restricts: 'opponentsCardEffects'
-                    })
-                }))
-            },
+        this.whileAttached({
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.Reaction, {
+                title: 'Give character a skill bonus',
+                when: {
+                    onConflictDeclared: (event, context) => event.attackers.includes(context.source),
+                    onDefendersDeclared: (event, context) => event.defenders.includes(context.source),
+                    onMoveToConflict: (event, context) => event.card === context.source
+                },
+                gameAction: AbilityDsl.actions.multiple([
+                    AbilityDsl.actions.cardLastingEffect((context) => ({
+                        target: context.source,
+                        effect: AbilityDsl.effects.modifyBothSkills(2)
+                    })),
 
-            effect: 'give +2{1} and +2{2} to {3}',
-            effectArgs: (context) => [
-                'military',
-                'political',
-                context.source.parent
-                // this.#hasKicker(context.source.parent) ? ' and protect them from being moved home by the opponent' : ''
-            ]
+                    AbilityDsl.actions.onAffinity({
+                        trait: 'earth',
+                        effect: "make {0} invulnerable to opponent's send home",
+                        effectArgs: (context) => [context.source],
+                        gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
+                            target: context.source,
+                            effect: AbilityDsl.effects.cardCannot({
+                                cannot: 'sendHome',
+                                restricts: 'opponentsCardEffects'
+                            })
+                        }))
+                    })
+                ]),
+
+                effect: 'give +2{1} and +2{2} to {3}',
+                effectArgs: (context) => ['military', 'political', context.source]
+            } as TriggeredAbilityProps)
         });
     }
 }
---
+++ test/server/cards/20-Core2/Phoenix/SanctifiedEarth.spec.js
@@ -0,0 +1,97 @@
+describe('Sanctified Earth', function () {
+    integration(function () {
+        beforeEach(function () {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    hand: ['sanctified-earth', 'one-with-the-sea'],
+                    inPlay: ['adept-of-the-waves', 'solemn-scholar', 'garanto-guardian']
+                },
+                player2: {
+                    hand: ['outwit'],
+                    inPlay: ['bayushi-shoju']
+                }
+            });
+
+            this.sanctifiedEarth = this.player1.findCardByName('sanctified-earth');
+            this.oneWithTheSea = this.player1.findCardByName('one-with-the-sea');
+            this.adept = this.player1.findCardByName('adept-of-the-waves');
+            this.solemn = this.player1.findCardByName('solemn-scholar');
+            this.garantoGuardian = this.player1.findCardByName('garanto-guardian');
+
+            this.shoju = this.player2.findCardByName('bayushi-shoju');
+            this.outwit = this.player2.findCardByName('outwit');
+
+            this.player1.playAttachment(this.sanctifiedEarth, this.garantoGuardian);
+            this.noMoreActions();
+        });
+
+        it('without affinity, it gives skill bonus to a character on commit', function () {
+            this.player1.moveCard(this.solemn, 'dynasty discard pile');
+
+            this.initiateConflict({
+                attackers: [this.garantoGuardian]
+            });
+
+            expect(this.player1).toHavePrompt('Any reactions?');
+
+            this.player1.clickCard(this.garantoGuardian);
+            expect(this.garantoGuardian.getMilitarySkill()).toBe(6);
+            expect(this.garantoGuardian.getPoliticalSkill()).toBe(5);
+            expect(this.getChatLogs(5)).toContain(
+                "player1 uses Garanto Guardian's gained ability from Sanctified Earth to give +2military and +2political to Garanto Guardian"
+            );
+        });
+
+        it('without affinity, it gives skill bonus to a character on move', function () {
+            this.player1.moveCard(this.solemn, 'dynasty discard pile');
+
+            this.initiateConflict({
+                attackers: [this.adept],
+                defenders: [this.shoju]
+            });
+
+            this.player2.pass();
+            this.player1.clickCard(this.oneWithTheSea);
+            this.player1.clickCard(this.garantoGuardian);
+
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            this.player1.clickCard(this.garantoGuardian);
+            expect(this.garantoGuardian.getMilitarySkill()).toBe(6);
+            expect(this.garantoGuardian.getPoliticalSkill()).toBe(5);
+            expect(this.getChatLogs(5)).toContain(
+                "player1 uses Garanto Guardian's gained ability from Sanctified Earth to give +2military and +2political to Garanto Guardian"
+            );
+
+            this.player2.clickCard(this.outwit);
+            this.player2.clickCard(this.garantoGuardian);
+            expect(this.garantoGuardian.isParticipating()).toBe(false);
+        });
+
+        it('with affinity, it gives skill bonus to a character on move and block send home', function () {
+            this.initiateConflict({
+                attackers: [this.adept],
+                defenders: [this.shoju]
+            });
+
+            this.player2.pass();
+            this.player1.clickCard(this.oneWithTheSea);
+            this.player1.clickCard(this.garantoGuardian);
+
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            this.player1.clickCard(this.garantoGuardian);
+            expect(this.garantoGuardian.getMilitarySkill()).toBe(6);
+            expect(this.garantoGuardian.getPoliticalSkill()).toBe(5);
+            expect(this.getChatLogs(5)).toContain(
+                "player1 uses Garanto Guardian's gained ability from Sanctified Earth to give +2military and +2political to Garanto Guardian"
+            );
+            expect(this.getChatLogs(5)).toContain(
+                "player1 channels their earth affinity to make Garanto Guardian invulnerable to opponent's send home"
+            );
+
+            this.player2.clickCard(this.outwit);
+            this.player2.clickCard(this.garantoGuardian);
+            expect(this.garantoGuardian.isParticipating()).toBe(true);
+        });
+    });
+});
