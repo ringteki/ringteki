c5f6b68957af88d82676bd92d6a6856cbc0e8b82
Luiz Socrate <luiz@socrate.com.br>
2023-11-13 12:32:16 +0100

    NEW: Wings of the Phoenix

---
+++ server/game/cards/20-Core2/Phoenix/WingsOfThePhoenix.ts
@@ -0,0 +1,34 @@
+import { CardTypes, Durations, Players } from '../../../Constants';
+import AbilityDsl from '../../../abilitydsl';
+import DrawCard from '../../../drawcard';
+
+export default class WingsOfThePhoenix extends DrawCard {
+    static id = 'wings-of-the-phoenix';
+
+    setupCardAbilities() {
+        this.action({
+            title: 'Move a character',
+            condition: (context) =>
+                context.game.isDuringConflict() &&
+                context.player.anyCardsInPlay((card: DrawCard) => card.hasTrait('shugenja')),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                gameAction: AbilityDsl.actions.multiple([
+                    AbilityDsl.actions.sendHome(),
+                    AbilityDsl.actions.moveToConflict(),
+                    AbilityDsl.actions.onAffinity({
+                        trait: 'fire',
+                        gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
+                            target: context.game.currentConflict.getCharacters(context.player.opponent),
+                            effect: AbilityDsl.effects.modifyBothSkills(-1),
+                            duration: Durations.UntilEndOfConflict
+                        })),
+                        effect: 'give all participating enemies -1{1}/-1{2} until the end of the conflict',
+                        effectArgs: ['military', 'political']
+                    })
+                ])
+            }
+        });
+    }
+}
