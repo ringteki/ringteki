c0b163012361738cddcb5eacf65b39070edea371
Luiz Socrate <luiz@socrate.com.br>
2023-11-15 19:43:09 +0100

    UPDATE: Rift to Toshigoku

--- server/game/cards/20-Core2/Neutral/RiftToToshigoku.ts
+++ server/game/cards/20-Core2/Neutral/RiftToToshigoku.ts
@@ -1,11 +1,22 @@
-import { Players, CardTypes } from '../../../Constants';
+import { Players, CardTypes, EventNames, AbilityTypes } from '../../../Constants';
 import { ProvinceCard } from '../../../ProvinceCard';
 import AbilityDsl from '../../../abilitydsl';
+import { EventRegistrar } from '../../../EventRegistrar';
 
 export default class RiftToToshigoku extends ProvinceCard {
     static id = 'rift-to-toshigoku';
 
-    setupCardAbilities() {
+    private eventRegistrar?: EventRegistrar;
+    private shouldCancelRingEffectsHere?: boolean;
+
+    public setupCardAbilities() {
+        this.eventRegistrar = new EventRegistrar(this.game, this);
+        this.eventRegistrar.register([
+            {
+                [EventNames.OnResolveRingElement + ':' + AbilityTypes.WouldInterrupt]: 'cancelRingEffect'
+            }
+        ]);
+
         this.reaction({
             title: 'Force opponent to remove all fate from a character and resolve the conflict',
             when: {
@@ -18,13 +29,23 @@ export default class RiftToToshigoku extends ProvinceCard {
                 controller: Players.Opponent,
                 cardType: CardTypes.Character,
                 cardCondition: (card) => card.isAttacking(),
-                gameAction: AbilityDsl.actions.sequential([
-                    AbilityDsl.actions.removeFate((context) => ({
-                        target: context.target,
-                        amount: context.target.getFate()
-                    }))
-                ])
+                gameAction: AbilityDsl.actions.discardFromPlay()
+            },
+            then: (context) => {
+                this.shouldCancelRingEffectsHere = true;
             }
         });
+    }
+
+    public cancelRingEffect(event: any) {
+        if (
+            event.context.game.currentConflict &&
+            this.isConflictProvince() &&
+            this.shouldCancelRingEffectsHere &&
+            !event.cancelled
+        ) {
+            event.cancel();
+            this.game.addMessage('{0} cancels the ring effect', this);
+        }
     }
 }
