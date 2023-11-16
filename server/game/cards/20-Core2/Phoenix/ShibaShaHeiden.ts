5e7134ac2e04249b98f78e6378f621689dc62b29
Luiz Socrate <luiz@socrate.com.br>
2023-11-16 22:58:24 +0100

    Simplify shared limit

--- server/game/cards/20-Core2/Phoenix/ShibaShaHeiden.ts
+++ server/game/cards/20-Core2/Phoenix/ShibaShaHeiden.ts
@@ -1,18 +1,11 @@
-import { EventNames, Phases } from '../../../Constants';
+import { Phases } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
-import { EventRegistrar } from '../../../EventRegistrar';
-import type Player from '../../../player';
 
 export default class ShibaShaHeiden extends DrawCard {
     static id = 'shiba-sha-heiden';
 
-    usedResourceTrade = new Set<Player>();
-
     setupCardAbilities() {
-        this.eventRegistrar = new EventRegistrar(this.game, this);
-        this.eventRegistrar.register([EventNames.OnRoundEnded, EventNames.OnCardLeavesPlay]);
-
         this.reaction({
             title: 'Fill this province with a card',
             when: {
@@ -29,42 +22,25 @@ export default class ShibaShaHeiden extends DrawCard {
          * The card is a single action, but implementing the cost was too much work
          * Should be fixed later when choice costs are implemented
          */
+
+        const sharedLimit = AbilityDsl.limit.perRound(1);
         this.action({
             title: 'Pay 1 fate to draw a card',
             cost: AbilityDsl.costs.payFate(1),
             gameAction: AbilityDsl.actions.draw(),
-            condition: (context) => !this.usedResourceTrade.has(context.player),
-            then: (context) => {
-                this.usedResourceTrade.add(context.player);
-            }
+            limit: sharedLimit
         });
         this.action({
             title: 'Discard a card to gain 1 fate',
             cost: AbilityDsl.costs.discardCard(),
             gameAction: AbilityDsl.actions.gainFate(),
-            condition: (context) => !this.usedResourceTrade.has(context.player),
-            then: (context) => {
-                this.usedResourceTrade.add(context.player);
-            }
+            limit: sharedLimit
         });
         this.action({
             title: 'Discard a card to draw a card',
             cost: AbilityDsl.costs.discardCard(),
             gameAction: AbilityDsl.actions.draw(),
-            condition: (context) => !this.usedResourceTrade.has(context.player),
-            then: (context) => {
-                this.usedResourceTrade.add(context.player);
-            }
+            limit: sharedLimit
         });
-    }
-
-    public onRoundEnded() {
-        this.usedResourceTrade.clear();
-    }
-
-    public onCardLeavesPlay(event: any) {
-        if (event.card === this) {
-            this.usedResourceTrade.clear();
-        }
     }
 }
