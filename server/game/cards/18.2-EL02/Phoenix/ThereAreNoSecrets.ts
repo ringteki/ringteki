7827ae069eb092d42e0992bee5733f9c1d543029
Luiz Socrate <luiz@socrate.com.br>
2023-11-16 12:25:52 +0100

    Update

---
+++ server/game/cards/18.2-EL02/Phoenix/ThereAreNoSecrets.ts
@@ -0,0 +1,28 @@
+import AbilityDsl from '../../../abilitydsl';
+import { CardTypes } from '../../../Constants';
+import DrawCard from '../../../drawcard';
+import TriggeredAbilityContext from '../../../TriggeredAbilityContext';
+
+export default class ThereAreNoSecrets extends DrawCard {
+    static id = 'there-are-no-secrets';
+
+    setupCardAbilities() {
+        this.wouldInterrupt({
+            title: 'Gain 1 fate',
+            limit: AbilityDsl.limit.perRound(2),
+            when: {
+                onMoveFate: (event, context) =>
+                    context.source.parent && event.origin === context.source.parent && event.fate > 0
+            },
+            gameAction: AbilityDsl.actions.gainFate((context) => ({ target: context.player }))
+        });
+    }
+
+    canPlay(context: TriggeredAbilityContext, playType: string) {
+        return (
+            context.player.cardsInPlay.any(
+                (card: DrawCard) => card.getType() === CardTypes.Character && card.hasTrait('shugenja')
+            ) && super.canPlay(context, playType)
+        );
+    }
+}
