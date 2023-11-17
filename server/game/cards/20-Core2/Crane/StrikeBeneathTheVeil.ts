fdc48b52929b01abfed0ad78bf9fb591c1fd0939
Luiz Socrate <luiz@socrate.com.br>
2023-11-17 17:27:03 +0100

    UPDATE: Strike Beneath the Veil

--- server/game/cards/20-Core2/Crane/StrikeBeneathTheVeil.ts
+++ server/game/cards/20-Core2/Crane/StrikeBeneathTheVeil.ts
@@ -2,29 +2,26 @@ import { CardTypes } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
 
+function penalty(target: DrawCard): number {
+    return -2 * target.attachments.length;
+}
+
 export default class StrikeBeneathTheVeil extends DrawCard {
     static id = 'strike-beneath-the-veil';
 
     setupCardAbilities() {
         this.action({
             title: 'Give a military penalty to a participating character',
-            condition: (context) =>
-                context.player.cardsInPlay.some(
-                    (card: DrawCard) => card.isParticipating('military') && card.hasTrait('bushi')
-                ),
+            condition: (context) => context.game.isDuringConflict('military'),
             target: {
                 cardType: CardTypes.Character,
                 cardCondition: (card) => card.isParticipating(),
                 gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
-                    effect: AbilityDsl.effects.modifyMilitarySkill(this.#penalty(context.target))
+                    effect: AbilityDsl.effects.modifyMilitarySkill(penalty(context.target))
                 }))
             },
             effect: 'give {0} {1}{2}',
-            effectArgs: (context) => [this.#penalty(context.target), 'military']
+            effectArgs: (context) => [penalty(context.target), 'military']
         });
-    }
-
-    #penalty(target: DrawCard): number {
-        return -2 * target.attachments.length;
     }
 }
