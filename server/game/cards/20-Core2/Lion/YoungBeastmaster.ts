8e7c3a51e821a30b1666961894c02a2dc3d871e2
Luiz Socrate <luiz@socrate.com.br>
2023-12-11 17:14:58 +0100

    UPDATE: Young Beastmaster new

--- server/game/cards/20-Core2/Lion/YoungBeastmaster.ts
+++ server/game/cards/20-Core2/Lion/YoungBeastmaster.ts
@@ -1,6 +1,22 @@
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
 
+function bonusSize(cards?: Array<DrawCard>) {
+    if (!cards) {
+        return 0;
+    }
+
+    let higherCost = 0;
+    for (const card of cards) {
+        const cardCost = card.getCost();
+        if (cardCost > higherCost) {
+            higherCost = cardCost;
+        }
+    }
+
+    return higherCost;
+}
+
 export default class YoungBeastmaster extends DrawCard {
     static id = 'young-beastmaster';
 
@@ -10,20 +26,12 @@ export default class YoungBeastmaster extends DrawCard {
             when: {
                 onConflictDeclared: (event, context) => event.attackers.includes(context.source)
             },
-            cost: AbilityDsl.costs.discardCardSpecific((context) => context.player.dynastyDeck.first()),
+            cost: AbilityDsl.costs.discardCardSpecific((context) => context.player.dynastyDeck.first(2)),
             effect: 'give {0} +{1}{2}',
-            effectArgs: (context) => [this.#bonusSize(context.costs.discardCard?.[0]), 'military'],
+            effectArgs: (context) => [bonusSize(context.costs.discardCard), 'military'],
             gameAction: AbilityDsl.actions.cardLastingEffect((context) => ({
-                effect: AbilityDsl.effects.modifyMilitarySkill(this.#bonusSize(context.costs.discardCard?.[0]))
+                effect: AbilityDsl.effects.modifyMilitarySkill(bonusSize(context.costs.discardCard))
             }))
         });
-    }
-
-    #bonusSize(card?: DrawCard) {
-        if (card == undefined) {
-            return 0;
-        }
-        const cost = card.getCost();
-        return isNaN(cost) ? 0 : cost;
     }
 }
