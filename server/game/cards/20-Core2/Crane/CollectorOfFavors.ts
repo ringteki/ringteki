403a7156bb62d51f3e7ebba76608ad3e58bd83e1
Luiz Socrate <luiz@socrate.com.br>
2023-12-11 16:58:17 +0100

    UPDATE: Collector of Favors new

--- server/game/cards/20-Core2/Crane/CollectorOfFavors.ts
+++ server/game/cards/20-Core2/Crane/CollectorOfFavors.ts
@@ -7,14 +7,16 @@ export default class CollectorOfFavors extends DrawCard {
     static id = 'collector-of-favors';
 
     setupCardAbilities() {
+        this.attachmentConditions({ trait: 'courtier' });
+
+        this.whileAttached({ effect: AbilityDsl.effects.addKeyword('courtesy') });
+
         this.whileAttached({
             effect: AbilityDsl.effects.gainAbility(AbilityTypes.Reaction, {
-                title: 'Gain a fate',
+                title: 'Gain 1 fate',
                 when: {
                     afterConflict: (event, context) =>
-                        event.conflict.winner === context.source.controller &&
-                        context.source.isParticipating() &&
-                        context.source.hasTrait('courtier')
+                        event.conflict.winner === context.source.controller && context.source.isParticipating()
                 },
                 gameAction: AbilityDsl.actions.gainFate()
             } as TriggeredAbilityProps)
