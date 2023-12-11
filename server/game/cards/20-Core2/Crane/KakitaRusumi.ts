480752ab949899217da6feaa77f849c41142d16c
Luiz Socrate <luiz@socrate.com.br>
2023-12-11 16:55:15 +0100

    UPDATE: kakita Rusumi new

--- server/game/cards/20-Core2/Crane/KakitaRusumi.ts
+++ server/game/cards/20-Core2/Crane/KakitaRusumi.ts
@@ -1,6 +1,11 @@
-import { CardTypes, Durations, Decks } from '../../../Constants';
+import { CardTypes, Decks, Durations } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
+import AbilityContext from '../../../AbilityContext';
+
+function statusOfIntern(context: AbilityContext) {
+    return (context.source as DrawCard).isHonored ? 'honored' : 'ordinary';
+}
 
 export default class KakitaRusumi extends DrawCard {
     static id = 'kakita-rusumi';
@@ -8,25 +13,25 @@ export default class KakitaRusumi extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Put a character into play',
-            condition: (context) => context.game.isDuringConflict(),
+            condition: (context) => context.player.isDefendingPlayer(),
             gameAction: AbilityDsl.actions.deckSearch(() => ({
                 activePromptTitle: 'Choose a character to put into play',
                 amount: 4,
                 deck: Decks.DynastyDeck,
                 cardCondition: (card) =>
                     card.type === CardTypes.Character && card.printedCost <= 2 && card.isFaction('crane'),
-                message: '{0} puts {1} into play honored',
-                messageArgs: (context, cards) => [context.player, cards],
+                message: '{0} puts {1} into play {2}',
+                messageArgs: (context, cards) => [context.player, cards, statusOfIntern(context)],
                 shuffle: true,
-                gameAction: AbilityDsl.actions.putIntoConflict({ status: 'honored' })
+                gameAction: AbilityDsl.actions.putIntoConflict((context) => ({ status: statusOfIntern(context) }))
             })),
             effect: 'search their dynasty deck for a character to put into play',
-            then: context => ({
+            then: (context) => ({
                 gameAction: AbilityDsl.actions.cardLastingEffect(() => {
                     let target = [];
                     if (context.selects['deckSearch']?.length > 0) {
                         target = context.selects['deckSearch'][0];
-                    } 
+                    }
                     return {
                         target: target,
                         duration: Durations.UntilEndOfPhase,
@@ -34,11 +39,11 @@ export default class KakitaRusumi extends DrawCard {
                             when: {
                                 onConflictFinished: () => true
                             },
-                            message: '{0} is discarded from play due to {1}\'s effect',
+                            message: "{0} is discarded from play due to {1}'s effect",
                             messageArgs: [target, context.source],
                             gameAction: AbilityDsl.actions.discardFromPlay()
                         })
-                    }
+                    };
                 })
             })
         });
--- test/server/cards/20-Core2/Crane/KakitaRusumi.spec.js
+++ test/server/cards/20-Core2/Crane/KakitaRusumi.spec.js
@@ -4,6 +4,9 @@ describe('Kakita Rusumi', function () {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
+                    inPlay: ['wandering-ronin']
+                },
+                player2: {
                     inPlay: ['kakita-rusumi'],
                     dynastyDiscard: [
                         'brash-samurai',
@@ -15,124 +18,124 @@ describe('Kakita Rusumi', function () {
                 }
             });
 
-            this.player1.reduceDeckToNumber('dynasty deck', 0);
+            this.ronin = this.player1.findCardByName('wandering-ronin');
 
-            this.storehouse = this.player1.moveCard('imperial-storehouse', 'dynasty deck');
-            this.sycophant = this.player1.moveCard('otomo-sycophant', 'dynasty deck');
-            this.whisperer = this.player1.moveCard('doji-whisperer', 'dynasty deck');
-            this.paragon = this.player1.moveCard('paragon-of-grace', 'dynasty deck');
-            this.brash = this.player1.moveCard('brash-samurai', 'dynasty deck');
+            this.player2.reduceDeckToNumber('dynasty deck', 0);
 
-            this.rusumi = this.player1.findCardByName('kakita-rusumi');
+            this.storehouse = this.player2.moveCard('imperial-storehouse', 'dynasty deck');
+            this.sycophant = this.player2.moveCard('otomo-sycophant', 'dynasty deck');
+            this.whisperer = this.player2.moveCard('doji-whisperer', 'dynasty deck');
+            this.paragon = this.player2.moveCard('paragon-of-grace', 'dynasty deck');
+            this.brash = this.player2.moveCard('brash-samurai', 'dynasty deck');
+
+            this.rusumi = this.player2.findCardByName('kakita-rusumi');
         });
 
-        it('should not work outside of a conflict', function () {
-            expect(this.player1).toHavePrompt('Action Window');
-            this.player1.clickCard(this.rusumi);
-            expect(this.player1).toHavePrompt('Action Window');
-        });
+        describe('not during conflicts', function () {
+            it('should not work outside of a conflict', function () {
+                this.player1.pass();
+                expect(this.player2).toHavePrompt('Action Window');
 
-        it('should search your top 4 cards', function () {
-            this.noMoreActions();
-
-            this.initiateConflict({
-                attackers: [this.rusumi],
-                defenders: [],
-                type: 'military'
+                this.player2.clickCard(this.rusumi);
+                expect(this.player2).toHavePrompt('Action Window');
             });
-            this.player2.pass();
-            this.player1.clickCard(this.rusumi);
-            expect(this.player1).toHavePrompt('Choose a character to put into play');
-            expect(this.player1).toHavePromptButton('Brash Samurai');
-            expect(this.player1).toHaveDisabledPromptButton('Paragon of Grace');
-            expect(this.player1).toHaveDisabledPromptButton('Otomo Sycophant');
-            expect(this.player1).toHavePromptButton('Doji Whisperer');
-            expect(this.player1).toHavePromptButton('Take nothing');
         });
 
-        it('should put the chosen character into the conflict', function () {
-            this.noMoreActions();
-
-            this.initiateConflict({
-                attackers: [this.rusumi],
-                defenders: [],
-                type: 'military'
+        describe('while attacking', function () {
+            beforeEach(function () {
+                this.player1.pass();
+                this.player2.pass();
+                this.player1.passConflict();
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.rusumi],
+                    defenders: []
+                });
+                this.player1.pass();
             });
-            this.player2.pass();
-            this.player1.clickCard(this.rusumi);
-            this.player1.clickPrompt('Doji Whisperer');
-            expect(this.game.currentConflict.attackers).toContain(this.whisperer);
-            expect(this.whisperer.isHonored).toBe(true);
+
+            it('should not work while attacking', function () {
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+
+                this.player2.clickCard(this.rusumi);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+            });
         });
 
-        it('chat messages', function () {
-            this.noMoreActions();
-
-            this.initiateConflict({
-                attackers: [this.rusumi],
-                defenders: [],
-                type: 'military'
+        describe('while defending', function () {
+            beforeEach(function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.ronin],
+                    defenders: []
+                });
             });
-            this.player2.pass();
-            this.player1.clickCard(this.rusumi);
-            this.player1.clickPrompt('Doji Whisperer');
 
-            expect(this.getChatLogs(6)).toContain(
-                'player1 uses Kakita Rusumi to search their dynasty deck for a character to put into play'
-            );
-            expect(this.getChatLogs(6)).toContain('player1 puts Doji Whisperer into play honored');
-        });
-
-        it('chat messages - taking nothing', function () {
-            this.noMoreActions();
-
-            this.initiateConflict({
-                attackers: [this.rusumi],
-                defenders: [],
-                type: 'military'
+            it('should search your top 4 cards', function () {
+                this.player2.clickCard(this.rusumi);
+                expect(this.player2).toHavePrompt('Choose a character to put into play');
+                expect(this.player2).toHavePromptButton('Brash Samurai');
+                expect(this.player2).toHaveDisabledPromptButton('Paragon of Grace');
+                expect(this.player2).toHaveDisabledPromptButton('Otomo Sycophant');
+                expect(this.player2).toHavePromptButton('Doji Whisperer');
+                expect(this.player2).toHavePromptButton('Take nothing');
             });
-            this.player2.pass();
-            this.player1.clickCard(this.rusumi);
-            this.player1.clickPrompt('Take nothing');
 
-            expect(this.getChatLogs(6)).toContain(
-                'player1 uses Kakita Rusumi to search their dynasty deck for a character to put into play'
-            );
-            expect(this.getChatLogs(6)).toContain('player1 takes nothing');
-        });
-
-        it('should shuffle your dynasty deck', function () {
-            this.noMoreActions();
-
-            this.initiateConflict({
-                attackers: [this.rusumi],
-                defenders: [],
-                type: 'military'
+            it('should put the chosen character into the conflict', function () {
+                this.player2.clickCard(this.rusumi);
+                this.player2.clickPrompt('Doji Whisperer');
+                expect(this.game.currentConflict.defenders).toContain(this.whisperer);
+                expect(this.whisperer.isHonored).toBe(false);
             });
-            this.player2.pass();
-            this.player1.clickCard(this.rusumi);
-            this.player1.clickPrompt('Doji Whisperer');
-            expect(this.getChatLogs(6)).toContain('player1 is shuffling their dynasty deck');
-        });
 
-        it('should discard the character at the end of the conflict', function () {
-            this.noMoreActions();
+            it('should put the chosen character into the conflict honored when rusumi is honored', function () {
+                this.rusumi.honor();
 
-            this.initiateConflict({
-                attackers: [this.rusumi],
-                defenders: [],
-                type: 'military'
+                this.player2.clickCard(this.rusumi);
+                this.player2.clickPrompt('Doji Whisperer');
+                expect(this.game.currentConflict.defenders).toContain(this.whisperer);
+                expect(this.whisperer.isHonored).toBe(true);
             });
-            this.player2.pass();
-            this.player1.clickCard(this.rusumi);
-            this.player1.clickPrompt('Doji Whisperer');
-            expect(this.game.currentConflict.attackers).toContain(this.whisperer);
-            expect(this.whisperer.isHonored).toBe(true);
 
-            this.noMoreActions();
-            this.player1.clickPrompt('Don\'t Resolve');
-            expect(this.whisperer.location).toBe('dynasty discard pile');
-            expect(this.getChatLogs(5)).toContain('Doji Whisperer is discarded from play due to Kakita Rusumi\'s effect');
+            it('chat messages', function () {
+                this.player2.clickCard(this.rusumi);
+                this.player2.clickPrompt('Doji Whisperer');
+
+                expect(this.getChatLogs(6)).toContain(
+                    'player2 uses Kakita Rusumi to search their dynasty deck for a character to put into play'
+                );
+                expect(this.getChatLogs(6)).toContain('player2 puts Doji Whisperer into play ordinary');
+            });
+
+            it('chat messages - taking nothing', function () {
+                this.player2.clickCard(this.rusumi);
+                this.player2.clickPrompt('Take nothing');
+
+                expect(this.getChatLogs(6)).toContain(
+                    'player2 uses Kakita Rusumi to search their dynasty deck for a character to put into play'
+                );
+                expect(this.getChatLogs(6)).toContain('player2 takes nothing');
+            });
+
+            it('should shuffle your dynasty deck', function () {
+                this.player2.clickCard(this.rusumi);
+                this.player2.clickPrompt('Doji Whisperer');
+                expect(this.getChatLogs(6)).toContain('player2 is shuffling their dynasty deck');
+            });
+
+            it('should discard the character at the end of the conflict', function () {
+                this.player2.clickCard(this.rusumi);
+                this.player2.clickPrompt('Doji Whisperer');
+                expect(this.game.currentConflict.defenders).toContain(this.whisperer);
+                expect(this.whisperer.isHonored).toBe(false);
+
+                this.noMoreActions();
+                this.player1.clickPrompt("Don't Resolve");
+                expect(this.whisperer.location).toBe('dynasty discard pile');
+                expect(this.getChatLogs(5)).toContain(
+                    "Doji Whisperer is discarded from play due to Kakita Rusumi's effect"
+                );
+            });
         });
     });
 });
