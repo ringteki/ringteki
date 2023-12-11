203469d5f0e016724722a0cfb11db60650443086
Luiz Socrate <luiz@socrate.com.br>
2023-12-11 21:02:04 +0100

    UPDATE: Objection new

--- server/game/cards/20-Core2/Neutral/Objection.ts
+++ server/game/cards/20-Core2/Neutral/Objection.ts
@@ -1,51 +1,72 @@
-import AbilityContext from '../../../AbilityContext';
-import { CardTypes, EventNames, Locations, Players } from '../../../Constants';
-import { EventRegistrar } from '../../../EventRegistrar';
+import type AbilityContext from '../../../AbilityContext';
+import { CardTypes, EventNames } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
+import type { Cost } from '../../../Costs';
+import type Player from '../../../player';
+import type TriggeredAbilityContext from '../../../TriggeredAbilityContext';
+import type Game from '../../../game';
+import { EventRegistrar } from '../../../EventRegistrar';
+
+const GLOBAL_TRACKER = new WeakMap<Game, WeakMap<Player, number>>();
+
+function refreshObjectionCost(game: Game): void {
+    GLOBAL_TRACKER.set(game, new WeakMap());
+}
+
+function currentObjectionCost(player: Player): number {
+    return GLOBAL_TRACKER.get(player.game)?.get(player) ?? 0;
+}
+
+function increaseObjectionCost(player: Player) {
+    const gameTracker = GLOBAL_TRACKER.get(player.game);
+    if (!gameTracker) {
+        GLOBAL_TRACKER.set(player.game, new WeakMap([[player, 1]]));
+        return;
+    }
+    gameTracker.set(player, currentObjectionCost(player) + 1);
+}
+
+class ObjectionCost implements Cost {
+    getActionName(): string {
+        return 'objectionCost';
+    }
+
+    canPay(context: AbilityContext): boolean {
+        const amount = currentObjectionCost(context.player);
+        return amount === 0 || AbilityDsl.actions.loseFate({ target: context.player, amount }).hasLegalTarget(context);
+    }
+
+    pay(context: TriggeredAbilityContext): void {
+        const fateCost = currentObjectionCost(context.player);
+        if (fateCost > 0) {
+            AbilityDsl.actions.loseFate({ target: context.player, amount: fateCost }).resolve(context.player, context);
+        }
+
+        increaseObjectionCost(context.player);
+    }
+}
 
 export default class Objection extends DrawCard {
     static id = 'objection-';
 
-    private copiesPlayedDuringPhase = new Array<string>();
-    private eventRegistrar?: EventRegistrar;
-
     setupCardAbilities() {
         this.eventRegistrar = new EventRegistrar(this.game, this);
-        this.eventRegistrar.register([EventNames.OnPhaseStarted, EventNames.OnCardPlayed]);
-
-        this.persistentEffect({
-            location: Locations.Any,
-            targetController: Players.Any,
-            // increaseCost doesn't take a variable argument so we use a negative reduce cost
-            effect: AbilityDsl.effects.reduceCost({
-                amount: (card, player) =>
-                    -1 * this.copiesPlayedDuringPhase.filter((uuid) => uuid === player.uuid).length,
-                match: (card, source) => card === source
-            })
-        });
+        this.eventRegistrar.register([EventNames.OnPhaseStarted]);
 
         this.wouldInterrupt({
             title: 'Cancel an event',
             when: {
-                onInitiateAbilityEffects: (event) => event.card.type === CardTypes.Event
+                onInitiateAbilityEffects: (event, context) =>
+                    event.card.type === CardTypes.Event && context.player.imperialFavor !== ''
             },
             cannotBeMirrored: true,
+            cost: new ObjectionCost(),
             gameAction: AbilityDsl.actions.cancel()
         });
     }
 
-    canPlay(context: AbilityContext, playType) {
-        return context.player.imperialFavor !== '' && super.canPlay(context, playType);
-    }
-
-    onCardPlayed(event: any) {
-        if (event.card.name === this.name) {
-            this.copiesPlayedDuringPhase.push(event.player.uuid);
-        }
-    }
-
     onPhaseStarted() {
-        this.copiesPlayedDuringPhase.length = 0;
+        refreshObjectionCost(this.game);
     }
 }
--- test/server/cards/20-Core2/Neutral/Objection.spec.js
+++ test/server/cards/20-Core2/Neutral/Objection.spec.js
@@ -9,17 +9,48 @@ describe('Objection', function () {
                 },
                 player2: {
                     inPlay: ['isawa-tadaka-2'],
-                    hand: ['against-the-waves', 'against-the-waves', 'against-the-waves', 'against-the-waves']
+                    hand: [
+                        'against-the-waves',
+                        'against-the-waves',
+                        'against-the-waves',
+                        'against-the-waves',
+                        'voice-of-honor'
+                    ]
                 }
             });
 
             this.player1.player.imperialFavor = 'miiltary';
 
-            this.tadaka = this.player2.findCardByName('isawa-tadaka-2');
             this.miyaMystic = this.player1.findCardByName('miya-mystic');
+            this.tadaka = this.player2.findCardByName('isawa-tadaka-2');
+            this.voiceOfHonor = this.player2.findCardByName('voice-of-honor');
 
             this.objections = this.player1.filterCardsByName('objection-');
             this.atws = this.player2.filterCardsByName('against-the-waves');
+        });
+
+        it('increases cost during cancel wars', function () {
+            let fate = this.player1.fate;
+            this.tadaka.honor();
+
+            this.player1.pass();
+            this.player2.clickCard(this.atws[0]);
+            this.player2.clickCard(this.tadaka);
+
+            expect(this.player1).toBeAbleToSelect(this.objections[0]);
+            this.player1.clickCard(this.objections[0]);
+            expect(this.player1.fate).toBe(fate);
+            expect(this.getChatLogs(1)).toContain(
+                'player1 plays Objection! to cancel the effects of Against the Waves'
+            );
+
+            this.player2.clickCard(this.voiceOfHonor);
+
+            expect(this.player1).toBeAbleToSelect(this.objections[1]);
+            this.player1.clickCard(this.objections[1]);
+            expect(this.player1.fate).toBe(fate - 1);
+            expect(this.tadaka.bowed).toBe(false);
+            expect(this.getChatLogs(1)).toContain('player1 plays Objection! to cancel the effects of Voice of Honor');
         });
 
         it('should increase cost by 1 for each copy that you play in the phase', function () {
