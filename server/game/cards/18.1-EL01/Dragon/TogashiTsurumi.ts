ed9e9741928f78f429fecda7c2c8f6d1da2550f8
Luiz Socrate <luiz@socrate.com.br>
2023-12-29 02:33:13 +0100

    UPDATE: new Togashi Tsurumi

--- server/game/cards/18.1-EL01/Dragon/TogashiTsurumi.ts
+++ server/game/cards/18.1-EL01/Dragon/TogashiTsurumi.ts
@@ -1,26 +1,35 @@
-import { CardTypes, Locations, PlayTypes, Players } from '../../../Constants';
-import { EventRegistrar } from '../../../EventRegistrar';
-import AbilityDsl = require('../../../abilitydsl');
-import BaseCard = require('../../../basecard');
-import DrawCard = require('../../../drawcard');
-import Player = require('../../../player');
+import AbilityDsl from '../../../abilitydsl';
+import type BaseCard from '../../../basecard';
+import { CardTypes, Locations, Players, PlayTypes } from '../../../Constants';
+import DrawCard from '../../../drawcard';
+import type Player from '../../../player';
 
 export default class TogashiTsurumi extends DrawCard {
     static id = 'togashi-tsurumi';
 
-    private triggeredThisRound = false;
-    private eventRegistrar?: EventRegistrar;
-
     public setupCardAbilities() {
-        this.eventRegistrar = new EventRegistrar(this.game, this);
-        this.eventRegistrar.register(['onRoundEnded']);
+        this.persistentEffect({
+            effect: AbilityDsl.effects.modifyBothSkills(() => this.getSkillBonus())
+        });
+
+        this.persistentEffect({
+            location: Locations.PlayArea,
+            targetLocation: this.uuid,
+            targetController: Players.Self,
+            match: (card) => card.location === this.uuid && card.hasTrait('kiho'),
+            effect: [
+                AbilityDsl.effects.canPlayFromOutOfPlay(
+                    (player: Player) => player === this.controller,
+                    PlayTypes.PlayFromHand
+                ),
+                AbilityDsl.effects.registerToPlayFromOutOfPlay()
+            ]
+        });
 
         this.action({
             title: 'Place a card underneath self',
-            cost: AbilityDsl.costs.payFate(),
-            limit: AbilityDsl.limit.unlimitedPerConflict(),
-            effect: 'place a card from their hand beneath {1}{2}',
-            effectArgs: (context) => [context.source, !this.triggeredThisRound ? ' and draw a card' : ''],
+            effect: 'place a card from their hand beneath {1} and draw a card',
+            effectArgs: (context) => [context.source],
             target: {
                 activePromptTitle: 'Choose a card',
                 location: Locations.Hand,
@@ -29,11 +38,10 @@ export default class TogashiTsurumi extends DrawCard {
                 gameAction: AbilityDsl.actions.multiple([
                     AbilityDsl.actions.draw((context) => ({
                         target: context.player,
-                        amount: this.triggeredThisRound ? 0 : 1
+                        amount: 1
                     })),
                     AbilityDsl.actions.handler({
                         handler: (context) => {
-                            this.triggeredThisRound = true;
                             const card = context.target;
                             context.player.moveCard(card, this.uuid);
                             card.controller = context.source.controller;
@@ -51,33 +59,11 @@ export default class TogashiTsurumi extends DrawCard {
                 ])
             }
         });
-
-        this.persistentEffect({
-            location: Locations.PlayArea,
-            targetLocation: this.uuid,
-            targetController: Players.Self,
-            match: (card) => card.location === this.uuid && card.hasTrait('kiho'),
-            effect: [
-                AbilityDsl.effects.canPlayFromOutOfPlay(
-                    (player: Player) => player === this.controller,
-                    PlayTypes.PlayFromHand
-                ),
-                AbilityDsl.effects.registerToPlayFromOutOfPlay()
-            ]
-        });
-
-        this.persistentEffect({
-            effect: AbilityDsl.effects.modifyBothSkills((card: BaseCard) => this.getSkillBonus(card))
-        });
     }
 
-    public onRoundEnded() {
-        this.triggeredThisRound = false;
-    }
-
-    private getSkillBonus(card: BaseCard) {
-        return (card.game.allCards as BaseCard[]).reduce(
-            (bonus, card) => (card.controller === this.controller && card.location === this.uuid ? bonus + 1 : bonus),
+    private getSkillBonus() {
+        return (this.game.allCards as Array<BaseCard>).reduce(
+            (total, card) => (card.controller === this.controller && card.location === this.uuid ? total + 1 : total),
             0
         );
     }
--- test/server/cards/18.1-EL01/Dragon/TogashiTsurumi.spec.js
+++ test/server/cards/18.1-EL01/Dragon/TogashiTsurumi.spec.js
@@ -1,6 +1,6 @@
-describe('Togashi Tsurumi', function() {
-    integration(function() {
-        beforeEach(function() {
+describe('Togashi Tsurumi', function () {
+    integration(function () {
+        beforeEach(function () {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
@@ -18,7 +18,7 @@ describe('Togashi Tsurumi', function() {
             this.hammer = this.player1.findCardByName('kyofuki-s-hammer');
         });
 
-        it('should put cards underneath self and get +1/+1 per card', function() {
+        it('should put cards underneath self and get +1/+1 per card', function () {
             this.noMoreActions();
             this.initiateConflict({
                 attackers: [this.tsurumi],
@@ -33,32 +33,25 @@ describe('Togashi Tsurumi', function() {
             let hand = this.player1.hand.length;
 
             this.player1.clickCard(this.tsurumi);
+            expect(this.player1).toHavePrompt('Choose a card');
             expect(this.player1).toBeAbleToSelect(this.punch);
             expect(this.player1).toBeAbleToSelect(this.stance);
             expect(this.player1).toBeAbleToSelect(this.hammer);
             this.player1.clickCard(this.punch);
             expect(this.punch.location).toBe(this.tsurumi.uuid);
-            expect(this.getChatLogs(5)).toContain('player1 uses Togashi Tsurumi, spending 1 fate to place a card from their hand beneath Togashi Tsurumi and draw a card');
+            expect(this.getChatLogs(5)).toContain(
+                'player1 uses Togashi Tsurumi to place a card from their hand beneath Togashi Tsurumi and draw a card'
+            );
 
-            expect(this.player1.fate).toBe(fate - 1);
+            expect(this.player1.fate).toBe(fate);
             expect(this.tsurumi.getMilitarySkill()).toBe(mil + 1);
             expect(this.tsurumi.getPoliticalSkill()).toBe(pol + 1);
             expect(this.player1.hand.length).toBe(hand);
-
-            this.player2.pass();
-
-            this.player1.clickCard(this.tsurumi);
-            this.player1.clickCard(this.stance);
-            expect(this.stance.location).toBe(this.tsurumi.uuid);
-            expect(this.getChatLogs(5)).toContain('player1 uses Togashi Tsurumi, spending 1 fate to place a card from their hand beneath Togashi Tsurumi');
-
-            expect(this.player1.fate).toBe(fate - 2);
-            expect(this.tsurumi.getMilitarySkill()).toBe(mil + 2);
-            expect(this.tsurumi.getPoliticalSkill()).toBe(pol + 2);
-            expect(this.player1.hand.length).toBe(hand - 1);
         });
 
-        it('kihos cards underneath self should be playable', function() {
+        it('kihos cards underneath self should be playable', function () {
+            let initialMIL = this.tsurumi.getMilitarySkill();
+            let initialPOL = this.tsurumi.getPoliticalSkill();
             this.noMoreActions();
             this.initiateConflict({
                 attackers: [this.tsurumi],
@@ -67,26 +60,30 @@ describe('Togashi Tsurumi', function() {
             });
             this.player2.pass();
 
-            this.player1.clickCard(this.tsurumi);
-            this.player1.clickCard(this.punch);
-            this.player2.pass();
+            expect(this.tsurumi.getMilitarySkill()).toBe(initialMIL);
+            expect(this.tsurumi.getPoliticalSkill()).toBe(initialPOL);
+
             this.player1.clickCard(this.tsurumi);
             this.player1.clickCard(this.stance);
 
-            let mil = this.tsurumi.getMilitarySkill();
-            let pol = this.tsurumi.getPoliticalSkill();
+            expect(this.tsurumi.getMilitarySkill()).toBe(initialMIL + 1);
+            expect(this.tsurumi.getPoliticalSkill()).toBe(initialPOL + 1);
 
             this.player2.pass();
             this.player1.clickCard(this.stance);
             this.player1.clickCard(this.tsurumi);
+            this.player2.pass();
 
-            expect(this.tsurumi.getMilitarySkill()).toBe(mil - 1);
-            expect(this.tsurumi.getPoliticalSkill()).toBe(pol - 1);
+            expect(this.getChatLogs(5)).toContain(
+                'player1 plays Iron Foundations Stance from their cards set aside by Togashi Tsurumi'
+            );
 
-            expect(this.getChatLogs(5)).toContain('player1 plays Iron Foundations Stance from their cards set aside by Togashi Tsurumi');
+            expect(this.stance.location).toBe('conflict discard pile');
+            expect(this.tsurumi.getMilitarySkill()).toBe(initialMIL);
+            expect(this.tsurumi.getPoliticalSkill()).toBe(initialPOL);
         });
 
-        it('non kihos cards underneath self should not be playable', function() {
+        it('non kihos cards underneath self should not be playable', function () {
             this.noMoreActions();
             this.initiateConflict({
                 attackers: [this.tsurumi],
