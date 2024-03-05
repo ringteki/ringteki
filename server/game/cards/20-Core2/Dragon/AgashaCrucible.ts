26757741f34d9fd7e9b68da1fc2dd4c8207ecaf5
Luiz Socrate <luiz@socrate.com.br>
2024-03-05 15:52:10 +0100

    UPDATE: new Agasha Crucible

--- server/game/cards/20-Core2/Dragon/AgashaCrucible.ts
+++ server/game/cards/20-Core2/Dragon/AgashaCrucible.ts
@@ -1,37 +1,43 @@
 import { CardTypes, Durations, Players } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
-import { PlayAttachmentAction } from '../../../PlayAttachmentAction';
+
+const options = Object.fromEntries(
+    ['Air', 'Earth', 'Void', 'Water'].map((option) => [
+        option,
+        {
+            message: `{1} gains the ${option} Trait`,
+            action: AbilityDsl.actions.sequential([
+                AbilityDsl.actions.cardLastingEffect({
+                    duration: Durations.UntilEndOfPhase,
+                    effect: AbilityDsl.effects.addTrait(option.toLowerCase())
+                }),
+                AbilityDsl.actions.playerLastingEffect((context) => ({
+                    targetController: context.player,
+                    duration: Durations.UntilPassPriority,
+                    effect: AbilityDsl.effects.additionalAction(1)
+                }))
+            ])
+        }
+    ])
+);
 
 export default class AgashaCrucible extends DrawCard {
     static id = 'agasha-crucible';
 
     public setupCardAbilities() {
-        this.interrupt({
-            title: 'Satisfy Affinity for the next Spell',
-            when: {
-                onCardPlayed: (event, context) =>
-                    event.player === context.player && this.#isSpell(event.card as DrawCard),
-                onAbilityResolverInitiated: (event, context) => {
-                    //might be able to remove the source.type check at some point
-                    const isAttachment =
-                        event.context.source.type === CardTypes.Attachment ||
-                        event.context.ability instanceof PlayAttachmentAction;
-                    return (
-                        event.context.player === context.player && isAttachment && this.#isSpell(event.context.source)
-                    );
-                }
+        this.action({
+            title: 'Give Elemental Trait to a Fire Shugenja',
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                cardCondition: (card) => card.hasEveryTrait('fire', 'shugenja'),
+                gameAction: AbilityDsl.actions.chooseAction({
+                    options,
+                    activePromptTitle: 'Choose Trait to gain'
+                })
             },
-            gameAction: AbilityDsl.actions.playerLastingEffect({
-                targetController: Players.Self,
-                duration: Durations.UntilSelfPassPriority,
-                effect: AbilityDsl.effects.satisfyAffinity(['air', 'earth', 'fire', 'water', 'void'])
-            }),
-            effect: 'satisfy all elemental affinities'
+            effect: 'give {0} another Elemental Trait, and take another action!',
         });
-    }
-
-    #isSpell(card: DrawCard) {
-        return card.hasTrait('spell');
     }
 }
--- test/server/cards/20-Core2/Dragon/AgashaCrucible.spec.js
+++ test/server/cards/20-Core2/Dragon/AgashaCrucible.spec.js
@@ -4,7 +4,7 @@ describe('Agasha Crucible', function () {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
-                    inPlay: ['doomed-shugenja'],
+                    inPlay: ['doomed-shugenja', 'miya-mystic'],
                     dynastyDiscard: ['agasha-crucible'],
                     hand: ['the-rushing-wave', 'grasp-of-earth-2']
                 },
@@ -16,41 +16,38 @@ describe('Agasha Crucible', function () {
             this.rushingWave = this.player1.findCardByName('the-rushing-wave');
             this.graspOfEarth = this.player1.findCardByName('grasp-of-earth-2');
             this.doomed = this.player1.findCardByName('doomed-shugenja');
+            this.mystic = this.player1.findCardByName('miya-mystic');
 
-            this.agashaFoundry = this.player1.placeCardInProvince('agasha-crucible', 'province 1');
+            this.agashaCrucible = this.player1.placeCardInProvince('agasha-crucible', 'province 1');
 
             this.shamefulDisplay = this.player2.findCardByName('shameful-display', 'province 1');
             this.favorableGround = this.player2.placeCardInProvince('favorable-ground', 'province 1');
         });
 
-        it('triggers when spell event is played', function () {
+        it('should trigger when a spell event is played', function () {
+            this.player1.clickCard(this.agashaCrucible);
+            expect(this.player1).toHavePrompt('Choose a character');
+            expect(this.player1).toBeAbleToSelect(this.doomed);
+            expect(this.player1).not.toBeAbleToSelect(this.mystic);
+
+            this.player1.clickCard(this.doomed);
+            expect(this.player1).toHavePrompt('Choose Trait to gain');
+            this.player1.clickPrompt('Water');
+            expect(this.getChatLogs(5)).toContain(
+                'player1 uses Agasha Crucible to give Doomed Shugenja another Elemental Trait, and take another action!'
+            );
+            expect(this.getChatLogs(5)).toContain('Doomed Shugenja gains the Water Trait');
+
             this.player1.clickCard(this.rushingWave);
             expect(this.player1).toHavePrompt('Choose a province');
             this.player1.clickCard(this.shamefulDisplay);
-            expect(this.player1).toHavePrompt('Any interrupts to The Rushing Wave being played?');
-            expect(this.player1).toBeAbleToSelect(this.agashaFoundry);
 
-            this.player1.clickCard(this.agashaFoundry);
-            expect(this.getChatLogs(5)).toContain('player1 uses Agasha Crucible to satisfy all elemental affinities');
             expect(this.getChatLogs(5)).toContain(
                 "player1 plays The Rushing Wave to set province 1's strength to 0 until the end of the phase"
             );
             expect(this.getChatLogs(5)).toContain(
                 'player1 channels their water affinity to also set the strength of province 2 to 0'
             );
-        });
-
-        it('triggers when spell attachment is played', function () {
-            const initFate = this.player1.fate;
-
-            this.player1.clickCard(this.graspOfEarth);
-            this.player1.clickCard(this.doomed);
-            expect(this.player1).toHavePrompt('Any interrupts?');
-            expect(this.player1).toBeAbleToSelect(this.agashaFoundry);
-
-            this.player1.clickCard(this.agashaFoundry);
-            expect(this.getChatLogs(5)).toContain('player1 uses Agasha Crucible to satisfy all elemental affinities');
-            expect(this.player1.fate).toBe(initFate);
         });
     });
 });
