cca9185b11e078dd57e8c510b4738d221f4db0e4
Luiz Socrate <luiz@socrate.com.br>
2024-03-05 15:45:05 +0100

    UPDATE: truth Is In The Killing can be used by non-Duelists

--- server/game/cards/20-Core2/Dragon/TruthIsInTheKilling.ts
+++ server/game/cards/20-Core2/Dragon/TruthIsInTheKilling.ts
@@ -1,6 +1,11 @@
-import { CardTypes, DuelTypes } from '../../../Constants';
+import { DuelTypes } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
+import type { Duel } from '../../../Duel';
+
+function applyFullEffect(duel: Duel) {
+    return duel.winner?.some((winner) => winner.hasTrait('duelist')) ?? false;
+}
 
 export default class TruthIsInTheKilling extends DrawCard {
     static id = 'truth-is-in-the-killing';
@@ -8,26 +13,34 @@ export default class TruthIsInTheKilling extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Initiate a military duel, discarding the loser',
-            condition: context => context.game.isDuringConflict('military'),
+            condition: (context) => context.game.isDuringConflict('military'),
             initiateDuel: {
                 type: DuelTypes.Military,
-                challengerCondition: (card) => card.hasTrait('duelist') && card.isParticipating(),
-                gameAction: (duel) => duel.loser && AbilityDsl.actions.sequentialContext(() => {
-                    const gameActions = [];
-                    duel.loser.forEach((loser) => {
-                        gameActions.push(AbilityDsl.actions.removeFate({
-                            target: loser,
-                            amount: loser.getFate(),
-                            recipient: loser.controller
-                        }));
-                        gameActions.push(AbilityDsl.actions.discardFromPlay({
-                            target: loser
-                        }));
-                    });
-                    return { gameActions };
-                }),
-                message: 'discard {0}, returning all fate on them to {1}\'s fate pool',
-                messageArgs: (duel) => [duel.loser, duel.losingPlayer],
+                challengerCondition: (card) => card.hasTrait('bushi') && card.isParticipating(),
+                gameAction: (duel) =>
+                    duel.loser &&
+                    AbilityDsl.actions.sequential(
+                        duel.loser.flatMap((loser) =>
+                            applyFullEffect(duel)
+                                ? [
+                                      AbilityDsl.actions.removeFate({
+                                          target: loser,
+                                          amount: loser.getFate(),
+                                          recipient: loser.controller
+                                      }),
+                                      AbilityDsl.actions.discardFromPlay({ target: loser })
+                                  ]
+                                : [
+                                      AbilityDsl.actions.removeFate({
+                                          target: loser,
+                                          amount: loser.getFate(),
+                                          recipient: loser.controller
+                                      })
+                                  ]
+                        )
+                    ),
+                message: "return all fate on {0} to {1}'s fate pool{2}",
+                messageArgs: (duel) => [duel.loser, duel.losingPlayer, applyFullEffect(duel) ? ' and discard them' : '']
             }
         });
     }
--- test/server/cards/20-Core2/Dragon/TruthIsInTheKilling.spec.js
+++ test/server/cards/20-Core2/Dragon/TruthIsInTheKilling.spec.js
@@ -5,7 +5,7 @@ describe('Truth is in the Killing', function () {
                 phase: 'conflict',
                 player1: {
                     fate: 20,
-                    inPlay: ['doji-challenger', 'kakita-yoshi', 'daidoji-uji'],
+                    inPlay: ['doji-challenger', 'cunning-negotiator', 'daidoji-uji', 'borderlands-defender'],
                     hand: ['truth-is-in-the-killing'],
                     conflictDiscard: ['two-hands']
                 },
@@ -16,7 +16,8 @@ describe('Truth is in the Killing', function () {
             });
 
             this.uji = this.player1.findCardByName('daidoji-uji');
-            this.yoshi = this.player1.findCardByName('kakita-yoshi');
+            this.borderlandsDefender = this.player1.findCardByName('borderlands-defender');
+            this.cunningNegotiator = this.player1.findCardByName('cunning-negotiator');
             this.truth = this.player1.findCardByName('truth-is-in-the-killing');
             this.toshimoko = this.player2.findCardByName('kakita-toshimoko');
             this.challenger = this.player1.findCardByName('doji-challenger');
@@ -27,110 +28,157 @@ describe('Truth is in the Killing', function () {
             this.hands = this.player1.findCardByName('two-hands');
         });
 
-        it('should discard the loser and return fate to its controller', function () {
-            let fate1 = this.player1.fate;
-            let fate2 = this.player2.fate;
+        describe('when winner is not Duelist', function () {
+            it('returns fate from loser to their controller', function () {
+                let fate1 = this.player1.fate;
+                let fate2 = this.player2.fate;
 
-            this.noMoreActions();
-            this.initiateConflict({
-                attackers: [this.challenger, this.yoshi],
-                defenders: [this.tsukune, this.toshimoko]
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderlandsDefender, this.cunningNegotiator],
+                    defenders: [this.tsukune, this.toshimoko]
+                });
+
+                this.toshimoko.fate = 4;
+                this.player2.pass();
+
+                this.player1.clickCard(this.truth);
+                expect(this.player1).toBeAbleToSelect(this.borderlandsDefender);
+                expect(this.player1).not.toBeAbleToSelect(this.cunningNegotiator);
+                this.player1.clickCard(this.borderlandsDefender);
+                this.player1.clickCard(this.toshimoko);
+
+                this.player1.clickPrompt('5');
+                this.player2.clickPrompt('1');
+
+                expect(this.player1.fate).toBe(fate1 - 1);
+                expect(this.player2.fate).toBe(fate2 + 4);
+                expect(this.toshimoko.location).toBe('play area');
+                expect(this.getChatLogs(10)).toContain(
+                    'player1 plays Truth Is In the Killing to initiate a military duel : Borderlands Defender vs. Kakita Toshimoko'
+                );
+                expect(this.getChatLogs(10)).toContain(
+                    "Duel Effect: return all fate on Kakita Toshimoko to player2's fate pool"
+                );
             });
-
-            this.toshimoko.fate = 4;
-            this.player2.pass();
-
-            this.player1.clickCard(this.truth);
-            expect(this.player1).toBeAbleToSelect(this.challenger);
-            expect(this.player1).not.toBeAbleToSelect(this.yoshi);
-            this.player1.clickCard(this.challenger);
-            this.player1.clickCard(this.toshimoko);
-
-            this.player1.clickPrompt('5');
-            this.player2.clickPrompt('1');
-
-            expect(this.player1.fate).toBe(fate1 - 1);
-            expect(this.player2.fate).toBe(fate2 + 4);
-            expect(this.toshimoko.location).toBe('dynasty discard pile');
-            expect(this.getChatLogs(10)).toContain('player1 plays Truth Is In the Killing to initiate a military duel : Doji Challenger vs. Kakita Toshimoko');
-            expect(this.getChatLogs(10)).toContain('Duel Effect: discard Kakita Toshimoko, returning all fate on them to player2\'s fate pool');
         });
 
-        it('should discard the loser and return fate to its controller (self losing)', function () {
-            let fate1 = this.player1.fate;
-            let fate2 = this.player2.fate;
+        describe('when winner is Duelist', function () {
+            it('should discard the loser and return fate to its controller', function () {
+                let fate1 = this.player1.fate;
+                let fate2 = this.player2.fate;
 
-            this.noMoreActions();
-            this.initiateConflict({
-                attackers: [this.challenger, this.yoshi],
-                defenders: [this.tsukune, this.toshimoko]
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.challenger, this.cunningNegotiator],
+                    defenders: [this.tsukune, this.toshimoko]
+                });
+
+                this.toshimoko.fate = 4;
+                this.player2.pass();
+
+                this.player1.clickCard(this.truth);
+                expect(this.player1).toBeAbleToSelect(this.challenger);
+                expect(this.player1).not.toBeAbleToSelect(this.cunningNegotiator);
+                this.player1.clickCard(this.challenger);
+                this.player1.clickCard(this.toshimoko);
+
+                this.player1.clickPrompt('5');
+                this.player2.clickPrompt('1');
+
+                expect(this.player1.fate).toBe(fate1 - 1);
+                expect(this.player2.fate).toBe(fate2 + 4);
+                expect(this.toshimoko.location).toBe('dynasty discard pile');
+                expect(this.getChatLogs(10)).toContain(
+                    'player1 plays Truth Is In the Killing to initiate a military duel : Doji Challenger vs. Kakita Toshimoko'
+                );
+                expect(this.getChatLogs(10)).toContain(
+                    "Duel Effect: return all fate on Kakita Toshimoko to player2's fate pool and discard them"
+                );
             });
 
-            this.challenger.fate = 3;
-            this.toshimoko.fate = 4;
-            this.player2.pass();
+            it('should discard the loser and return fate to its controller (self losing)', function () {
+                let fate1 = this.player1.fate;
+                let fate2 = this.player2.fate;
 
-            this.player1.clickCard(this.truth);
-            this.player1.clickCard(this.challenger);
-            this.player1.clickCard(this.toshimoko);
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.challenger, this.cunningNegotiator],
+                    defenders: [this.tsukune, this.toshimoko]
+                });
 
-            this.player1.clickPrompt('1');
-            this.player2.clickPrompt('5');
+                this.challenger.fate = 3;
+                this.toshimoko.fate = 4;
+                this.player2.pass();
 
-            expect(this.player1.fate).toBe(fate1 + 2);
-            expect(this.player2.fate).toBe(fate2);
-            expect(this.challenger.location).toBe('dynasty discard pile');
-            expect(this.toshimoko.location).toBe('play area');
-            expect(this.toshimoko.fate).toBe(4);
-            expect(this.getChatLogs(10)).toContain('player1 plays Truth Is In the Killing to initiate a military duel : Doji Challenger vs. Kakita Toshimoko');
-            expect(this.getChatLogs(10)).toContain('Duel Effect: discard Doji Challenger, returning all fate on them to player1\'s fate pool');
-        });
+                this.player1.clickCard(this.truth);
+                this.player1.clickCard(this.challenger);
+                this.player1.clickCard(this.toshimoko);
 
-        it('should not work in pol conflicts', function () {
-            this.noMoreActions();
-            this.initiateConflict({
-                attackers: [this.challenger, this.yoshi],
-                defenders: [this.tsukune, this.toshimoko],
-                type: 'political'
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('5');
+
+                expect(this.player1.fate).toBe(fate1 + 2);
+                expect(this.player2.fate).toBe(fate2);
+                expect(this.challenger.location).toBe('dynasty discard pile');
+                expect(this.toshimoko.location).toBe('play area');
+                expect(this.toshimoko.fate).toBe(4);
+                expect(this.getChatLogs(10)).toContain(
+                    'player1 plays Truth Is In the Killing to initiate a military duel : Doji Challenger vs. Kakita Toshimoko'
+                );
+                expect(this.getChatLogs(10)).toContain(
+                    "Duel Effect: return all fate on Doji Challenger to player1's fate pool and discard them"
+                );
             });
 
-            this.player2.pass();
+            it('should not work in pol conflicts', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.challenger, this.cunningNegotiator],
+                    defenders: [this.tsukune, this.toshimoko],
+                    type: 'political'
+                });
 
-            expect(this.player1).toHavePrompt('Conflict Action Window');
-            this.player1.clickCard(this.truth);
-            expect(this.player1).toHavePrompt('Conflict Action Window');
-        });
+                this.player2.pass();
 
-        it('multiple targets', function () {
-            let fate1 = this.player1.fate;
-            let fate2 = this.player2.fate;
-
-            this.player1.moveCard(this.hands, 'hand');
-
-            this.noMoreActions();
-            this.initiateConflict({
-                attackers: [this.challenger, this.yoshi],
-                defenders: [this.diplomat, this.toshimoko]
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.truth);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
             });
 
-            this.toshimoko.fate = 4;
-            this.diplomat.fate = 3;
-            this.player2.pass();
+            it('multiple targets', function () {
+                let fate1 = this.player1.fate;
+                let fate2 = this.player2.fate;
 
-            this.player1.clickCard(this.truth);
-            this.player1.clickCard(this.challenger);
-            this.player1.clickCard(this.toshimoko);
-            this.player1.clickCard(this.hands);
-            this.player1.clickCard(this.diplomat);
+                this.player1.moveCard(this.hands, 'hand');
 
-            this.player1.clickPrompt('5');
-            this.player2.clickPrompt('1');
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.challenger, this.cunningNegotiator],
+                    defenders: [this.diplomat, this.toshimoko]
+                });
 
-            expect(this.player1.fate).toBe(fate1 - 1);
-            expect(this.player2.fate).toBe(fate2 + 7);
-            expect(this.toshimoko.location).toBe('dynasty discard pile');
-            expect(this.diplomat.location).toBe('dynasty discard pile');
-            expect(this.getChatLogs(10)).toContain('Duel Effect: discard Kakita Toshimoko and Doji Diplomat, returning all fate on them to player2\'s fate pool');
+                this.toshimoko.fate = 4;
+                this.diplomat.fate = 3;
+                this.player2.pass();
+
+                this.player1.clickCard(this.truth);
+                this.player1.clickCard(this.challenger);
+                this.player1.clickCard(this.toshimoko);
+                this.player1.clickCard(this.hands);
+                this.player1.clickCard(this.diplomat);
+
+                this.player1.clickPrompt('5');
+                this.player2.clickPrompt('1');
+
+                expect(this.player1.fate).toBe(fate1 - 1);
+                expect(this.player2.fate).toBe(fate2 + 7);
+                expect(this.toshimoko.location).toBe('dynasty discard pile');
+                expect(this.diplomat.location).toBe('dynasty discard pile');
+                expect(this.getChatLogs(10)).toContain(
+                    "Duel Effect: return all fate on Kakita Toshimoko and Doji Diplomat to player2's fate pool and discard them"
+                );
+            });
         });
     });
 });
