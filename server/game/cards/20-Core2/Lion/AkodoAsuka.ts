ce886e205c8c65364ed433d28b23d860a9f27334
Luiz Socrate <luiz@socrate.com.br>
2024-03-05 15:21:20 +0100

    UPDATE: Akodo Asuka selects from top of deck

--- server/game/cards/20-Core2/Lion/AkodoAsuka.ts
+++ server/game/cards/20-Core2/Lion/AkodoAsuka.ts
@@ -1,5 +1,11 @@
+import type { AbilityContext } from '../../../AbilityContext';
 import AbilityDsl from '../../../abilitydsl';
+import { Locations } from '../../../Constants';
 import DrawCard from '../../../drawcard';
+
+function getCharactersWithoutFate(context: AbilityContext) {
+    return context.game.currentConflict?.getNumberOfParticipantsFor(context.player) ?? 0;
+}
 
 export default class AkodoAsuka extends DrawCard {
     static id = 'akodo-asuka';
@@ -9,9 +15,22 @@ export default class AkodoAsuka extends DrawCard {
             title: 'Draw a card',
             when: {
                 afterConflict: (event, context) =>
-                    event.conflict.winner === context.source.controller && context.source.isParticipating()
+                    event.conflict.winner === context.source.controller &&
+                    context.source.isParticipating() &&
+                    context.player.conflictDeck.size() > 0
             },
-            gameAction: AbilityDsl.actions.draw()
+            gameAction: AbilityDsl.actions.deckSearch({
+                amount: (context) => getCharactersWithoutFate(context),
+                activePromptTitle: 'Choose a card to put in your hand',
+                gameAction: AbilityDsl.actions.moveCard({
+                    destination: Locations.Hand
+                }),
+                shuffle: false,
+                reveal: false,
+                placeOnBottomInRandomOrder: true
+            }),
+            effect: 'look at the top {1} cards of their conflict deck',
+            effectArgs: (context) => getCharactersWithoutFate(context)
         });
     }
 }
--- test/server/cards/20-Core2/Lion/AkodoAsuka.spec.js
+++ test/server/cards/20-Core2/Lion/AkodoAsuka.spec.js
@@ -4,12 +4,13 @@ describe('Akodo Asuka', function () {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
-                    inPlay: ['akodo-asuka'],
+                    inPlay: ['akodo-asuka', 'matsu-berserker'],
                     hand: ['fine-katana']
                 },
                 player2: {}
             });
             this.akodoAsuka = this.player1.findCardByName('akodo-asuka');
+            this.matsuBerserker = this.player1.findCardByName('matsu-berserker');
             this.katana = this.player1.findCardByName('fine-katana');
         });
 
@@ -23,17 +24,23 @@ describe('Akodo Asuka', function () {
             expect(this.player1).toBeAbleToSelect(this.akodoAsuka);
         });
 
-        it('draws a card', function () {
+        it('draws a card from the top, depending on number of participants', function () {
             const initialHandSize = this.player1.hand.length;
             this.noMoreActions();
             this.initiateConflict({
-                attackers: [this.akodoAsuka],
+                attackers: [this.akodoAsuka, this.matsuBerserker],
                 defenders: []
             });
             this.noMoreActions();
             this.player1.clickCard(this.akodoAsuka);
+
+            expect(this.player1).toHavePrompt('Akodo Asuka');
+            this.player1.clickPrompt('Supernatural Storm (2)');
             expect(this.player1.hand.length).toBe(initialHandSize + 1);
-            expect(this.getChatLogs(5)).toContain('player1 uses Akodo Asuka to draw 1 card');
+            expect(this.getChatLogs(5)).toContain(
+                'player1 uses Akodo Asuka to look at the top 2 cards of their conflict deck'
+            );
+            expect(this.getChatLogs(5)).toContain('player1 takes 1 card');
         });
     });
 });
