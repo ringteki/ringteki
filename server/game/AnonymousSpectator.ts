4a97eecb89c6e8b9e372fecc7320902621ea7697
Luiz Socrate <luiz@socrate.com.br>
2024-03-18 11:04:31 +0100

    TS: AnonymousSpectator

---
+++ server/game/AnonymousSpectator.ts
@@ -0,0 +1,14 @@
+export class AnonymousSpectator {
+    name = 'Anonymous';
+    emailHash = '';
+    buttons = [];
+    menuTitle = 'Spectator mode';
+
+    getCardSelectionState() {
+        return {};
+    }
+
+    getRingSelectionState() {
+        return {};
+    }
+}
