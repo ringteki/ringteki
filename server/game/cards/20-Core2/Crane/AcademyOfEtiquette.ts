a5b96e5c1c96d9cd439411bfa7e7b6f77566d081
Luiz Socrate <luiz@socrate.com.br>
2024-07-10 21:18:53 +0200

    Remove double Thriving

--- server/game/cards/20-Core2/Crane/AcademyOfEtiquette.ts
+++ server/game/cards/20-Core2/Crane/AcademyOfEtiquette.ts
@@ -1,4 +1,4 @@
-import { TargetModes, Players, Locations, Phases, CardTypes, Durations } from '../../../Constants';
+import { TargetModes, Players, Phases, CardTypes, Durations } from '../../../Constants';
 import AbilityDsl from '../../../abilitydsl';
 import DrawCard from '../../../drawcard';
 
@@ -24,26 +24,6 @@ export default class AcademyOfEtiquette extends DrawCard {
                 }))
             },
             effect: 'give {0} courtesy'
-        });
-
-        this.reaction({
-            title: 'Place a card in this province',
-            when: {
-                onPhaseStarted: (event) => event.phase === Phases.Dynasty
-            },
-            gameAction: AbilityDsl.actions.handler({
-                handler: (context) => {
-                    const province = context.source.location;
-                    this.game.queueSimpleStep(() => context.player.putTopDynastyCardInProvince(province));
-                }
-            }),
-            effect: 'place {1} faceup in {2}',
-            effectArgs: (context) => [
-                context.player.dynastyDeck.first() ? context.player.dynastyDeck.first() : 'a card',
-                context.player.getProvinceCardInProvince(context.source.location).isFacedown()
-                    ? context.source.location
-                    : context.player.getProvinceCardInProvince(context.source.location)
-            ]
         });
     }
 }
