192d0931ff1c5052f57a99c21181e1884b4d3edb
Luiz Socrate <luiz@socrate.com.br>
2024-08-08 23:37:05 +0200

    NEW: Kaiu Foreman

---
+++ server/game/cards/21-Noob/Crab/KaiuForeman.ts
@@ -0,0 +1,30 @@
+import AbilityDsl from '../../../abilitydsl';
+import { Conflict } from '../../../conflict';
+import { CardTypes, Players } from '../../../Constants';
+import DrawCard from '../../../drawcard';
+
+export default class KaiuForeman extends DrawCard {
+    static id = 'kaiu-foreman';
+
+    setupCardAbilities() {
+        this.persistentEffect({
+            condition: (context) =>
+                (context.game.currentConflict as undefined | Conflict)?.anyParticipants(
+                    (card) => card.controller === context.player.opponent && card.isFaction('unicorn')
+                ),
+            effect: AbilityDsl.effects.modifyBothSkills(1)
+        });
+
+        this.action({
+            title: 'Move a character into or out of the conflict',
+            condition: (context) => context.game.isDuringConflict(),
+            cost: AbilityDsl.costs.sacrificeSelf(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                cardCondition: (card, context) => card !== context.source,
+                gameAction: [AbilityDsl.actions.sendHome(), AbilityDsl.actions.moveToConflict()]
+            }
+        });
+    }
+}
