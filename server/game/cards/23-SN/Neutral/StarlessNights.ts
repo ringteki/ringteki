90ca16c2d343a8c27c4e33f3b5225dc56699004c
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-08-31 18:42:32 +0200

    Fix starless nights

--- server/game/cards/23-SN/Neutral/StarlessNights.ts
+++ server/game/cards/23-SN/Neutral/StarlessNights.ts
@@ -1,13 +1,17 @@
 import DrawCard from '../../../DrawCard.js';
 import AbilityDsl from '../../../abilitydsl.js';
 import { AbilityContext } from '../../../AbilityContext.js';
+import { Phases } from '../../../Constants.js';
 
 export default class StarlessNights extends DrawCard {
     static id = 'starless-nights';
 
     setupCardAbilities() {
-        this.action({
-            title: 'Place or take fate from rings',
+        this.reaction({
+            title: 'Place 1 fate on each unclaimed ring',
+            when: {
+                onPhaseStarted: (event) => event.phase === Phases.Conflict
+            },
             gameAction: AbilityDsl.actions.placeFateOnRing((context: AbilityContext) => ({
                 target: Object.values(context.game.rings).filter(ring => ring.isUnclaimed())
             })),
--- test/server/cards/23-SN/Neutral/StarlessNights.spec.js
+++ test/server/cards/23-SN/Neutral/StarlessNights.spec.js
@@ -1,28 +1,63 @@
 describe('Starless Nights', function () {
     integration(function () {
-        beforeEach(function () {
-            this.setupTest({
-                phase: 'dynasty',
-                player1: {
-                    hand: ['starless-nights']
-                },
-                player2: {
-                }
+        describe('during the dynasty phase', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'dynasty',
+                    player1: {
+                        hand: ['starless-nights']
+                    },
+                    player2: {
+                    }
+                });
+                this.starlessNights = this.player1.findCardByName('starless-nights');
+                this.player1.claimRing('air');
+                this.game.rings.earth.fate = 1;
+                this.game.rings.void.fate = 2;
             });
-            this.writtenInTheStars = this.player1.findCardByName('starless-nights');
-            this.player1.claimRing('air');
-            this.game.rings.earth.fate = 1;
-            this.game.rings.void.fate = 2;
+
+            it('cannot be played', function () {
+                this.player1.clickCard(this.starlessNights);
+
+                expect(this.game.rings.fire.fate).toBe(0);
+                expect(this.game.rings.water.fate).toBe(0);
+                expect(this.game.rings.earth.fate).toBe(1);
+                expect(this.game.rings.void.fate).toBe(2);
+            });
         });
 
-        it('should place 1 fate on each unclaimed ring if chosen', function () {
-            this.player1.clickCard(this.writtenInTheStars);
-            expect(this.game.rings.air.fate).toBe(0);
-            expect(this.game.rings.fire.fate).toBe(1);
-            expect(this.game.rings.earth.fate).toBe(2);
-            expect(this.game.rings.void.fate).toBe(3);
-            expect(this.game.rings.water.fate).toBe(1);
-            expect(this.getChatLogs(1)).toContain('player1 plays Starless Nights to place 1 fate on Earth Ring, Fire Ring, Void Ring and Water Ring');
+        describe('when the conflict phase begins', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'draw',
+                    player1: {
+                        hand: ['starless-nights']
+                    },
+                    player2: {
+                    }
+                });
+                this.starlessNights = this.player1.findCardByName('starless-nights');
+                this.player1.claimRing('air');
+                this.game.rings.earth.fate = 1;
+                this.game.rings.void.fate = 2;
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+            });
+
+            it('should place 1 fate on each unclaimed ring', function () {
+                this.noMoreActions();
+
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.starlessNights);
+                this.player1.clickCard(this.starlessNights);
+
+                expect(this.game.rings.air.fate).toBe(0);
+                expect(this.game.rings.fire.fate).toBe(1);
+                expect(this.game.rings.earth.fate).toBe(2);
+                expect(this.game.rings.void.fate).toBe(3);
+                expect(this.game.rings.water.fate).toBe(1);
+                expect(this.getChatLogs(5)).toContain('player1 plays Starless Nights to place 1 fate on Earth Ring, Fire Ring, Void Ring and Water Ring');
+            });
         });
     });
 });
