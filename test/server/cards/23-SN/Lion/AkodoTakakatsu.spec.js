f3974dd84068c06d76f05f3c410b007e82b93869
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-09-12 21:05:30 +0200

    Make Tadakatsu react to opponent only

--- test/server/cards/23-SN/Lion/AkodoTakakatsu.spec.js
+++ test/server/cards/23-SN/Lion/AkodoTakakatsu.spec.js
@@ -8,7 +8,8 @@ describe('Akodo Takakatsu', function () {
                     hand: ['invocation-of-ash']
                 },
                 player2: {
-                    inPlay: ['togashi-mitsu', 'doji-whisperer']
+                    inPlay: ['togashi-mitsu', 'doji-whisperer'],
+                    hand: ['mono-no-aware']
                 }
             });
             this.mitsu = this.player2.findCardByName('togashi-mitsu');
@@ -17,16 +18,16 @@ describe('Akodo Takakatsu', function () {
             this.tadakatsu = this.player1.findCardByName('akodo-tadakatsu');
             this.challenger = this.player1.findCardByName('doji-challenger');
             this.invocation = this.player1.findCardByName('invocation-of-ash');
+            this.monoNoAware = this.player2.findCardByName('mono-no-aware');
 
             this.tadakatsu.fate = 2;
             this.mitsu.fate = 1;
         });
 
-        it('reaction to removing fate', function () {
-            this.player1.playAttachment(this.invocation, this.challenger);
-            this.player2.pass();
-            this.player1.clickCard(this.invocation);
-            this.player1.clickCard(this.tadakatsu);
+        it('reaction to an opponent removing fate', function () {
+            this.player1.pass();
+            this.player2.clickCard(this.monoNoAware);
+            expect(this.tadakatsu.fate).toBe(1);
             expect(this.player1).toHavePrompt('Triggered Abilities');
             expect(this.player1).toBeAbleToSelect(this.tadakatsu);
 
@@ -39,6 +40,38 @@ describe('Akodo Takakatsu', function () {
             this.player1.clickCard(this.whisperer);
             expect(this.whisperer.location).toBe('dynasty discard pile');
             expect(this.getChatLogs(5)).toContain('player1 uses Akodo Tadakatsu to injure Doji Whisperer');
+        });
+
+        it('reaction to an opponent resolving a ring effect', function () {
+            this.noMoreActions();
+            this.player1.passConflict();
+            this.noMoreActions();
+            this.initiateConflict({
+                ring: 'void',
+                type: 'political',
+                attackers: [this.whisperer],
+                defenders: []
+            });
+            this.noMoreActions();
+
+            this.player2.clickCard(this.tadakatsu);
+            expect(this.tadakatsu.fate).toBe(1);
+
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            this.player1.clickCard(this.tadakatsu);
+            this.player1.clickCard(this.mitsu);
+            expect(this.mitsu.fate).toBe(0);
+        });
+
+        it('no reaction when your own card removes the fate', function () {
+            this.player1.playAttachment(this.invocation, this.challenger);
+            this.player2.pass();
+            this.player1.clickCard(this.invocation);
+            this.player1.clickCard(this.tadakatsu);
+
+            expect(this.tadakatsu.fate).toBe(1);
+            expect(this.player1).not.toBeAbleToSelect(this.tadakatsu);
+            expect(this.player1).toHavePrompt('Waiting for opponent to take an action or pass');
         });
 
         it('reaction to attacking - bow', function () {
